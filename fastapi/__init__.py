8cc9ccab92e0de849f344fe895b06c2b7ea6ceac
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-10 05:53:46 +0400

    :art: Update main imports (and exports)

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -1,3 +1,7 @@
-"""Fast API framework, high performance, fast to learn, fast to code, fast to production"""
+"""FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
+
+from .applications import FastAPI
+from .routing import APIRouter
+from .params import Body, Path, Query, Header, Cookie, Form, File, Security, Depends
---
+++ fastapi/security/__init__.py
@@ -0,0 +1,4 @@
+from .api_key import APIKeyQuery, APIKeyHeader, APIKeyCookie
+from .http import HTTPBasic, HTTPBearer, HTTPDigest
+from .oauth2 import OAuth2PasswordRequestForm, OAuth2
+from .open_id_connect_url import OpenIdConnect
