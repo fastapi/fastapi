f539217b5bd2362f76071eab55a8c82b5d7cc2da
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-13 21:26:30 +0400

    :truck: Script to auto-resort tutorial src files

---
+++ scripts/rename_tutorial_src_files.py
@@ -0,0 +1,15 @@
+#%%
+from pathlib import Path
+from string import digits
+
+
+#%%
+directory = Path("../docs/tutorial/src")
+output_directory = Path("../docs/tutorial/out")
+files = sorted([Path(f) for f in directory.iterdir()])
+for i, f in enumerate(files):
+    f: Path
+    index = str(i + 1).zfill(2)
+    new_name = output_directory / f"tutorial{index}.py"
+    print(new_name)
+    f.rename(new_name)
