786c392bd5d4931f012a4dff8d9e97a7a3b99190
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-17 18:56:48 +0400

    :green_heart: Fix Netlify build with pipenv

--- scripts/netlify-docs.sh
+++ scripts/netlify-docs.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
-$HOME/python$PIPENV_RUNTIME/bin/pipenv run -- mkdocs build
+# Install pipenv to be able to install from Pipfile
+pip install pipenv
+# Install Pipfile including --dev, to install mkdocs and plugins
+pipenv install --dev
+# Finally, run mkdocs
+mkdocs build
