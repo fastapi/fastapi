e9f2a3c29185466f1c92121e9f4e4b727fb20fd0
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-18 21:59:51 +0400

    :sparkles: Update tutorial renamer to exclude files

--- scripts/rename_tutorial_src_files.py
+++ scripts/rename_tutorial_src_files.py
@@ -3,7 +3,9 @@ from pathlib import Path, PurePath
 from string import digits
 
 directory = Path("./docs/tutorial/src")
-dirs = sorted([Path(f) for f in directory.iterdir()])
+skip_names = {"bigger_applications"}
+skip_dirs = {directory / name for name in skip_names}
+dirs = sorted([Path(f) for f in directory.iterdir() if f not in skip_dirs])
 d: PurePath
 sufix = "__out__"
 for d in dirs:
@@ -13,9 +15,13 @@ for d in dirs:
     output_directory = directory / output_dir_name
     output_directory.mkdir(exist_ok=True)
     files = sorted([Path(f) for f in d.iterdir()])
+    f: PurePath
     for i, f in enumerate(files):
         index = str(i + 1).zfill(3)
-        new_name = output_directory / f"tutorial{index}.py"
+        if f.name != "__init__.py" and f.name.endswith(".py"):
+            new_name = output_directory / f"tutorial{index}.py"
+        else:
+            new_name = output_directory / f.name
         print(new_name)
         f.rename(new_name)
 
