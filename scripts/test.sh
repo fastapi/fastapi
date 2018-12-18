343ecb7669267d01f75a5410ca868b30cf746a40
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-18 22:08:47 +0400

    :green_heart: Use pipenv in Travis

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,11 +8,11 @@ export PYTHON_VERSION=`python -c "$VERSION_SCRIPT"`
 
 
 # PYTHONPATH=. pytest --cov=fastapi --cov=tests --cov-fail-under=100 --cov-report=term-missing ${@} --cov-report=html
-PYTHONPATH=.:./docs/tutorial/src pytest --cov=fastapi --cov=tests --cov=docs/tutorial/src --cov-report=term-missing ${@} --cov-report=html
-mypy fastapi --disallow-untyped-defs
+PYTHONPATH=.:./docs/tutorial/src pipenv run -- pytest --cov=fastapi --cov=tests --cov=docs/tutorial/src --cov-report=term-missing ${@} --cov-report=html
+pipenv run -- mypy fastapi --disallow-untyped-defs
 if [ "${PYTHON_VERSION}" = '3.7' ]; then
     echo "Skipping 'black' on 3.7. See issue https://github.com/ambv/black/issues/494"
 else
-    black fastapi tests --check
+    pipenv run -- black fastapi tests --check
 fi
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only fastapi tests
+pipenv run -- isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only fastapi tests
