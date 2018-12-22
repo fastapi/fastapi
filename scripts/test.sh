23ef570bf62569f717f3dcd709cd752a1a50b35e
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-22 07:42:24 +0400

    :white_check_mark: Add test-cov-html script for local coverage
    analysis and debugging

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,8 +7,7 @@ export VERSION_SCRIPT="import sys; print('%s.%s' % sys.version_info[0:2])"
 export PYTHON_VERSION=`python -c "$VERSION_SCRIPT"`
 
 export PYTHONPATH=./docs/src
-# PYTHONPATH=. pytest --cov=fastapi --cov=tests --cov-fail-under=100 --cov-report=term-missing ${@} --cov-report=html
-pytest --cov=fastapi --cov=tests --cov=docs/src --cov-report=term-missing ${@} --cov-report=html
+pytest --cov=fastapi --cov=tests --cov=docs/src --cov-report=term-missing ${@}
 mypy fastapi --disallow-untyped-defs
 if [ "${PYTHON_VERSION}" = '3.7' ]; then
     echo "Skipping 'black' on 3.7. See issue https://github.com/ambv/black/issues/494"
