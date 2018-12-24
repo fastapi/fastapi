098e629344bde573865ac96b24f7beb6689759b0
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-24 20:21:28 +0400

    :bookmark: Bump version, after changes in OAuth2 utils

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -1,6 +1,6 @@
 """FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
 
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 
 from .applications import FastAPI
 from .routing import APIRouter
