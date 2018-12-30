3c08b05ea6b58b3e5564c026f071cbb0979a6797
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-30 21:46:49 +0400

    :bookmark: Bump version, after query and header as lists
    and bug fixes for Python 3.7

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -1,6 +1,6 @@
 """FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
 
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 
 from .applications import FastAPI
 from .routing import APIRouter
