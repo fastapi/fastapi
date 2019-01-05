e5b341c7ddbd085e7203276398bdf10a1d86446b
Sebastián Ramírez <tiangolo@gmail.com>
2019-01-05 17:38:59 +0400

    :bookmark: Bump version after fix for constrained bytes

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -1,6 +1,6 @@
 """FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
 
-__version__ = "0.1.16"
+__version__ = "0.1.17"
 
 from .applications import FastAPI
 from .routing import APIRouter
