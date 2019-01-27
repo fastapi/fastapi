06f0c93bc0059fc77e45b2fd60c04ed975a5c6d6
euri10 <benoit.barthelet@gmail.com>
2019-01-27 14:39:19 +0100

    Modified app.route decorators so that it's xxx_starlette and grouped the test under TestStarletteStyle

--- tests/test_dbmiddleware.py
+++ tests/test_dbmiddleware.py
@@ -37,7 +37,7 @@ def create_test_database():
     engine.execute("DROP TABLE notes")
 
 
-@app.route("/notes", methods=["GET"])
+@app.route("/notes_starlette", methods=["GET"])
 async def list_notes(request):
     query = notes.select()
     results = await request.database.fetchall(query)
@@ -47,7 +47,7 @@ async def list_notes(request):
     return JSONResponse(content)
 
 
-@app.route("/notes", methods=["POST"])
+@app.route("/notes_starlette", methods=["POST"])
 @transaction
 async def add_note(request):
     data = await request.json()
@@ -58,7 +58,7 @@ async def add_note(request):
     return JSONResponse({"text": data["text"], "completed": data["completed"]})
 
 
-@app.route("/notes/bulk_create", methods=["POST"])
+@app.route("/notes_starlette/bulk_create", methods=["POST"])
 async def bulk_create_notes(request):
     data = await request.json()
     query = notes.insert()
@@ -66,7 +66,7 @@ async def bulk_create_notes(request):
     return JSONResponse({"notes": data})
 
 
-@app.route("/notes/{note_id:int}", methods=["GET"])
+@app.route("/notes_starlette/{note_id:int}", methods=["GET"])
 async def read_note(request):
     note_id = request.path_params["note_id"]
     query = notes.select().where(notes.c.id == note_id)
@@ -75,7 +75,7 @@ async def read_note(request):
     return JSONResponse(content)
 
 
-@app.route("/notes/{note_id:int}/text", methods=["GET"])
+@app.route("/notes_starlette/{note_id:int}/text", methods=["GET"])
 async def read_note_text(request):
     note_id = request.path_params["note_id"]
     query = sqlalchemy.select([notes.c.text]).where(notes.c.id == note_id)
@@ -83,79 +83,81 @@ async def read_note_text(request):
     return JSONResponse(text)
 
 
-def test_database():
-    with TestClient(app) as client:
-        response = client.post(
-            "/notes", json={"text": "buy the milk", "completed": True}
-        )
-        assert response.status_code == 200
+class TestStarletteStyle():
 
-        with pytest.raises(RuntimeError):
+    def test_database(self):
+        with TestClient(app) as client:
             response = client.post(
-                "/notes",
-                json={"text": "you wont see me", "completed": False},
-                params={"raise_exc": "true"},
+                "/notes_starlette", json={"text": "buy the milk", "completed": True}
             )
+            assert response.status_code == 200
 
-        response = client.post(
-            "/notes", json={"text": "walk the dog", "completed": False}
-        )
-        assert response.status_code == 200
+            with pytest.raises(RuntimeError):
+                response = client.post(
+                    "/notes_starlette",
+                    json={"text": "you wont see me", "completed": False},
+                    params={"raise_exc": "true"},
+                )
 
-        response = client.get("/notes")
-        assert response.status_code == 200
-        assert response.json() == [
-            {"text": "buy the milk", "completed": True},
-            {"text": "walk the dog", "completed": False},
-        ]
+            response = client.post(
+                "/notes_starlette", json={"text": "walk the dog", "completed": False}
+            )
+            assert response.status_code == 200
 
-        response = client.get("/notes/1")
-        assert response.status_code == 200
-        assert response.json() == {"text": "buy the milk", "completed": True}
+            response = client.get("/notes_starlette")
+            assert response.status_code == 200
+            assert response.json() == [
+                {"text": "buy the milk", "completed": True},
+                {"text": "walk the dog", "completed": False},
+            ]
 
-        response = client.get("/notes/1/text")
-        assert response.status_code == 200
-        assert response.json() == "buy the milk"
+            response = client.get("/notes_starlette/1")
+            assert response.status_code == 200
+            assert response.json() == {"text": "buy the milk", "completed": True}
 
-
-def test_database_executemany():
-    with TestClient(app) as client:
-        data = [
-            {"text": "buy the milk", "completed": True},
-            {"text": "walk the dog", "completed": False},
-        ]
-        response = client.post("/notes/bulk_create", json=data)
-        assert response.status_code == 200
-
-        response = client.get("/notes")
-        assert response.status_code == 200
-        assert response.json() == [
-            {"text": "buy the milk", "completed": True},
-            {"text": "walk the dog", "completed": False},
-        ]
+            response = client.get("/notes_starlette/1/text")
+            assert response.status_code == 200
+            assert response.json() == "buy the milk"
 
 
-def test_database_isolated_during_test_cases():
-    """
-    Using `TestClient` as a context manager
-    """
+    def test_database_executemany(self):
+        with TestClient(app) as client:
+            data = [
+                {"text": "buy the milk", "completed": True},
+                {"text": "walk the dog", "completed": False},
+            ]
+            response = client.post("/notes_starlette/bulk_create", json=data)
+            assert response.status_code == 200
 
-    with TestClient(app) as client:
-        response = client.post(
-            "/notes", json={"text": "just one note", "completed": True}
-        )
-        assert response.status_code == 200
+            response = client.get("/notes_starlette")
+            assert response.status_code == 200
+            assert response.json() == [
+                {"text": "buy the milk", "completed": True},
+                {"text": "walk the dog", "completed": False},
+            ]
 
-        response = client.get("/notes")
-        assert response.status_code == 200
-        assert response.json() == [{"text": "just one note", "completed": True}]
 
-    with TestClient(app) as client:
-        response = client.post(
-            "/notes", json={"text": "just one note", "completed": True}
-        )
-        assert response.status_code == 200
+    def test_database_isolated_during_test_cases(self):
+        """
+        Using `TestClient` as a context manager
+        """
 
-        response = client.get("/notes")
-        assert response.status_code == 200
-        assert response.json() == [{"text": "just one note", "completed": True}]
+        with TestClient(app) as client:
+            response = client.post(
+                "/notes_starlette", json={"text": "just one note", "completed": True}
+            )
+            assert response.status_code == 200
+
+            response = client.get("/notes_starlette")
+            assert response.status_code == 200
+            assert response.json() == [{"text": "just one note", "completed": True}]
+
+        with TestClient(app) as client:
+            response = client.post(
+                "/notes_starlette", json={"text": "just one note", "completed": True}
+            )
+            assert response.status_code == 200
+
+            response = client.get("/notes_starlette")
+            assert response.status_code == 200
+            assert response.json() == [{"text": "just one note", "completed": True}]
