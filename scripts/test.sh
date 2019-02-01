ecdc775040ae6d61162eee3f146800d3006a1782
euri10 <benoit.barthelet@gmail.com>
2019-02-01 22:33:35 +0100

    Isort --diff might be more interesting

--- scripts/test.sh
+++ scripts/test.sh
@@ -14,4 +14,4 @@ if [ "${PYTHON_VERSION}" = '3.7' ]; then
 else
     black fastapi tests --check
 fi
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --verbose fastapi tests
+isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --diff fastapi tests
