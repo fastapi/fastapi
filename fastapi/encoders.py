955e9fcb3198e1f34c562fc240591df783ae1725
Sebastián Ramírez <tiangolo@gmail.com>
2019-02-12 23:02:21 +0400

    Update fix SQLAlchemy support with ORM (#30)
    :sparkles: SQLAlchemy ORM support
    
    Improved jsonable_encoder with SQLAlchemy support, tests running with SQLite, improved and updated SQL docs
    
    * :heavy_plus_sign: Add SQLAlchemy to development dependencies (not required for using FastAPI)
    
    * :heavy_plus_sign: Add sqlalchemy to testing dependencies (not required to use FastAPI)

--- fastapi/encoders.py
+++ fastapi/encoders.py
@@ -13,6 +13,7 @@ def jsonable_encoder(
     by_alias: bool = False,
     include_none: bool = True,
     custom_encoder: dict = {},
+    sqlalchemy_safe: bool = True,
 ) -> Any:
     if isinstance(obj, BaseModel):
         encoder = getattr(obj.Config, "json_encoders", custom_encoder)
@@ -20,39 +21,55 @@ def jsonable_encoder(
             obj.dict(include=include, exclude=exclude, by_alias=by_alias),
             include_none=include_none,
             custom_encoder=encoder,
+            sqlalchemy_safe=sqlalchemy_safe,
         )
     if isinstance(obj, Enum):
         return obj.value
     if isinstance(obj, (str, int, float, type(None))):
         return obj
     if isinstance(obj, dict):
-        return {
-            jsonable_encoder(
-                key,
-                by_alias=by_alias,
-                include_none=include_none,
-                custom_encoder=custom_encoder,
-            ): jsonable_encoder(
-                value,
-                by_alias=by_alias,
-                include_none=include_none,
-                custom_encoder=custom_encoder,
-            )
-            for key, value in obj.items()
-            if value is not None or include_none
-        }
+        encoded_dict = {}
+        for key, value in obj.items():
+            if (
+                (
+                    not sqlalchemy_safe
+                    or (not isinstance(key, str))
+                    or (not key.startswith("_sa"))
+                )
+                and (value is not None or include_none)
+                and ((include and key in include) or key not in exclude)
+            ):
+                encoded_key = jsonable_encoder(
+                    key,
+                    by_alias=by_alias,
+                    include_none=include_none,
+                    custom_encoder=custom_encoder,
+                    sqlalchemy_safe=sqlalchemy_safe,
+                )
+                encoded_value = jsonable_encoder(
+                    value,
+                    by_alias=by_alias,
+                    include_none=include_none,
+                    custom_encoder=custom_encoder,
+                    sqlalchemy_safe=sqlalchemy_safe,
+                )
+                encoded_dict[encoded_key] = encoded_value
+        return encoded_dict
     if isinstance(obj, (list, set, frozenset, GeneratorType, tuple)):
-        return [
-            jsonable_encoder(
-                item,
-                include=include,
-                exclude=exclude,
-                by_alias=by_alias,
-                include_none=include_none,
-                custom_encoder=custom_encoder,
+        encoded_list = []
+        for item in obj:
+            encoded_list.append(
+                jsonable_encoder(
+                    item,
+                    include=include,
+                    exclude=exclude,
+                    by_alias=by_alias,
+                    include_none=include_none,
+                    custom_encoder=custom_encoder,
+                    sqlalchemy_safe=sqlalchemy_safe,
+                )
             )
-            for item in obj
-        ]
+        return encoded_list
     errors = []
     try:
         if custom_encoder and type(obj) in custom_encoder:
@@ -71,4 +88,10 @@ def jsonable_encoder(
             except Exception as e:
                 errors.append(e)
                 raise ValueError(errors)
-    return jsonable_encoder(data, by_alias=by_alias, include_none=include_none)
+    return jsonable_encoder(
+        data,
+        by_alias=by_alias,
+        include_none=include_none,
+        custom_encoder=custom_encoder,
+        sqlalchemy_safe=sqlalchemy_safe,
+    )
