0aaa655a833f0a792eafba754a3c5ed7c4ed145d
Sebastián Ramírez <tiangolo@gmail.com>
2019-02-20 21:54:21 +0400

    :sparkles: Add docs for GraphQL

---
+++ docs/src/graphql/tutorial001.py
@@ -0,0 +1,14 @@
+import graphene
+from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return "Hello " + name
+
+
+app = FastAPI()
+app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
