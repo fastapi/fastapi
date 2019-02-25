63710242b0de007ed10bbf8811c0b96b97ff6215
euri10 <benoit.barthelet@gmail.com>
2019-02-25 11:26:18 +0100

    Hopefully better coverage adding tags generation on other methods

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -9,6 +9,7 @@ def framework():
     tag1 = APIRouter(tags=["tag1"])
     tag2 = APIRouter(tags=["tag2", "tag1"])
     tag3 = APIRouter(tags=["tag2", "tag1"])
+    tag4 = APIRouter(tags=["tag4"])
 
     @tag1.get("/get")
     async def tag1_get():
@@ -50,9 +51,42 @@ def framework():
     async def tag3_get():
         return {"Tag": "tag3"}
 
+    @tag4.get("/get", tags=["tag4get"])
+    async def tag4_get():
+        return {"Tag": "tag4"}
+
+    @tag4.put("/put", tags=["tag4put"])
+    async def tag4_put():
+        return {"Tag": "tag4"}
+
+    @tag4.post("/post", tags=["tag4post"])
+    async def tag4_post():
+        return {"Tag": "tag4"}
+
+    @tag4.delete("/delete", tags=["tag4delete"])
+    async def tag4_delete():
+        return {"Tag": "tag4"}
+
+    @tag4.options("/options", tags=["tag4options"])
+    async def tag4_options():
+        return {"Tag": "tag4"}
+
+    @tag4.head("/head", tags=["tag4head"])
+    async def tag4_head():
+        return {"Tag": "tag4"}
+
+    @tag4.patch("/patch", tags=["tag4patch"])
+    async def tag4_patch():
+        return {"Tag": "tag4"}
+
+    @tag4.trace("/trace", tags=["tag4trace"])
+    async def tag4_trace():
+        return {"Tag": "tag4"}
+
     app.include_router(tag1, prefix="/tag1prefix")
     app.include_router(tag2, prefix="/tag2prefix")
     app.include_router(tag3, prefix="/tag3prefix")
+    app.include_router(tag4, prefix="/tag4prefix")
 
     yield app
 
@@ -66,6 +100,12 @@ data = [
     ),
     ("tag2", ["get"], ["tag2", "tag1"], {"Tag": "tag2"}),
     ("tag3", ["get"], ["tag2", "tag1", "tag3"], {"Tag": "tag3"}),
+    (
+        "tag4",
+        ["get", "put", "post", "delete", "options", "patch", "trace"],
+        [],
+        {"Tag": "tag4"},
+    ),
 ]
 
 
@@ -74,17 +114,33 @@ def test_tags_in_schema(framework, tag, methods, expected_tags, expected_json):
     with TestClient(framework) as client:
         response = client.get("/openapi.json")
         assert response.status_code == 200
-        print(response.json())
-        for method in methods:
-            assert (
-                response.json()
-                .get("paths")
-                .get(f"/{tag}prefix/{method}")
-                .get(method)
-                .get("tags")
-                == expected_tags
-            )
-            url = framework.url_path_for(f"{tag}_{method}")
-            rresponse = client.request(method, url)
-            assert rresponse.status_code == 200
-            assert rresponse.json() == expected_json
+        if tag in ["tag1", "tag2", "tag3"]:
+            for method in methods:
+                assert (
+                    response.json()
+                    .get("paths")
+                    .get(f"/{tag}prefix/{method}")
+                    .get(method)
+                    .get("tags")
+                    == expected_tags
+                )
+                url = framework.url_path_for(f"{tag}_{method}")
+                rresponse = client.request(method, url)
+                assert rresponse.status_code == 200
+                assert rresponse.json() == expected_json
+        elif tag == "tag4":
+            for method in methods:
+                expected_tags = ["tag4"]
+                expected_tags.append(f"tag4{method}")
+                assert (
+                    response.json()
+                    .get("paths")
+                    .get(f"/{tag}prefix/{method}")
+                    .get(method)
+                    .get("tags")
+                    == expected_tags
+                )
+                url = framework.url_path_for(f"{tag}_{method}")
+                rresponse = client.request(method, url)
+                assert rresponse.status_code == 200
+                assert rresponse.json() == expected_json
