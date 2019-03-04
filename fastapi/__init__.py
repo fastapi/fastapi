ac2b18bf40c3b24ae5f7998d236a342f2888f9ff
Sebastián Ramírez <tiangolo@gmail.com>
2019-03-04 20:07:11 +0400

    :bookmark: Release 0.7.1, Raspberry Pi deployment and docs

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -1,6 +1,6 @@
 """FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 from .applications import FastAPI
 from .routing import APIRouter
