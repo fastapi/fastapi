ef5fb33045568bbea0649ac7c7d21fd81ec43ec7
Sebastián Ramírez <tiangolo@gmail.com>
2019-03-24 12:42:45 +0400

    :sparkles: Add events docs and tests

---
+++ docs/src/events/tutorial001.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = {}
+
+
+@app.on_event("startup")
+async def startup_event():
+    items["foo"] = {"name": "Fighters"}
+    items["bar"] = {"name": "Tenders"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    return items[item_id]
