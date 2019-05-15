fcaff646469f509900a7b390e72e5b425f6ead19
Sebastián Ramírez <tiangolo@gmail.com>
2019-05-15 22:13:06 +0400

    :wrench: Separate format and lint scripts (#232)

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,6 +10,4 @@ fi
 
 export PYTHONPATH=./docs/src
 pytest --cov=fastapi --cov=tests --cov=docs/src --cov-report=term-missing ${@}
-mypy fastapi --disallow-untyped-defs --follow-imports=skip
-black fastapi tests --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only --thirdparty fastapi fastapi tests
+bash ./scripts/lint.sh
