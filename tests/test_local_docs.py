338dce7e260031ecd4818a2f3c1bfa90065d99df
euri10 <benoit.barthelet@gmail.com>
2019-05-16 10:46:49 +0200

    Simplified tests

--- tests/test_local_docs.py
+++ tests/test_local_docs.py
@@ -1,43 +1,35 @@
-from itertools import permutations
+import inspect
 
 from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
 
 
 def test_string_in_generated_swagger():
-    custom_kwargs_keys = ["swagger_js_url", "swagger_css_url", "swagger_favicon_url"]
+    sig = inspect.signature(get_swagger_ui_html)
+    swagger_js_url = sig.parameters.get("swagger_js_url").default
+    swagger_css_url = sig.parameters.get("swagger_css_url").default
+    swagger_favicon_url = sig.parameters.get("swagger_favicon_url").default
+    html = get_swagger_ui_html(openapi_url="/docs", title="title")
+    assert swagger_js_url in html.body.decode()
+    assert swagger_css_url in html.body.decode()
+    assert swagger_favicon_url in html.body.decode()
     fake = {
         "swagger_js_url": "fake_js",
         "swagger_css_url": "fake_css",
         "swagger_favicon_url": "fake_favicon",
     }
-    for r in range(3):
-        for permutation in permutations(custom_kwargs_keys, r):
-            custom_kwargs = {}
-            for p in permutation:
-                if p == "swagger_js_url":
-                    custom_kwargs["swagger_js_url"] = fake.get(p)
-                elif p == "swagger_css_url":
-                    custom_kwargs["swagger_css_url"] = fake.get(p)
-                elif p == "swagger_favicon_url":
-                    custom_kwargs["swagger_favicon_url"] = fake.get(p)
-            html = get_swagger_ui_html(
-                openapi_url="/docs", title="title", **custom_kwargs
-            )
-            for k in custom_kwargs.keys():
-                assert fake.get(k) in html.body.decode()
+    html = get_swagger_ui_html(openapi_url="/docs", title="title", **fake)
+    for k in fake.keys():
+        assert fake.get(k) in html.body.decode()
 
 
 def test_string_in_generated_redoc():
-    custom_kwargs_keys = ["redoc_js_url", "redoc_favicon_url"]
+    sig = inspect.signature(get_redoc_html)
+    redoc_js_url = sig.parameters.get("redoc_js_url").default
+    redoc_favicon_url = sig.parameters.get("redoc_favicon_url").default
+    html = get_redoc_html(openapi_url="/docs", title="title")
+    assert redoc_js_url in html.body.decode()
+    assert redoc_favicon_url in html.body.decode()
     fake = {"redoc_js_url": "fake_js", "redoc_favicon_url": "fake_favicon"}
-    for r in range(3):
-        for permutation in permutations(custom_kwargs_keys, r):
-            custom_kwargs = {}
-            for p in permutation:
-                if p == "redoc_js_url":
-                    custom_kwargs["redoc_js_url"] = fake.get(p)
-                elif p == "redoc_favicon_url":
-                    custom_kwargs["redoc_favicon_url"] = fake.get(p)
-            html = get_redoc_html(openapi_url="/redoc", title="title", **custom_kwargs)
-            for k in custom_kwargs.keys():
-                assert fake.get(k) in html.body.decode()
+    html = get_redoc_html(openapi_url="/docs", title="title", **fake)
+    for k in fake.keys():
+        assert fake.get(k) in html.body.decode()
