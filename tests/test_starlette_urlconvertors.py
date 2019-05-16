accedeb4cee362547808b24cf58b82e554741537
Sebastián Ramírez <tiangolo@gmail.com>
2019-05-16 17:50:51 +0400

    :white_check_mark: Separate Starlette URL convertors in their own functions

--- tests/test_starlette_urlconvertors.py
+++ tests/test_starlette_urlconvertors.py
@@ -22,23 +22,30 @@ def path_convertor(param: str = Path(...)):
 client = TestClient(app)
 
 
-def test_route_converters():
+def test_route_converters_int():
     # Test integer conversion
     response = client.get("/int/5")
     assert response.status_code == 200
     assert response.json() == {"int": 5}
     assert app.url_path_for("int_convertor", param=5) == "/int/5"
 
+
+def test_route_converters_float():
     # Test float conversion
     response = client.get("/float/25.5")
     assert response.status_code == 200
     assert response.json() == {"float": 25.5}
     assert app.url_path_for("float_convertor", param=25.5) == "/float/25.5"
 
+
+def test_route_converters_path():
     # Test path conversion
     response = client.get("/path/some/example")
     assert response.status_code == 200
     assert response.json() == {"path": "some/example"}
+
+
+def test_url_path_for_path_convertor():
     assert (
         app.url_path_for("path_convertor", param="some/example") == "/path/some/example"
     )
