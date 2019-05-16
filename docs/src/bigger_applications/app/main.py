981a50a08fa4b6e9e62f82ad63d21b8ee5d225a0
Sebastián Ramírez <tiangolo@gmail.com>
2019-05-16 16:50:22 +0400

    :memo: Add docs for parameter dependencies

--- docs/src/bigger_applications/app/main.py
+++ docs/src/bigger_applications/app/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 from .routers import items, users
 
 app = FastAPI()
+
+
+async def get_token_header(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
 
 app.include_router(users.router)
 app.include_router(
     items.router,
     prefix="/items",
     tags=["items"],
+    dependencies=[Depends(get_token_header)],
     responses={404: {"description": "Not found"}},
 )
