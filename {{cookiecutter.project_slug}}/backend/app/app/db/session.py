170231783adddb3ba974bfb98dc66116b95ee6af
Manu <emmanuel@breton.ch>
2019-05-22 13:18:59 +0200

    :card_file_box: Fix SQLAlchemy operation error after database restarts (#32)

--- {{cookiecutter.project_slug}}/backend/app/app/db/session.py
+++ {{cookiecutter.project_slug}}/backend/app/app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from app.core import config
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
