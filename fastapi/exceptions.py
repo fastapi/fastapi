439089a6b8a6f01b055343cfd8e035e26ce017a2
Sebastián Ramírez <tiangolo@gmail.com>
2019-05-30 19:33:52 +0400

    :bug: Fix type declaration of HTTPException

--- fastapi/exceptions.py
+++ fastapi/exceptions.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 from pydantic import ValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 
 class HTTPException(StarletteHTTPException):
     def __init__(
-        self, status_code: int, detail: str = None, headers: dict = None
+        self, status_code: int, detail: Any = None, headers: dict = None
     ) -> None:
         super().__init__(status_code=status_code, detail=detail)
         self.headers = headers
