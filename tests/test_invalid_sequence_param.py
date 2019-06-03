67a486fcf3117d9f8244ee0c9875740d775a2b84
Sebastián Ramírez <tiangolo@gmail.com>
2019-06-03 21:33:24 +0400

    :white_check_mark: Add several tests to ensure invalid types are not accepted

--- tests/test_invalid_sequence_param.py
+++ tests/test_invalid_sequence_param.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 import pytest
 from fastapi import FastAPI, Query
@@ -26,4 +26,28 @@ def test_invalid_tuple():
 
         @app.get("/items/")
         def read_items(q: Tuple[Item, Item] = Query(None)):
+            pass  # pragma: no cover
+
+
+def test_invalid_dict():
+    with pytest.raises(AssertionError):
+        app = FastAPI()
+
+        class Item(BaseModel):
+            title: str
+
+        @app.get("/items/")
+        def read_items(q: Dict[str, Item] = Query(None)):
+            pass  # pragma: no cover
+
+
+def test_invalid_simple_dict():
+    with pytest.raises(AssertionError):
+        app = FastAPI()
+
+        class Item(BaseModel):
+            title: str
+
+        @app.get("/items/")
+        def read_items(q: dict = Query(None)):
             pass  # pragma: no cover
