4bbfdac8894b045909dfd6feea81f7605ac5ac81
Sebastián Ramírez <tiangolo@gmail.com>
2019-06-28 20:08:55 +0200

    :white_check_mark: Add test for templates in include_router path

---
+++ tests/test_router_prefix_with_template.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, FastAPI
+from starlette.testclient import TestClient
+
+app = FastAPI()
+
+router = APIRouter()
+
+
+@router.get("/users/{id}")
+def read_user(segment: str, id: str):
+    return {"segment": segment, "id": id}
+
+
+app.include_router(router, prefix="/{segment}")
+
+
+client = TestClient(app)
+
+
+def test_get():
+    response = client.get("/seg/users/foo")
+    assert response.status_code == 200
+    assert response.json() == {"segment": "seg", "id": "foo"}
