cc3fc86bb129d9d6abdc9b134cce501a64cf0adc
David Montague <david@exactrics.com>
2019-08-11 18:37:11 -0700

    Add type-checking for dependencies

--- fastapi/param_functions.py
+++ fastapi/param_functions.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Sequence
+from typing import Any, Callable, Sequence, TypeVar, overload
 
 from fastapi import params
 
@@ -238,10 +238,40 @@ def File(  # noqa: N802
     )
 
 
+T = TypeVar("T")
+
+
+@overload
+def Depends(dependency: Callable[..., T], *, use_cache: bool = True) -> T:
+    ...
+
+
+@overload
+def Depends(dependency: None = None, *, use_cache: bool = True) -> Any:
+    ...
+
+
 def Depends(  # noqa: N802
     dependency: Callable = None, *, use_cache: bool = True
 ) -> Any:
     return params.Depends(dependency=dependency, use_cache=use_cache)
+
+
+@overload
+def Security(
+    dependency: Callable[..., T],
+    *,
+    scopes: Sequence[str] = None,
+    use_cache: bool = True,
+) -> T:
+    ...
+
+
+@overload
+def Security(
+    dependency: None = None, *, scopes: Sequence[str] = None, use_cache: bool = True
+) -> Any:
+    ...
 
 
 def Security(  # noqa: N802
