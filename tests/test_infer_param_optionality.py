9c862582a473d8c40f269cbea7370cf4aa63821e
jonathanunderwood <jonathan.underwood@gmail.com>
2019-09-29 17:37:42 +0100

    Update tests/test_infer_param_optionality.py
    Co-Authored-By: Sebastián Ramírez <tiangolo@gmail.com>

--- tests/test_infer_param_optionality.py
+++ tests/test_infer_param_optionality.py
@@ -52,7 +52,6 @@ def test_get_users():
 
 def test_get_user():
     """Check that /users/{user_id} returns expected data"""
-    """
     response = client.get("/users/abc123")
     assert response.status_code == 200
     assert response.json() == {"user_id": "abc123"}
