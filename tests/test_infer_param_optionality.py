d49bd8b1fdeffa412bbf4e46c78f5d473e0e88e8
jonathanunderwood <jonathan.underwood@gmail.com>
2019-09-29 17:37:00 +0100

    Update tests/test_infer_param_optionality.py
    Co-Authored-By: Sebastián Ramírez <tiangolo@gmail.com>

--- tests/test_infer_param_optionality.py
+++ tests/test_infer_param_optionality.py
@@ -45,7 +45,6 @@ client = TestClient(app)
 
 def test_get_users():
     """Check that /users returns expected data"""
-    """
     response = client.get("/users")
     assert response.status_code == 200
     assert response.json() == [{"user_id": "u1"}, {"user_id": "u2"}]
