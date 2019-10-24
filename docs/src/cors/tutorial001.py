66fbb00f8d59bf3435a06b6707299f0da8f175ad
Forest Monsen <forest.monsen@gmail.com>
2019-10-23 19:51:22 -0700

    Fix protocol separator typo.

--- docs/src/cors/tutorial001.py
+++ docs/src/cors/tutorial001.py
@@ -6,8 +6,8 @@ app = FastAPI()
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
-    "http:localhost",
-    "http:localhost:8080",
+    "http://localhost",
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
