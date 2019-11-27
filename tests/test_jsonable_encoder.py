35ada5c2eef70741da1d18400fdd506e8a6e9009
Sebastián Ramírez <tiangolo@gmail.com>
2019-11-27 21:17:47 +0100

    :art: Fix format for test

--- tests/test_jsonable_encoder.py
+++ tests/test_jsonable_encoder.py
@@ -116,7 +116,7 @@ def test_custom_encoders():
 
     instance = MyModel(dt_field=safe_datetime.now())
 
-    encoded_instance = jsonable_encoder(instance, custom_encoder={
-        safe_datetime: lambda o: o.isoformat()
-    })
-    assert encoded_instance['dt_field'] == instance.dt_field.isoformat()
+    encoded_instance = jsonable_encoder(
+        instance, custom_encoder={safe_datetime: lambda o: o.isoformat()}
+    )
+    assert encoded_instance["dt_field"] == instance.dt_field.isoformat()
