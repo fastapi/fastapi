8b12675e994a16a80d9541f651034da876309cc2
Jesse P. Johnson <kuwv@users.noreply.github.com>
2019-12-17 22:08:31 -0500

    Update oauth2.py

--- fastapi/security/oauth2.py
+++ fastapi/security/oauth2.py
@@ -163,6 +163,43 @@ class OAuth2PasswordBearer(OAuth2):
         return param
 
 
+class OAuth2AuthorizationCodeBearer(OAuth2):
+    def __init__(
+        self,
+        authorization_url: str,
+        token_url: str,
+        refresh_url: str = None,
+        scopes: dict = {},
+        scheme_name: str = None,
+        auto_error: bool = True,
+    ):
+        flows = OAuthFlowsModel(
+            authorizationCode={
+                "authorizationUrl": authorization_url,
+                "tokenUrl": token_url,
+                "refreshUrl": refresh_url,
+                "scopes": scopes,
+            })
+        super().__init__(
+            flows=flows, scheme_name=scheme_name, auto_error=auto_error
+        )
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.headers.get("Authorization")
+        print(authorization)
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Unauthorized",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
+
+
 class SecurityScopes:
     def __init__(self, scopes: List[str] = None):
         self.scopes = scopes or []
