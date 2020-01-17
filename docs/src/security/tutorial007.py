174e7b1730c6b21eb015a8b8ff3e56e018bc0de3
Pavel Isaev <isaevpd@users.noreply.github.com>
2020-01-17 13:49:54 +0200

    :lock: Check both username and password in security tutorial (#865)

--- docs/src/security/tutorial007.py
+++ docs/src/security/tutorial007.py
@@ -12,7 +12,7 @@ security = HTTPBasic()
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
     correct_password = secrets.compare_digest(credentials.password, "swordfish")
-    if not correct_username and correct_password:
+    if not (correct_username and correct_password):
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
