957313063076e345771eb24e98cbdc9fc2f1f641
Sebastián Ramírez <tiangolo@gmail.com>
2020-02-04 05:41:42 +0100

    :white_check_mark: Lint first in tests, to error out faster (#948)

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,9 +7,8 @@ set -x
 if [ -f ./test.db ]; then
     rm ./test.db
 fi
-
-export PYTHONPATH=./docs/src
-pytest --cov=fastapi --cov=tests --cov=docs/src --cov-report=term-missing ${@}
 bash ./scripts/lint.sh
 # Check README.md is up to date
 diff --brief docs/index.md README.md
+export PYTHONPATH=./docs/src
+pytest --cov=fastapi --cov=tests --cov=docs/src --cov-report=term-missing ${@}
