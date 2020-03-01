12325e07fdf11127eeac56614bc88a321437e02f
Sebastián Ramírez <tiangolo@gmail.com>
2020-03-01 21:31:25 +0100

    :white_check_mark: Test WSGIMiddleware example

---
+++ tests/test_tutorial/test_wsgi/test_tutorial001.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+from wsgi.tutorial001 import app
+
+client = TestClient(app)
+
+
+def test_flask():
+    response = client.get("/v1/")
+    assert response.status_code == 200
+    assert response.text == "Hello, World from Flask!"
+
+
+def test_app():
+    response = client.get("/v2")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
