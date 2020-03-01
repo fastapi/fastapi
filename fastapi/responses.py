6548731956402f170d83edbdfd5f04e747381607
Sebastián Ramírez <tiangolo@gmail.com>
2020-03-01 22:24:00 +0100

    :sparkles: Add ORJSONResponse

--- fastapi/responses.py
+++ fastapi/responses.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from starlette.responses import FileResponse  # noqa
 from starlette.responses import HTMLResponse  # noqa
 from starlette.responses import JSONResponse  # noqa
@@ -6,3 +8,16 @@ from starlette.responses import RedirectResponse  # noqa
 from starlette.responses import Response  # noqa
 from starlette.responses import StreamingResponse  # noqa
 from starlette.responses import UJSONResponse  # noqa
+
+try:
+    import orjson
+except ImportError:  # pragma: nocover
+    orjson = None  # type: ignore
+
+
+class ORJSONResponse(JSONResponse):
+    media_type = "application/json"
+
+    def render(self, content: Any) -> bytes:
+        assert orjson is not None, "orjson must be installed to use ORJSONResponse"
+        return orjson.dumps(content)
