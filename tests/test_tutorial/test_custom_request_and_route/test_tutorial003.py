740e8eeef4ff52b4a2dfd2ef21409359c1bf2d74
Sebastián Ramírez <tiangolo@gmail.com>
2020-03-28 20:24:22 +0100

    :white_check_mark: Update tests to run on Windows

--- tests/test_tutorial/test_custom_request_and_route/test_tutorial003.py
+++ tests/test_tutorial/test_custom_request_and_route/test_tutorial003.py
@@ -15,4 +15,4 @@ def test_get_timed():
     response = client.get("/timed")
     assert response.json() == {"message": "It's the time of my life"}
     assert "X-Response-Time" in response.headers
-    assert float(response.headers["X-Response-Time"]) > 0
+    assert float(response.headers["X-Response-Time"]) >= 0
