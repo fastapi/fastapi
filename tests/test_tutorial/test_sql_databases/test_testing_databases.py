8d87a31be03c0a646f0044eeae670d77777cbe55
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-08 07:21:50 +0200

    :card_file_box: Update database setup for tests

--- tests/test_tutorial/test_sql_databases/test_testing_databases.py
+++ tests/test_tutorial/test_sql_databases/test_testing_databases.py
@@ -1,13 +1,16 @@
+import importlib
 from pathlib import Path
 
 
 def test_testing_dbs():
-    # Import while creating the client to create the DB after starting the test session
-    from sql_databases.sql_app.tests.test_sql_app import test_create_user
-
     test_db = Path("./test.db")
-    app_db = Path("./sql_app.db")
-    test_create_user()
-    test_db.unlink()
-    if app_db.is_file():  # pragma: nocover
-        app_db.unlink()
+    if test_db.is_file():  # pragma: nocover
+        test_db.unlink()
+    # Import while creating the client to create the DB after starting the test session
+    from sql_databases.sql_app.tests import test_sql_app
+
+    # Ensure import side effects are re-executed
+    importlib.reload(test_sql_app)
+    test_sql_app.test_create_user()
+    if test_db.is_file():  # pragma: nocover
+        test_db.unlink()
