a7d3671a7210608e56f4d4ccb5c7496a24339171
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-19 09:17:14 +0200

    :recycle: Simplify env files, merge to one .env (#151)

--- scripts/discard-dev-files.sh
+++ scripts/discard-dev-files.sh
@@ -6,9 +6,6 @@ git checkout \{\{cookiecutter.project_slug\}\}/README.md
 git checkout \{\{cookiecutter.project_slug\}\}/.gitlab-ci.yml
 git checkout \{\{cookiecutter.project_slug\}\}/cookiecutter-config-file.yml
 git checkout \{\{cookiecutter.project_slug\}\}/docker-compose.deploy.networks.yml
-git checkout \{\{cookiecutter.project_slug\}\}/env-backend.env
-git checkout \{\{cookiecutter.project_slug\}\}/env-flower.env
 git checkout \{\{cookiecutter.project_slug\}\}/.env
 git checkout \{\{cookiecutter.project_slug\}\}/frontend/.env
-git checkout \{\{cookiecutter.project_slug\}\}/env-pgadmin.env
-git checkout \{\{cookiecutter.project_slug\}\}/env-postgres.env
+
