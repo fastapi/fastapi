a5155c77c7a6f9d4c49b50804d6014d9cc3f9b88
yankeexe <yankee.exe@gmail.com>
2020-06-05 21:13:34 +0545

    Added line breaks for readability

--- docs_src/path_params/tutorial005.py
+++ docs_src/path_params/tutorial005.py
@@ -16,6 +16,8 @@ app = FastAPI()
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
+
     return {"model_name": model_name, "message": "Have some residuals"}
