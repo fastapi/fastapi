eb4da7bf17daf4de4155097988b8852972e3a49b
ihakh <45719816+ihakh@users.noreply.github.com>
2020-06-06 16:57:28 +0430

    a template for use PUT or PATCH with files
    the basic HTTP form only supports post
    with this template the new FASTAPI user can quickly use POST or PATCH ...

--- docs_src/request_files/tutorial002.py
+++ docs_src/request_files/tutorial002.py
@@ -20,14 +20,24 @@ async def create_upload_files(files: List[UploadFile] = File(...)):
 async def main():
     content = """
 <body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
+<form id="file" enctype="multipart/form-data">
+    <input id="files" name="files" type="file" multiple>
 </form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
+<input type="submit" value="files" onclick="submit('/files/')">
+<input type="submit" value="uploadfiles" onclick="submit('/uploadfiles/')">
+<pre id="json"></pre>
+<script>
+    function submit(url){
+        const xhr = new XMLHttpRequest();
+        // with this you can use PUT or PATCH
+        xhr.open('POST', url);
+        xhr.onload = function () {
+            document.getElementById("json").textContent = JSON.stringify(JSON.parse(this.response), undefined, 2);
+        };
+        Data = new FormData(document.getElementById('file'));
+        xhr.send(Data);
+    }
+</script>
 </body>
     """
     return HTMLResponse(content=content)
