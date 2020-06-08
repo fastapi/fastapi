7392fcf6d164f87323346bb6b116c02fcfba278e
retnikt <master@retnikt.uk>
2020-06-08 14:06:37 +0100

    add endpoint test for enum status code

--- tests/test_application.py
+++ tests/test_application.py
@@ -1161,3 +1161,9 @@ def test_redoc():
     assert response.status_code == 200, response.text
     assert response.headers["content-type"] == "text/html; charset=utf-8"
     assert "redoc@next" in response.text
+
+
+def test_enum_status_code_response():
+    response = client.get("/enum-status-code")
+    assert response.status_code == 201, response.text
+    assert response.json() == "foo bar"
