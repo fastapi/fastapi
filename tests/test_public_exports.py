c4cd1d7825bc74b9bae205264c9360b3d9c37d24
Gergő Jedlicska <57442769+gjedlicska@users.noreply.github.com>
2020-06-09 21:22:46 +0200

    Update tests/test_public_exports.py
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- tests/test_public_exports.py
+++ tests/test_public_exports.py
@@ -2,6 +2,17 @@ import fastapi
 
 
 def test_fastapi_module_exports():
+"""
+Test the publicly exported module set.
+Benefits for declaring explicitly the public exports:
+    * Using __all__ sets what gets imported with `from fastapi import *`
+    * Enables mypy to pass all checks with fastapi when using `implicit_reexport = False` 
+"""
+import yourpackage
+
+missing = set(n for n in yourpackage.__all__
+              if getattr(yourpackage, n, None) is None)
+self.assertEmpty(missing, msg=f"__all__ contains unresolved names: {missing}")
     """
     Test the publicly exported module set.
 
