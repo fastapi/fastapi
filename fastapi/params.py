f6b115fdff8f1d1592392133e1475e3d4a44d86d
rkbeatss <rkaus053@uottawa.ca>
2020-06-10 08:46:17 -0400

    formatting again

--- fastapi/params.py
+++ fastapi/params.py
@@ -318,8 +318,7 @@ class Depends:
         self.use_cache = use_cache
 
     def __repr__(self) -> str:
-        attr = getattr(self.dependency, "__name__",
-                       type(self.dependency).__name__)
+        attr = getattr(self.dependency, "__name__", type(self.dependency).__name__)
         cache = '' if self.use_cache else ', use_cache=False'
         return f"{self.__class__.__name__}({attr}{cache})"
 
