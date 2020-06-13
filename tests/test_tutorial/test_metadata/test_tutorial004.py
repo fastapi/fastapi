a8f335ef746341711f75930ab748e4af0e649b22
Sebastián Ramírez <tiangolo@gmail.com>
2020-06-13 13:46:17 +0200

    ✅ Move tags metadata test to tutorial subdir

--- tests/test_tutorial/test_metadata/test_tutorial004.py
+++ tests/test_tutorial/test_metadata/test_tutorial004.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from docs_src.metadata.tutorial004 import app
+from metadata.tutorial004 import app
 
 client = TestClient(app)
 
@@ -8,42 +8,45 @@ openapi_schema = {
     "openapi": "3.0.2",
     "info": {"title": "FastAPI", "version": "0.1.0"},
     "paths": {
-        "/bar": {
+        "/users/": {
             "get": {
-                "operationId": "get_bar_bar_get",
+                "tags": ["users"],
+                "summary": "Get Users",
+                "operationId": "get_users_users__get",
                 "responses": {
                     "200": {
-                        "content": {"application/json": {"schema": {}}},
                         "description": "Successful Response",
+                        "content": {"application/json": {"schema": {}}},
                     }
                 },
-                "summary": "Get Bar",
-                "tags": ["bar"],
             }
         },
-        "/foo": {
+        "/items/": {
             "get": {
-                "operationId": "get_foo_foo_get",
+                "tags": ["items"],
+                "summary": "Get Items",
+                "operationId": "get_items_items__get",
                 "responses": {
                     "200": {
-                        "content": {"application/json": {"schema": {}}},
                         "description": "Successful Response",
+                        "content": {"application/json": {"schema": {}}},
                     }
                 },
-                "summary": "Get Foo",
-                "tags": ["foo"],
             }
         },
     },
     "tags": [
-        {"description": "This is the description for tag FOO", "name": "foo"},
         {
-            "description": "This is the description for tag BAR",
+            "name": "users",
+            "description": "Operations with users. The **login** logic is also here.",
+        },
+        {
+            "name": "items",
+            "description": "Manage items. So _fancy_ they have their own docs.",
             "externalDocs": {
-                "description": "External documentation",
+                "description": "Items external docs",
                 "url": "https://fastapi.tiangolo.com/",
             },
-            "name": "bar",
         },
     ],
 }
@@ -55,13 +58,8 @@ def test_openapi_schema():
     assert response.json() == openapi_schema
 
 
-def test_get_api_route1():
-    response = client.get("/foo")
+def test_path_operations():
+    response = client.get("/items/")
     assert response.status_code == 200, response.text
-    assert response.json() == {"id": "foo"}
-
-
-def test_get_api_route2():
-    response = client.get("/bar")
+    response = client.get("/users/")
     assert response.status_code == 200, response.text
-    assert response.json() == {"id": "bar"}
