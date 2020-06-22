eca6e038fadb75f8df112604766f1e8ee3a3a290
yk396 <yk396@cornell.edu>
2020-06-22 12:16:07 -0400

    Address PR comments

--- docs_src/path_params_numeric_validations/tutorial001.py
+++ docs_src/path_params_numeric_validations/tutorial001.py
@@ -8,7 +8,7 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_items(
     item_id: int = Path(..., title="The ID of the item to get"),
-    q: Optional[str] = Query(None, alias="item-query"), # can someone check this?
+    q: Optional[str] = Query(None, alias="item-query"),
 ):
     results = {"item_id": item_id}
     if q:
