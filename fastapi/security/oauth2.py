427f849d3ed9d1eb7e4e5788921d0798fb3aed78
vtomardb <varun.tomar@databricks.com>
2020-06-24 10:05:39 -0700

    fixing docstring for oauth2

--- fastapi/security/oauth2.py
+++ fastapi/security/oauth2.py
@@ -14,7 +14,7 @@ class OAuth2PasswordRequestForm:
     This is a dependency class, use it like:
 
         @app.post("/login")
-        def login(form_data: Oauth2PasswordRequestForm = Depends()):
+        def login(form_data: OAuth2PasswordRequestForm = Depends()):
             data = form_data.parse()
             print(data.username)
             print(data.password)
@@ -64,7 +64,7 @@ class OAuth2PasswordRequestFormStrict(OAuth2PasswordRequestForm):
     This is a dependency class, use it like:
 
         @app.post("/login")
-        def login(form_data: Oauth2PasswordRequestFormStrict = Depends()):
+        def login(form_data: OAuth2PasswordRequestFormStrict = Depends()):
             data = form_data.parse()
             print(data.username)
             print(data.password)
