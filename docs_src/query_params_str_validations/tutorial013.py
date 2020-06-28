dad6867ac4f3863a41aba10b227859d068bf86b4
Sebastián Ramírez <tiangolo@gmail.com>
2020-06-28 20:02:10 +0200

    ♻️ Simplify query param list example

--- docs_src/query_params_str_validations/tutorial013.py
+++ docs_src/query_params_str_validations/tutorial013.py
@@ -1,11 +1,9 @@
-from typing import Optional
-
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Optional[list] = Query(None)):
+async def read_items(q: list = Query([])):
     query_items = {"q": q}
     return query_items
