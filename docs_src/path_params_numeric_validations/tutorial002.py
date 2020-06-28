ce5c7d584273efd4a166fd31dd967df186235b22
Sebastián Ramírez <tiangolo@gmail.com>
2020-06-28 17:41:59 +0200

    ⏪ Revert modified example

--- docs_src/path_params_numeric_validations/tutorial002.py
+++ docs_src/path_params_numeric_validations/tutorial002.py
@@ -1,14 +1,11 @@
-from typing import Optional
-
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 
 @app.get("/items/{item_id}")
 async def read_items(
-    item_id: int = Path(..., title="The ID of the item to get"),
-    q: Optional[str] = Query(None, alias="item-query"),
+    q: str, item_id: int = Path(..., title="The ID of the item to get")
 ):
     results = {"item_id": item_id}
     if q:
