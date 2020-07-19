17d9f6c88fa07a96fa7d0a0dace97491c90cfb47
Sebastián Ramírez <tiangolo@gmail.com>
2020-07-19 20:17:08 +0200

    ♻️ Update script to trigger docs preview

--- scripts/trigger-docs-preview.sh
+++ scripts/trigger-docs-preview.sh
@@ -3,14 +3,13 @@
 set -x
 set -e
 
-PR1=${PR1:?Variable not set}
-PR2=${PR2:?Variable not set}
-PR3=${PR3:?Variable not set}
-PR4=${PR4:?Variable not set}
-echo "$PR1"
-echo "$PR2"
-echo "$PR3"
-echo "$PR4"
+PR=${PR}
+
+if [ -z "$PR" ]; then
+    echo "Not a PR build, skip trigger docs preview"
+    exit 0
+fi
+
 NAME=${NAME:?Variable not set}
 GITHUB_TOKEN=${GITHUB_TOKEN:?Variable not set}
 
