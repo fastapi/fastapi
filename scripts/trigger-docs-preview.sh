446134512a5642526c0c5a5d2cb6c3815b654b36
Sebastián Ramírez <tiangolo@gmail.com>
2020-07-19 19:38:50 +0200

    👷 Update PR extraction

--- scripts/trigger-docs-preview.sh
+++ scripts/trigger-docs-preview.sh
@@ -6,9 +6,11 @@ set -e
 PR1=${PR1:?Variable not set}
 PR2=${PR2:?Variable not set}
 PR3=${PR3:?Variable not set}
+PR4=${PR4:?Variable not set}
 echo "$PR1"
 echo "$PR2"
 echo "$PR3"
+echo "$PR4"
 NAME=${NAME:?Variable not set}
 GITHUB_TOKEN=${GITHUB_TOKEN:?Variable not set}
 
