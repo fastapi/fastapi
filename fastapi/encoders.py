7416c0ae564e44143a8656bb00eed3d910347ff7
Sergey Teplyakov <katilot@mail.ru>
2020-07-19 16:27:41 +0300

    Fix `encoders.py` format

--- fastapi/encoders.py
+++ fastapi/encoders.py
@@ -96,7 +96,9 @@ def jsonable_encoder(
                     or (not key.startswith("_sa"))
                 )
                 and (value is not None or not exclude_none)
-                and ((include and key in include) or (not exclude or key not in exclude))
+                and (
+                    (include and key in include) or (not exclude or key not in exclude)
+                )
             ):
                 encoded_key = jsonable_encoder(
                     key,
