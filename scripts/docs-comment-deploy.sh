6a227ca62ec7c65308aa735e7abdddc523005c04
Sebastián Ramírez <tiangolo@gmail.com>
2020-07-20 16:14:10 +0200

    🔧 Update GitHub Actions circus to use commit hash

--- scripts/docs-comment-deploy.sh
+++ scripts/docs-comment-deploy.sh
@@ -6,8 +6,9 @@ set -e
 PR=${PR:?Variable not set}
 DEPLOY_URL=${DEPLOY_URL:?Variable not set}
 GITHUB_TOKEN=${GITHUB_TOKEN:?Variable not set}
+COMMIT=${COMMIT:?Variable not set}
 
 curl \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     https://api.github.com/repos/tiangolo/fastapi/issues/${PR}/comments \
-    -d '{"body": "📝 Docs preview: '"${DEPLOY_URL}"'"}'
+    -d '{"body": "📝 Docs preview for commit '"${COMMIT} at: ${DEPLOY_URL}"'"}'
