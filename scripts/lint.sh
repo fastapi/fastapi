edb986d9a78fd6393ba4001595c09d1bd02648a2
nimashadix <nimashadix@pop-os.localdomain>
2020-07-22 17:49:27 +0430

    add flake8 to ./scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,5 +4,6 @@ set -e
 set -x
 
 mypy fastapi
+flake8 fastapi tests --max-line-length=88 --select=C,E,F,W,B,B950 --ignore=E203,E501,W503
 black fastapi tests --check
 isort fastapi tests docs_src scripts --check-only
