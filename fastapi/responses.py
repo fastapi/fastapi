e12e0d5d94c3ea927b934546797a5210313e95a0
Oleksii Petrenko <opetrenko@mirantis.com>
2020-08-07 11:06:38 +0300

    Add orjson response factory with ability to pass option

--- fastapi/responses.py
+++ fastapi/responses.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Type
 
 from starlette.responses import FileResponse  # noqa
 from starlette.responses import HTMLResponse  # noqa
@@ -17,7 +17,19 @@ except ImportError:  # pragma: nocover
 
 class ORJSONResponse(JSONResponse):
     media_type = "application/json"
+    option: int = 0
 
     def render(self, content: Any) -> bytes:
         assert orjson is not None, "orjson must be installed to use ORJSONResponse"
-        return orjson.dumps(content)
+        return orjson.dumps(content, option=self.option)
+
+
+def create_or_json_class(options: int = 0) -> Type[ORJSONResponse]:
+    """
+    Create a new ORJsonResponse class with additional options.
+    """
+
+    class _ORJSONResponse(ORJSONResponse):
+        option = options
+
+    return _ORJSONResponse
