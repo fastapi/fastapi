02fe46d229e93fbcf2a0b4456d3159243d4aa4da
Nik Sidnev <sidnev.nick@gmail.com>
2020-08-08 12:18:50 +0300

    fix typo in tests

--- tests/test_http_connection_injection.py
+++ tests/test_http_connection_injection.py
@@ -7,18 +7,18 @@ app = FastAPI()
 app.state.value = 42
 
 
-async def extact_value_from_http_connection(conn: HTTPConnection):
+async def extract_value_from_http_connection(conn: HTTPConnection):
     return conn.app.state.value
 
 
 @app.get("/http")
-async def get_value_by_http(value: int = Depends(extact_value_from_http_connection)):
+async def get_value_by_http(value: int = Depends(extract_value_from_http_connection)):
     return value
 
 
 @app.websocket("/ws")
 async def get_value_by_ws(
-    websocket: WebSocket, value: int = Depends(extact_value_from_http_connection)
+    websocket: WebSocket, value: int = Depends(extract_value_from_http_connection)
 ):
     await websocket.accept()
     await websocket.send_json(value)
