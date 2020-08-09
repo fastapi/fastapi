eaadfde9a15ce7f6629cb206fdeae8924149827e
Sebastián Ramírez <tiangolo@gmail.com>
2020-08-09 15:44:43 +0200

    ✅ Increase test for multiple clients

--- tests/test_tutorial/test_websockets/test_tutorial003.py
+++ tests/test_tutorial/test_websockets/test_tutorial003.py
@@ -1,6 +1,4 @@
-import pytest
 from fastapi.testclient import TestClient
-from fastapi.websockets import WebSocketDisconnect
 
 from docs_src.websockets.tutorial003 import app
 
@@ -11,6 +9,14 @@ def test_websocket_handle_disconnection():
     with client.websocket_connect("/ws/1234") as connection, client.websocket_connect(
         "/ws/5678"
     ) as connection_two:
+        connection.send_text("Hello from 1234")
+        data1 = connection.receive_text()
+        assert data1 == "You wrote: Hello from 1234"
+        data2 = connection_two.receive_text()
+        client1_says = "Client #1234 says: Hello from 1234"
+        assert data2 == client1_says
+        data1 = connection.receive_text()
+        assert data1 == client1_says
         connection_two.close()
-        data = connection.receive_text()
-        assert data == f"Client #5678 left the chat."
+        data1 = connection.receive_text()
+        assert data1 == "Client #5678 left the chat"
