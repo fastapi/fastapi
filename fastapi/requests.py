f5e665f4241432787e186aab9401de3743739c97
James Curtin <jacurtin@wayfair.com>
2020-08-10 10:15:16 -0400

    Import types used by Request properties

--- fastapi/requests.py
+++ fastapi/requests.py
@@ -1,2 +1,9 @@
+from starlette.datastructures import URL  # noqa: F401
+from starlette.datastructures import Address  # noqa: F401
+from starlette.datastructures import FormData  # noqa: F401
+from starlette.datastructures import Headers  # noqa: F401
+from starlette.datastructures import QueryParams  # noqa: F401
+from starlette.datastructures import State  # noqa: F401
 from starlette.requests import HTTPConnection as HTTPConnection  # noqa: F401
 from starlette.requests import Request as Request  # noqa: F401
+from starlette.types import Receive  # noqa: F401
