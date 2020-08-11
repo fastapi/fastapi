2202dc63203f58d455e46d4319e6be8323ce0b06
James Curtin <jacurtin@wayfair.com>
2020-08-11 09:24:16 -0400

    Remove repeated import

--- fastapi/requests.py
+++ fastapi/requests.py
@@ -1,3 +1,2 @@
 from starlette.requests import HTTPConnection as HTTPConnection  # noqa: F401
 from starlette.requests import Request as Request  # noqa: F401
-from starlette.types import Receive  # noqa: F401
