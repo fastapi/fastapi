42e8e73c0099229fc5aaf7625307dd6d1c69ef49
Marcelo Trylesinski <marcelotryle@gmail.com>
2020-08-16 22:56:50 +0200

    fix more >:)

---
+++ tests/test_tutorial/test_testing/test_main_b.py
@@ -0,0 +1,6 @@
+from docs_src.app_testing.test_main_b import test_create_existing_item  # noqa: F401
+from docs_src.app_testing.test_main_b import test_create_item  # noqa: F401
+from docs_src.app_testing.test_main_b import test_create_item_bad_token  # noqa: F401
+from docs_src.app_testing.test_main_b import test_read_inexistent_item  # noqa: F401
+from docs_src.app_testing.test_main_b import test_read_item  # noqa: F401
+from docs_src.app_testing.test_main_b import test_read_item_bad_token  # noqa: F401
