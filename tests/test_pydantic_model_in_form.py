5affe1813568f41f16681a7d5d6c566e825072f4
Vobinics <vobinics@gmail.com>
2020-09-17 16:13:46 +0300

    Added the ability to transfer Pydantic model to FormData

---
+++ tests/test_pydantic_model_in_form.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from starlette.testclient import TestClient
+
+
+class Message(BaseModel):
+    msg: str
+
+
+app = FastAPI()
+
+
+@app.post("/new_message", status_code=201)
+def new_entry(message: Message = Form(...)):
+    return message
+
+
+client = TestClient(app)
+
+
+def test_new_entry():
+    new_message = Message(msg="Fast API is the best")
+    resp = client.post("/new_message", data={"message": new_message.json()})
+
+    assert resp.status_code == 201, resp.json()
+    assert resp.json() == new_message.dict()
