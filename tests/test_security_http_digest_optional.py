cf9a1e27e54e412244217f20291d6994073aaf4a
Grzegorz Jaworski <gostekk94@gmail.com>
2020-10-02 17:40:33 +0200

    Updated security scheme tests to match changed status codes

--- tests/test_security_http_digest_optional.py
+++ tests/test_security_http_digest_optional.py
@@ -66,5 +66,5 @@ def test_security_http_digest_incorrect_scheme_credentials():
     response = client.get(
         "/users/me", headers={"Authorization": "Other invalidauthorization"}
     )
-    assert response.status_code == 403, response.text
+    assert response.status_code == 401, response.text
     assert response.json() == {"detail": "Invalid authentication credentials"}
