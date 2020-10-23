a613184247f5707003060a6e20a9e430a795be4a
NoahCardoza <noahcardoza@gmail.com>
2020-10-23 03:38:06 -0700

    fixed aliases type def

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -44,7 +44,7 @@ def create_response_field(
     model_config: Type[BaseConfig] = BaseConfig,
     field_info: Optional[FieldInfo] = None,
     alias: Optional[str] = None,
-    aliases: Optional[Tuple[str]] = None,
+    aliases: Optional[Tuple[str, ...]] = None,
 ) -> MultiAliasablModelField:
     """
     Create a new response field. Raises if type_ is invalid.
