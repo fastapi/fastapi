8e686521d56b49457c44ac26dec3ebf803fce484
Leynier Gutiérrez González <leynier41@gmail.com>
2020-11-11 01:39:24 -0500

    Fix not use of ACCESS_TOKEN_EXPIRE_MINUTES
    Fix for issue: #2343

--- docs_src/security/tutorial004.py
+++ docs_src/security/tutorial004.py
@@ -80,7 +80,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
