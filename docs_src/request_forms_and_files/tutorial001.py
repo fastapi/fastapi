ed918e4b1f1b88b03b5ad56da96a7fb36085f0ab
Tricosmo <tricosmo@Tricosmos-MacBook-Pro.local>
2020-11-27 21:39:09 +0000

    Update request_form_files tutorial to cover multipart/form-data with object formfield

--- docs_src/request_forms_and_files/tutorial001.py
+++ docs_src/request_forms_and_files/tutorial001.py
@@ -1,14 +1,33 @@
+from typing import List
+
 from fastapi import FastAPI, File, Form, UploadFile
+from pydantic import BaseModel
+
+
+class Address(BaseModel):
+    first_line: str
+    postcode: str
+
+
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    address: List[Address]
+
 
 app = FastAPI()
 
 
 @app.post("/files/")
 async def create_file(
-    file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)
+    file: bytes = File(...),
+    fileb: UploadFile = File(...),
+    token: str = Form(...),
+    user: User = Form(...),
 ):
     return {
         "file_size": len(file),
         "token": token,
         "fileb_content_type": fileb.content_type,
+        "user": user,
     }
--- tests/test_tutorial/test_request_forms_and_files/test_tutorial001.py
+++ tests/test_tutorial/test_request_forms_and_files/test_tutorial001.py
@@ -1,6 +1,8 @@
+import json
 import os
 from pathlib import Path
 
+import pytest
 from fastapi.testclient import TestClient
 
 from docs_src.request_forms_and_files.tutorial001 import app
@@ -13,6 +15,18 @@ openapi_schema = {
     "paths": {
         "/files/": {
             "post": {
+                "summary": "Create File",
+                "operationId": "create_file_files__post",
+                "requestBody": {
+                    "content": {
+                        "multipart/form-data": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Body_create_file_files__post"
+                            }
+                        }
+                    },
+                    "required": True,
+                },
                 "responses": {
                     "200": {
                         "description": "Successful Response",
@@ -29,31 +43,54 @@ openapi_schema = {
                         },
                     },
                 },
-                "summary": "Create File",
-                "operationId": "create_file_files__post",
-                "requestBody": {
-                    "content": {
-                        "multipart/form-data": {
-                            "schema": {
-                                "$ref": "#/components/schemas/Body_create_file_files__post"
-                            }
-                        }
-                    },
-                    "required": True,
-                },
             }
         }
     },
     "components": {
         "schemas": {
+            "Address": {
+                "title": "Address",
+                "required": ["first_line", "postcode"],
+                "type": "object",
+                "properties": {
+                    "first_line": {"title": "First Line", "type": "string"},
+                    "postcode": {"title": "Postcode", "type": "string"},
+                },
+            },
             "Body_create_file_files__post": {
                 "title": "Body_create_file_files__post",
-                "required": ["file", "fileb", "token"],
+                "required": ["file", "fileb", "token", "user"],
                 "type": "object",
                 "properties": {
                     "file": {"title": "File", "type": "string", "format": "binary"},
                     "fileb": {"title": "Fileb", "type": "string", "format": "binary"},
                     "token": {"title": "Token", "type": "string"},
+                    "user": {"$ref": "#/components/schemas/User"},
+                },
+            },
+            "HTTPValidationError": {
+                "title": "HTTPValidationError",
+                "type": "object",
+                "properties": {
+                    "detail": {
+                        "title": "Detail",
+                        "type": "array",
+                        "items": {"$ref": "#/components/schemas/ValidationError"},
+                    }
+                },
+            },
+            "User": {
+                "title": "User",
+                "required": ["first_name", "last_name", "address"],
+                "type": "object",
+                "properties": {
+                    "first_name": {"title": "First Name", "type": "string"},
+                    "last_name": {"title": "Last Name", "type": "string"},
+                    "address": {
+                        "title": "Address",
+                        "type": "array",
+                        "items": {"$ref": "#/components/schemas/Address"},
+                    },
                 },
             },
             "ValidationError": {
@@ -70,17 +107,6 @@ openapi_schema = {
                     "type": {"title": "Error Type", "type": "string"},
                 },
             },
-            "HTTPValidationError": {
-                "title": "HTTPValidationError",
-                "type": "object",
-                "properties": {
-                    "detail": {
-                        "title": "Detail",
-                        "type": "array",
-                        "items": {"$ref": "#/components/schemas/ValidationError"},
-                    }
-                },
-            },
         }
     },
 }
@@ -93,74 +119,56 @@ def test_openapi_schema():
 
 
 file_required = {
-    "detail": [
-        {
-            "loc": ["body", "file"],
-            "msg": "field required",
-            "type": "value_error.missing",
-        },
-        {
-            "loc": ["body", "fileb"],
-            "msg": "field required",
-            "type": "value_error.missing",
-        },
-    ]
+    "loc": ["body", "file"],
+    "msg": "field required",
+    "type": "value_error.missing",
 }
-
+fileb_required = {
+    "loc": ["body", "fileb"],
+    "msg": "field required",
+    "type": "value_error.missing",
+}
 token_required = {
-    "detail": [
-        {
-            "loc": ["body", "fileb"],
-            "msg": "field required",
-            "type": "value_error.missing",
-        },
-        {
-            "loc": ["body", "token"],
-            "msg": "field required",
-            "type": "value_error.missing",
-        },
-    ]
+    "loc": ["body", "token"],
+    "msg": "field required",
+    "type": "value_error.missing",
 }
-
-# {'detail': [, {'loc': ['body', 'token'], 'msg': 'field required', 'type': 'value_error.missing'}]}
-
-file_and_token_required = {
-    "detail": [
-        {
-            "loc": ["body", "file"],
-            "msg": "field required",
-            "type": "value_error.missing",
-        },
-        {
-            "loc": ["body", "fileb"],
-            "msg": "field required",
-            "type": "value_error.missing",
-        },
-        {
-            "loc": ["body", "token"],
-            "msg": "field required",
-            "type": "value_error.missing",
-        },
-    ]
+user_required = {
+    "loc": ["body", "user"],
+    "msg": "field required",
+    "type": "value_error.missing",
+}
+test_user = {
+    "first_name": "Bob",
+    "last_name": "England",
+    "address": [{"first_line": "1 Buckingham Ave", "postcode": "E1 0RR"}],
+}
+test_user_string = json.dumps(test_user)
+files_required_detail = {"detail": [file_required, fileb_required]}
+fileb_token_required_detail = {"detail": [fileb_required, token_required]}
+all_required = {
+    "detail": [file_required, fileb_required, token_required, user_required]
 }
 
 
 def test_post_form_no_body():
     response = client.post("/files/")
     assert response.status_code == 422, response.text
-    assert response.json() == file_and_token_required
+    assert response.json() == all_required
 
 
 def test_post_form_no_file():
-    response = client.post("/files/", data={"token": "foo"})
+    response = client.post("/files/", data={"token": "foo", "user": test_user_string})
     assert response.status_code == 422, response.text
-    assert response.json() == file_required
+    assert response.json() == files_required_detail
 
 
 def test_post_body_json():
-    response = client.post("/files/", json={"file": "Foo", "token": "Bar"})
+    response = client.post(
+        "/files/", json={"file": "Foo", "token": "Bar", "user": test_user_string}
+    )
     assert response.status_code == 422, response.text
-    assert response.json() == file_and_token_required
+    assert response.json() == all_required
 
 
 def test_post_file_no_token(tmpdir):
@@ -169,12 +177,52 @@ def test_post_file_no_token(tmpdir):
         file.write(b"<file content>")
 
     client = TestClient(app)
-    response = client.post("/files/", files={"file": open(path, "rb")})
+    response = client.post(
+        "/files/", files={"file": open(path, "rb")}, data={"user": test_user_string}
+    )
     assert response.status_code == 422, response.text
-    assert response.json() == token_required
+    assert response.json() == fileb_token_required_detail
 
 
-def test_post_files_and_token(tmpdir):
+@pytest.mark.parametrize(
+    "user_string,status_code,expected",
+    [
+        (
+            test_user_string,
+            200,
+            {
+                "file_size": 14,
+                "token": "foo",
+                "user": test_user,
+                "fileb_content_type": "text/plain",
+            },
+        ),
+        (
+            "{}",
+            422,
+            {
+                "detail": [
+                    {
+                        "loc": ["body", "user", "first_name"],
+                        "msg": "field required",
+                        "type": "value_error.missing",
+                    },
+                    {
+                        "loc": ["body", "user", "last_name"],
+                        "msg": "field required",
+                        "type": "value_error.missing",
+                    },
+                    {
+                        "loc": ["body", "user", "address"],
+                        "msg": "field required",
+                        "type": "value_error.missing",
+                    },
+                ],
+            },
+        ),
+    ],
+)
+def test_post_files_and_token_user(tmpdir, user_string, status_code, expected):
     patha = Path(tmpdir) / "test.txt"
     pathb = Path(tmpdir) / "testb.txt"
     patha.write_text("<file content>")
@@ -183,15 +231,11 @@ def test_post_files_and_token(tmpdir):
     client = TestClient(app)
     response = client.post(
         "/files/",
-        data={"token": "foo"},
+        data={"token": "foo", "user": user_string},
         files={
             "file": patha.open("rb"),
             "fileb": ("testb.txt", pathb.open("rb"), "text/plain"),
         },
     )
-    assert response.status_code == 200, response.text
-    assert response.json() == {
-        "file_size": 14,
-        "token": "foo",
-        "fileb_content_type": "text/plain",
-    }
+    assert response.status_code == status_code, response.text
+    assert response.json() == expected
