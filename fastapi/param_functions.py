5f6b3e4bbad744f3bc91e7c290b28e6070000612
kernc <kerncece@gmail.com>
2020-12-05 23:48:16 +0100

    black ... :roll_eyes:

--- fastapi/param_functions.py
+++ fastapi/param_functions.py
@@ -3,7 +3,7 @@ from typing import Any, Callable, Optional, Sequence, TypeVar
 from fastapi import params
 
 
-_T = TypeVar('_T')
+_T = TypeVar("_T")
 
 
 def Path(  # noqa: N802
@@ -254,7 +254,5 @@ def Security(  # noqa: N802
     use_cache: bool = True,
 ) -> _T:
     return params.Security(  # type: ignore
-        dependency=dependency,
-        scopes=scopes,
-        use_cache=use_cache,
+        dependency=dependency, scopes=scopes, use_cache=use_cache,
     )
