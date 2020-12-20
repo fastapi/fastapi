a9082fd17e3478c53a10d47298f66ff9add72089
Sebastián Ramírez <tiangolo@gmail.com>
2020-12-20 18:12:39 +0100

    ✨ Re-export variables to support mypy strict

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -2,24 +2,23 @@
 
 __version__ = "0.62.0"
 
-from starlette import status
+from starlette import status as status
 
-from .applications import FastAPI
-from .background import BackgroundTasks
-from .datastructures import UploadFile
-from .exceptions import HTTPException
-from .param_functions import (
-    Body,
-    Cookie,
-    Depends,
-    File,
-    Form,
-    Header,
-    Path,
-    Query,
-    Security,
-)
-from .requests import Request
-from .responses import Response
-from .routing import APIRouter
-from .websockets import WebSocket, WebSocketDisconnect
+from .applications import FastAPI as FastAPI
+from .background import BackgroundTasks as BackgroundTasks
+from .datastructures import UploadFile as UploadFile
+from .exceptions import HTTPException as HTTPException
+from .param_functions import Body as Body
+from .param_functions import Cookie as Cookie
+from .param_functions import Depends as Depends
+from .param_functions import File as File
+from .param_functions import Form as Form
+from .param_functions import Header as Header
+from .param_functions import Path as Path
+from .param_functions import Query as Query
+from .param_functions import Security as Security
+from .requests import Request as Request
+from .responses import Response as Response
+from .routing import APIRouter as APIRouter
+from .websockets import WebSocket as WebSocket
+from .websockets import WebSocketDisconnect as WebSocketDisconnect
