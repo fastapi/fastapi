e1efece8cc21caa645ce4fe9e76ba503d570fff0
Greg Gandenberger <ggandenberger@shoprunner.com>
2021-02-11 12:30:47 -0600

    Update tutorial004.py
    Use `set()` instead of `[]` as default value for set field.
    
    The code seems to run fine as it is, but `mypy` complains.

--- docs_src/body_nested_models/tutorial004.py
+++ docs_src/body_nested_models/tutorial004.py
@@ -16,7 +16,7 @@ class Item(BaseModel):
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
-    tags: Set[str] = []
+    tags: Set[str] = set()
     image: Optional[Image] = None
 
 
