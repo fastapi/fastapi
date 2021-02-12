50d7072aa0428efc4299f324ebd45c1b388d2013
Vinay Karanam <vinayinvicible@gmail.com>
2021-02-12 21:14:28 +0530

    Fixed python 3.8.7 compatibility
    * Upgraded starlette to 0.14.2
    * Added UJSONResponse into the project

--- fastapi/responses.py
+++ fastapi/responses.py
@@ -7,12 +7,16 @@ from starlette.responses import PlainTextResponse as PlainTextResponse  # noqa
 from starlette.responses import RedirectResponse as RedirectResponse  # noqa
 from starlette.responses import Response as Response  # noqa
 from starlette.responses import StreamingResponse as StreamingResponse  # noqa
-from starlette.responses import UJSONResponse as UJSONResponse  # noqa
 
 try:
     import orjson
 except ImportError:  # pragma: nocover
     orjson = None  # type: ignore
+
+try:
+    import ujson
+except ImportError:  # pragma: nocover
+    ujson = None  # type: ignore
 
 
 class ORJSONResponse(JSONResponse):
@@ -21,3 +25,11 @@ class ORJSONResponse(JSONResponse):
     def render(self, content: Any) -> bytes:
         assert orjson is not None, "orjson must be installed to use ORJSONResponse"
         return orjson.dumps(content)
+
+
+class UJSONResponse(JSONResponse):
+    media_type = "application/json"
+
+    def render(self, content: Any) -> bytes:
+        assert ujson is not None, "ujson must be installed to use UJSONResponse"
+        return ujson.dumps(content, ensure_ascii=False).encode("utf-8")
