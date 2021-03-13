4620732d9e0f8e40ecfd4a45ced320d33e096202
Jordan Speicher <jordan@jspeicher.com>
2021-03-12 22:57:08 -0600

    Add tests for lifespan context and refactor router event tests

--- tests/test_router_events.py
+++ tests/test_router_events.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, FastAPI
 from fastapi.testclient import TestClient
 from pydantic import BaseModel
+import pytest
 
 
 class State(BaseModel):
@@ -12,57 +13,53 @@ class State(BaseModel):
     sub_router_shutdown: bool = False
 
 
-state = State()
-
-app = FastAPI()
-
-
-@app.on_event("startup")
-def app_startup():
-    state.app_startup = True
+@pytest.fixture
+def state():
+    return State()
 
 
-@app.on_event("shutdown")
-def app_shutdown():
-    state.app_shutdown = True
+def test_router_events(state):
+    app = FastAPI()
 
+    @app.get("/")
+    def main():
+        return {"message": "Hello World"}
 
-router = APIRouter()
+    @app.on_event("startup")
+    def app_startup():
+        state.app_startup = True
 
+    @app.on_event("shutdown")
+    def app_shutdown():
+        state.app_shutdown = True
 
-@router.on_event("startup")
-def router_startup():
-    state.router_startup = True
+    router = APIRouter()
 
+    @router.on_event("startup")
+    def router_startup():
+        state.router_startup = True
 
-@router.on_event("shutdown")
-def router_shutdown():
-    state.router_shutdown = True
+    @router.on_event("shutdown")
+    def router_shutdown():
+        state.router_shutdown = True
 
+    sub_router = APIRouter()
 
-sub_router = APIRouter()
+    @sub_router.on_event("startup")
+    def sub_router_startup():
+        state.sub_router_startup = True
 
+    @sub_router.on_event("shutdown")
+    def sub_router_shutdown():
+        state.sub_router_shutdown = True
 
-@sub_router.on_event("startup")
-def sub_router_startup():
-    state.sub_router_startup = True
+    @sub_router.get("/")
+    def main():
+        return {"message": "Hello World"}
 
+    router.include_router(sub_router)
+    app.include_router(router)
 
-@sub_router.on_event("shutdown")
-def sub_router_shutdown():
-    state.sub_router_shutdown = True
-
-
-@sub_router.get("/")
-def main():
-    return {"message": "Hello World"}
-
-
-router.include_router(sub_router)
-app.include_router(router)
-
-
-def test_router_events():
     assert state.app_startup is False
     assert state.router_startup is False
     assert state.sub_router_startup is False
@@ -85,3 +82,27 @@ def test_router_events():
     assert state.app_shutdown is True
     assert state.router_shutdown is True
     assert state.sub_router_shutdown is True
+
+
+def test_app_lifespan_state(state):
+    def lifespan(app):
+        state.app_startup = True
+        yield
+        state.app_shutdown = True
+
+    app = FastAPI(lifespan=lifespan)
+
+    @app.get("/")
+    def main():
+        return {"message": "Hello World"}
+
+    assert state.app_startup is False
+    assert state.app_shutdown is False
+    with TestClient(app) as client:
+        assert state.app_startup is True
+        assert state.app_shutdown is False
+        response = client.get("/")
+        assert response.status_code == 200, response.text
+        assert response.json() == {"message": "Hello World"}
+    assert state.app_startup is True
+    assert state.app_shutdown is True
