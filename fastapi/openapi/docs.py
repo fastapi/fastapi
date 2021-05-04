0ff7c610be457a9b233d920b203eb49d825d4e0b
andriyunantoreza <reza.andriyunanto@narindo.com>
2021-05-04 20:59:03 +0800

    add defaultModelsExpandDepth to config show depth or not show schema models

--- fastapi/openapi/docs.py
+++ fastapi/openapi/docs.py
@@ -14,6 +14,7 @@ def get_swagger_ui_html(
     swagger_favicon_url: str = "https://fastapi.tiangolo.com/img/favicon.png",
     oauth2_redirect_url: Optional[str] = None,
     init_oauth: Optional[Dict[str, Any]] = None,
+    defaultModelsExpandDepth: int = 1,
 ) -> HTMLResponse:
 
     html = f"""
@@ -32,6 +33,7 @@ def get_swagger_ui_html(
     <script>
     const ui = SwaggerUIBundle({{
         url: '{openapi_url}',
+        defaultModelsExpandDepth: {defaultModelsExpandDepth},
     """
 
     if oauth2_redirect_url:
