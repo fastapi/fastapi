f6aef1d40cae4873ce800af9b1ba769aa4c470c9
Sebastián Ramírez <tiangolo@gmail.com>
2021-07-19 19:42:10 +0200

    📝 Update docs for custom StreamingResponse
    with new fixes using a with open() block

--- docs_src/custom_response/tutorial008.py
+++ docs_src/custom_response/tutorial008.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 def main():
-    def iterfile():
-        with open(some_file_path, mode="rb") as file_like:
-            yield from file_like
+    def iterfile():  # (1)
+        with open(some_file_path, mode="rb") as file_like:  # (2)
+            yield from file_like  # (3)
 
     return StreamingResponse(iterfile(), media_type="video/mp4")
