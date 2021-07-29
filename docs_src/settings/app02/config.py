894e91b2ecffec800714fb31d0c387e7f156fd0a
Sebastián Ramírez <tiangolo@gmail.com>
2021-07-29 10:51:25 +0200

    📝 Tweak source examples for consistency and minimize changes

--- docs_src/settings/app02/config.py
+++ docs_src/settings/app02/config.py
@@ -3,5 +3,5 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
-    admin_email: str = "admin@example.com"
+    admin_email: str
     items_per_user: int = 50
--- docs_src/settings/app02/test_main.py
+++ docs_src/settings/app02/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 
 def get_settings_override():
-    return Settings(admin_email="another_admin@example.com")
+    return Settings(admin_email="testing_admin@example.com")
 
 
 app.dependency_overrides[get_settings] = get_settings_override
@@ -15,8 +15,9 @@ app.dependency_overrides[get_settings] = get_settings_override
 
 def test_app():
     response = client.get("/info")
-    assert response.json() == {
+    data = response.json()
+    assert data == {
         "app_name": "Awesome API",
-        "admin_email": "another_admin@example.com",
+        "admin_email": "testing_admin@example.com",
         "items_per_user": 50,
     }
