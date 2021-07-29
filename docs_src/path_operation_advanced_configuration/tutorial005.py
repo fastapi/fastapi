536c48d49de740c37adcb2633bd6bf138050a16e
Sebastián Ramírez <tiangolo@gmail.com>
2021-07-29 21:53:39 +0200

    📝 Add additional source examples for extending the OpenAPI schema

--- docs_src/path_operation_advanced_configuration/tutorial005.py
+++ docs_src/path_operation_advanced_configuration/tutorial005.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/", **{"x-my-open-api-extension": "value"})
+@app.get("/items/", openapi_extra={"x-aperture-labs-portal": "blue"})
 async def read_items():
-    return [{"item_id": "Foo"}]
+    return [{"item_id": "portal-gun"}]
