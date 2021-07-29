297ef3858ad1daafa2a5c04c21424870e11b0d5f
Sebastián Ramírez <tiangolo@gmail.com>
2021-07-29 16:58:08 +0200

    ✅ Update tests for OpenAPI metadata

--- tests/test_tutorial/test_metadata/test_tutorial001.py
+++ tests/test_tutorial/test_metadata/test_tutorial001.py
@@ -7,31 +7,31 @@ client = TestClient(app)
 openapi_schema = {
     "openapi": "3.0.2",
     "info": {
-        "title": "My API",
-        "version": "0.0.1",
-        "description": "This is my API, with auto docs for the API and everything",
+        "title": "ChimichangApp",
+        "description": "\nChimichangApp API helps you do awesome stuff. 🚀\n\n## Items\n\nYou can **read items**.\n\n## Users\n\nYou will be able to:\n\n* **Create users** (_not implemented_).\n* **Read users** (_not implemented_).\n",
         "termsOfService": "http://example.com/terms/",
         "contact": {
-            "name": "John Doe",
-            "url": "http://example.com/contact/",
-            "email": "johndoe@example.com",
+            "name": "Deadpoolio the Amazing",
+            "url": "http://x-force.example.com/contact/",
+            "email": "dp@x-force.example.com",
         },
         "license": {
             "name": "Apache 2.0",
             "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
         },
+        "version": "0.0.1",
     },
     "paths": {
         "/items/": {
             "get": {
+                "summary": "Read Items",
+                "operationId": "read_items_items__get",
                 "responses": {
                     "200": {
                         "description": "Successful Response",
                         "content": {"application/json": {"schema": {}}},
                     }
                 },
-                "summary": "Read Items",
-                "operationId": "read_items_items__get",
             }
         }
     },
@@ -47,4 +47,4 @@ def test_openapi_schema():
 def test_items():
     response = client.get("/items/")
     assert response.status_code == 200, response.text
-    assert response.json() == [{"name": "Foo"}]
+    assert response.json() == [{"name": "Katana"}]
