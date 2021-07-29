5e15af14f7a9cc2d0105d545cb4b54cd6900f95d
Sebastián Ramírez <tiangolo@gmail.com>
2021-07-29 17:22:47 +0200

    🎨 Update import style to improve typing support

--- fastapi/datastructures.py
+++ fastapi/datastructures.py
@@ -1,11 +1,11 @@
 from typing import Any, Callable, Iterable, Type
 
-from starlette.datastructures import URL  # noqa: F401
-from starlette.datastructures import Address  # noqa: F401
-from starlette.datastructures import FormData  # noqa: F401
-from starlette.datastructures import Headers  # noqa: F401
-from starlette.datastructures import QueryParams  # noqa: F401
-from starlette.datastructures import State  # noqa: F401
+from starlette.datastructures import URL as URL  # noqa: F401
+from starlette.datastructures import Address as Address  # noqa: F401
+from starlette.datastructures import FormData as FormData  # noqa: F401
+from starlette.datastructures import Headers as Headers  # noqa: F401
+from starlette.datastructures import QueryParams as QueryParams  # noqa: F401
+from starlette.datastructures import State as State  # noqa: F401
 from starlette.datastructures import UploadFile as StarletteUploadFile
 
 
