4eada92883e97ea3d01ce716abfa34079c691d84
James Curtin <jameswcurtin@gmail.com>
2021-07-29 11:30:18 -0400

    ✨ Import and re-export data structures from Starlette, used by Request properties, on `fastapi.datastructures` (#1872)
    Co-authored-by: Sebastián Ramírez <tiangolo@gmail.com>

--- fastapi/datastructures.py
+++ fastapi/datastructures.py
@@ -1,5 +1,10 @@
 from typing import Any, Callable, Iterable, Type, TypeVar
 
+from starlette.datastructures import URL as URL  # noqa: F401
+from starlette.datastructures import Address as Address  # noqa: F401
+from starlette.datastructures import FormData as FormData  # noqa: F401
+from starlette.datastructures import Headers as Headers  # noqa: F401
+from starlette.datastructures import QueryParams as QueryParams  # noqa: F401
 from starlette.datastructures import State as State  # noqa: F401
 from starlette.datastructures import UploadFile as StarletteUploadFile
 
