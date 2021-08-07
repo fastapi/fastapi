61183c1018891017b65e6e74c86a2bc260967994
Yasser Tahiri <yasserth19@gmail.com>
2021-08-07 11:45:52 +0100

    🛠 Replace `dict()` with `{}`
    - Replace dict() with {}
    - Use items() to directly unpack dictionary values

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -76,7 +76,7 @@ def create_cloned_field(
 ) -> ModelField:
     # _cloned_types has already cloned types, to support recursive models
     if cloned_types is None:
-        cloned_types = dict()
+        cloned_types = {}
     original_type = field.type_
     if is_dataclass(original_type) and hasattr(original_type, "__pydantic_model__"):
         original_type = original_type.__pydantic_model__
@@ -127,13 +127,13 @@ def generate_operation_id_for_path(*, name: str, path: str, method: str) -> str:
 
 
 def deep_dict_update(main_dict: Dict[Any, Any], update_dict: Dict[Any, Any]) -> None:
-    for key in update_dict:
+    for key, value in update_dict.items():
         if (
             key in main_dict
             and isinstance(main_dict[key], dict)
             and isinstance(update_dict[key], dict)
         ):
-            deep_dict_update(main_dict[key], update_dict[key])
+            deep_dict_update(main_dict[key], value)
         else:
             main_dict[key] = update_dict[key]
 
