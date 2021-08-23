3196aca0f151c3419c5a32a38e8bf1805be0b095
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-08-23 19:58:10 +0300

    🔧 enable ansi in cmd

--- fastapi/logger.py
+++ fastapi/logger.py
@@ -1,3 +1,7 @@
 import logging
 
 logger = logging.getLogger("fastapi")
+
+if platform.system() == "Windows":
+    kernel32 = ctypes.windll.kernel32
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
