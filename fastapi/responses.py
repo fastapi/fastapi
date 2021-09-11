d81ea3a1d137cc8cc5eab2c348c64cd828da9032
abc.zxy <zxnzysj@163.com>
2021-09-11 16:56:21 +0800

    Update orjson support non str keys

--- fastapi/responses.py
+++ fastapi/responses.py
@@ -31,4 +31,4 @@ class ORJSONResponse(JSONResponse):
 
     def render(self, content: Any) -> bytes:
         assert orjson is not None, "orjson must be installed to use ORJSONResponse"
-        return orjson.dumps(content)
+        return orjson.dumps(content, option=orjson.OPT_NON_STR_KEYS)
