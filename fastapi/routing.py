e66406a36905d16475dd06ad411919c34180f172
Andrey Proskurnev <andrey@proskurnev.ru>
2021-09-15 16:00:13 +0300

    Remove unused function

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -148,19 +148,6 @@ async def serialize_response(
         return jsonable_encoder(response_content)
 
 
-async def run_endpoint_function(
-    *, dependant: Dependant, values: Dict[str, Any], is_coroutine: bool
-) -> Any:
-    # Only called by get_request_handler. Has been split into its own function to
-    # facilitate profiling endpoints, since inner functions are harder to profile.
-    assert dependant.call is not None, "dependant.call must be a function"
-
-    if is_coroutine:
-        return await dependant.call(**values)
-    else:
-        return await run_in_threadpool(dependant.call, **values)
-
-
 def get_request_handler(
     dependant: Dependant,
     body_field: Optional[ModelField] = None,
