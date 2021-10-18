227145b01daa146059e8d7ffa1c07d08b8cb57c1
Huon Wilson <huon@exoflare.io>
2021-10-18 16:47:52 +1100

    Cache cloning of pydantic types globally for 2+x init speed-up
    If `response_model=...` is specified in a route handler, FastAPI
    creates a copy of the type to avoid accidental leakage of
    secrets. This is done in a pure way, but is not cached. This means
    that that if there's models that occur several times (for instance,
    multiple routes that return the same model), identical work is done
    and re-done. This change adds a cache for the conversion: when
    converting a type that's already been seen, just use the cache.
    
    This dramatically improves the start-up time of the server, making for
    a nicer developer experience, and reducing cold-start times if run in
    FaaS/serverless platforms like AWS Lambda.
    
    For instance, we have an app with 40 routes, defined in the usual way
    with an `app = FastAPI(...)` in app.py and various routers across
    other files.
    
    I measured the impact on this change in two ways.
    
    - Just the time to initialise all the routes etc (command below):
    - The time to reach "Application startup complete" when actually
      running (`uvicorn.run(..., port=8000, reload=True, debug=True,
      workers=1)`)
    
    | measurement    | 0.70.0 (s) | this patch (s) |
    |----------------|-----------:|---------------:|
    | initialisation |       2.60 |           1.26 |
    | full startup   |       5.94 |           2.15 |
    
    Initialisation measurement command:
    
        python -c 'import time; start = time.perf_counter(); import ....app; print(time.perf_counter() - start)'

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -69,28 +69,22 @@ def create_response_field(
         )
 
 
-def create_cloned_field(
-    field: ModelField,
-    *,
-    cloned_types: Optional[Dict[Type[BaseModel], Type[BaseModel]]] = None,
-) -> ModelField:
-    # _cloned_types has already cloned types, to support recursive models
-    if cloned_types is None:
-        cloned_types = dict()
+_CLONED_TYPES_CACHE: Dict[Type[BaseModel], Type[BaseModel]] = dict()
+
+
+def create_cloned_field(field: ModelField) -> ModelField:
     original_type = field.type_
     if is_dataclass(original_type) and hasattr(original_type, "__pydantic_model__"):
         original_type = original_type.__pydantic_model__
     use_type = original_type
     if lenient_issubclass(original_type, BaseModel):
         original_type = cast(Type[BaseModel], original_type)
-        use_type = cloned_types.get(original_type)
+        use_type = _CLONED_TYPES_CACHE.get(original_type)
         if use_type is None:
             use_type = create_model(original_type.__name__, __base__=original_type)
-            cloned_types[original_type] = use_type
+            _CLONED_TYPES_CACHE[original_type] = use_type
             for f in original_type.__fields__.values():
-                use_type.__fields__[f.name] = create_cloned_field(
-                    f, cloned_types=cloned_types
-                )
+                use_type.__fields__[f.name] = create_cloned_field(f)
     new_field = create_response_field(name=field.name, type_=use_type)
     new_field.has_alias = field.has_alias
     new_field.alias = field.alias
@@ -103,13 +97,10 @@ def create_cloned_field(
     new_field.validate_always = field.validate_always
     if field.sub_fields:
         new_field.sub_fields = [
-            create_cloned_field(sub_field, cloned_types=cloned_types)
-            for sub_field in field.sub_fields
+            create_cloned_field(sub_field) for sub_field in field.sub_fields
         ]
     if field.key_field:
-        new_field.key_field = create_cloned_field(
-            field.key_field, cloned_types=cloned_types
-        )
+        new_field.key_field = create_cloned_field(field.key_field)
     new_field.validators = field.validators
     new_field.pre_validators = field.pre_validators
     new_field.post_validators = field.post_validators
