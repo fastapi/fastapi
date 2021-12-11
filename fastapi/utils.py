23ef44029854709dff7fbe912b36ada9db5d2c84
Ori Levari <orilevari97@gmail.com>
2021-12-10 18:33:29 -0800

    modify deep_dict_update to merge rather than overwrite arrays. This allows query parameters to be specified in openapi_extras not be appended to the spec, not overwrite.

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -134,6 +134,11 @@ def deep_dict_update(main_dict: Dict[Any, Any], update_dict: Dict[Any, Any]) -> 
             and isinstance(update_dict[key], dict)
         ):
             deep_dict_update(main_dict[key], update_dict[key])
+        elif (key in main_dict
+            and isinstance(main_dict[key], list)
+            and isinstance(update_dict[key], list)
+        ):
+            main_dict[key] = main_dict[key] + update_dict[key]
         else:
             main_dict[key] = update_dict[key]
 
