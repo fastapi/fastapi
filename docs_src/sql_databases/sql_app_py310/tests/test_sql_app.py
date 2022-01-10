8b8341402484438ee16b3cbadbc1e4fd8ed8a495
Maxim Martynov <martinov_m_s_@mail.ru>
2022-01-11 00:29:24 +0300

    Fix sql_app_py39 and py310 tests

--- docs_src/sql_databases/sql_app_py310/tests/test_sql_app.py
+++ docs_src/sql_databases/sql_app_py310/tests/test_sql_app.py
@@ -32,16 +32,16 @@ client = TestClient(app)
 def test_create_user():
     response = client.post(
         "/users/",
-        json={"email": "deadpool@example.com", "password": "chimichangas4life"},
+        json={"email": "deadpool310@example.com", "password": "chimichangas4life"},
     )
     assert response.status_code == 200, response.text
     data = response.json()
-    assert data["email"] == "deadpool@example.com"
+    assert data["email"] == "deadpool310@example.com"
     assert "id" in data
     user_id = data["id"]
 
     response = client.get(f"/users/{user_id}")
     assert response.status_code == 200, response.text
     data = response.json()
-    assert data["email"] == "deadpool@example.com"
+    assert data["email"] == "deadpool310@example.com"
     assert data["id"] == user_id
