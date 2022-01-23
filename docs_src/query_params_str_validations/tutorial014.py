78e743939d68f1f80b30ad711a957870531a0d93
Sebastián Ramírez <tiangolo@gmail.com>
2022-01-23 16:41:48 +0100

    📝 Update source example for include_in_schema

--- docs_src/query_params_str_validations/tutorial014.py
+++ docs_src/query_params_str_validations/tutorial014.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(hidden_query: str = Query(None, include_in_schema=False)):
+async def read_items(
+    hidden_query: Optional[str] = Query(None, include_in_schema=False)
+):
     if hidden_query:
         return {"hidden_query": hidden_query}
     else:
