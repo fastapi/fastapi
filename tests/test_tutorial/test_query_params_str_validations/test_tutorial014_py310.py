fcf3c1cda4625fb42e2f2a360057f663a4429ffa
Sebastián Ramírez <tiangolo@gmail.com>
2022-01-23 16:49:56 +0100

    ✅ Fix tests for include_in_schema for Python 3.10

--- tests/test_tutorial/test_query_params_str_validations/test_tutorial014_py310.py
+++ tests/test_tutorial/test_query_params_str_validations/test_tutorial014_py310.py
@@ -1,11 +1,7 @@
+import pytest
 from fastapi.testclient import TestClient
 
-from docs_src.query_params_str_validations.tutorial014_py310 import app
-
 from ...utils import needs_py310
-
-client = TestClient(app)
-
 
 openapi_schema = {
     "openapi": "3.0.2",
@@ -66,22 +62,30 @@ openapi_schema = {
 }
 
 
+@pytest.fixture(name="client")
+def get_client():
+    from docs_src.query_params_str_validations.tutorial014_py310 import app
+
+    client = TestClient(app)
+    return client
+
+
 @needs_py310
-def test_openapi_schema():
+def test_openapi_schema(client: TestClient):
     response = client.get("/openapi.json")
     assert response.status_code == 200, response.text
     assert response.json() == openapi_schema
 
 
 @needs_py310
-def test_hidden_query():
+def test_hidden_query(client: TestClient):
     response = client.get("/items?hidden_query=somevalue")
     assert response.status_code == 200, response.text
     assert response.json() == {"hidden_query": "somevalue"}
 
 
 @needs_py310
-def test_no_hidden_query():
+def test_no_hidden_query(client: TestClient):
     response = client.get("/items")
     assert response.status_code == 200, response.text
     assert response.json() == {"hidden_query": "Not found"}
