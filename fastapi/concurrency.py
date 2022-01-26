fb2fdf5d17afb2cd436594cc7e1c5d924b7ab5a9
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-01-26 17:50:14 +0100

    Remove unused Optional import

--- fastapi/concurrency.py
+++ fastapi/concurrency.py
@@ -1,5 +1,5 @@
 import sys
-from typing import AsyncGenerator, ContextManager, Optional, TypeVar
+from typing import AsyncGenerator, ContextManager, TypeVar
 
 from starlette.concurrency import iterate_in_threadpool as iterate_in_threadpool  # noqa
 from starlette.concurrency import run_in_threadpool as run_in_threadpool  # noqa
