4305a0c5f9bcd1ece36ed858960326bb06d0627a
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-01-27 20:33:01 +0100

    Remove HTTPException

--- fastapi/exceptions.py
+++ fastapi/exceptions.py
@@ -1,20 +1,8 @@
-from typing import Any, Dict, Optional, Sequence, Type
+from typing import Any, Sequence, Type
 
 from pydantic import BaseModel, ValidationError, create_model
 from pydantic.error_wrappers import ErrorList
-from starlette.exceptions import HTTPException as StarletteHTTPException
-
-
-class HTTPException(StarletteHTTPException):
-    def __init__(
-        self,
-        status_code: int,
-        detail: Any = None,
-        headers: Optional[Dict[str, Any]] = None,
-    ) -> None:
-        super().__init__(status_code=status_code, detail=detail)
-        self.headers = headers
-
+from starlette.exceptions import HTTPException as HTTPException
 
 RequestErrorModel: Type[BaseModel] = create_model("Request")
 WebSocketErrorModel: Type[BaseModel] = create_model("WebSocket")
