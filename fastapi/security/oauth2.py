75dbe9d0cd164bacb44719c9ca308a3e77c09eae
Sylvain MOUQUET <sylvain.mouquet@gmail.com>
2022-01-29 14:49:04 +0100

    feat(oauth2): add oauth2 client credentials bearer

--- fastapi/security/oauth2.py
+++ fastapi/security/oauth2.py
@@ -213,6 +213,44 @@ class OAuth2AuthorizationCodeBearer(OAuth2):
                 return None  # pragma: nocover
         return param
 
+class OAuth2ClientCredentialsBearer(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[Dict[str, str]] = None,
+        description: Optional[str] = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(
+            clientCredentials={
+                "tokenUrl": tokenUrl,
+            }
+        )
+        super().__init__(
+            flows=flows,
+            scheme_name=scheme_name,
+            description=description,
+            auto_error=auto_error,
+        )
+
+    
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None  # pragma: nocover
+        return param
+
 
 class SecurityScopes:
     def __init__(self, scopes: Optional[List[str]] = None):
