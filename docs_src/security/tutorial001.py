36fd9b4936d86f9dc5c223d8d89c5736ecfd243a
Ansh Agarwal <95248503+github4ansh@users.noreply.github.com>
2022-02-10 00:44:04 +0530

    Update tutorial001.py
    Dear team,
    Line "return {"token": token}" is giving misinterpretation that read_items path operation is responsible for returning the token. Instead it is endpoint defined in tokenUrl while creating instance of OAuth2PasswordBearer.

--- docs_src/security/tutorial001.py
+++ docs_src/security/tutorial001.py
@@ -8,4 +8,6 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/items/")
 async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+    items = {'apple', 'mango', 'orange'}
+    return {"items": items}
+
