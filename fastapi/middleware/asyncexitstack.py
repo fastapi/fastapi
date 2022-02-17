d33cde6711e03b82d926b164c7d1906fc19974ee
Sebastián Ramírez <tiangolo@gmail.com>
2022-02-17 12:34:52 +0100

    🐛 Fix AsyncExitStackMiddleware, bubble up raised exceptions
    after being handled by dependencies

--- fastapi/middleware/asyncexitstack.py
+++ fastapi/middleware/asyncexitstack.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi.concurrency import AsyncExitStack
 from starlette.types import ASGIApp, Receive, Scope, Send
 
@@ -9,8 +11,18 @@ class AsyncExitStackMiddleware:
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if AsyncExitStack:
+            dependency_exception: Optional[Exception] = None
             async with AsyncExitStack() as stack:
                 scope[self.context_name] = stack
-                await self.app(scope, receive, send)
+                try:
+                    await self.app(scope, receive, send)
+                except Exception as e:
+                    dependency_exception = e
+                    raise e
+            if dependency_exception:
+                # This exception was possibly handled by the dependency but it should
+                # still bubble up so that the ServerErrorMiddleware can return a 500
+                # or the ExceptionMiddleware can catch and handle any other exceptions
+                raise dependency_exception
         else:
             await self.app(scope, receive, send)  # pragma: no cover
