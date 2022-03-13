50cd8c0495db4e425436d02a329cbdbef9a2dfbf
Harun Yasar <harunyasar@outlook.com>
2022-03-13 21:29:57 +0000

    Remove redundant if condition

--- docs_src/query_params_str_validations/tutorial006.py
+++ docs_src/query_params_str_validations/tutorial006.py
@@ -5,7 +5,5 @@ app = FastAPI()
 
 @app.get("/items/")
 async def read_items(q: str = Query(..., min_length=3)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
+    results = {"q": q, "items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     return results
