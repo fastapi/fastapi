a3db3224ae8cc7d6d3c93b8a688d48cd333ca046
sobhan.saf79 <sobhan.saf79@gmail.com>
2022-03-25 19:00:05 +0430

    added and changed tests according to change in exclude_routes code

--- tests/test_exclude_routes.py
+++ tests/test_exclude_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 
-def test_delete_api_route_method_1():
+def test_exclude_routes_method_1():
     # normal flow
     router = APIRouter()
 
@@ -15,24 +15,22 @@ def test_delete_api_route_method_1():
 
     assert len(router.routes) == 2
 
-    res = router.delete_api_route("/foo")
-    assert res is True
-    assert len(router.routes) == 1
+    router.exclude_routes(["/foo"])
 
+    assert len(router.routes) == 1
     assert router.routes[0].path == "/bar"
 
 
-def test_delete_api_route_method_2():
+def test_exclude_routes_method_2():
     # a router with no route
     router = APIRouter()
 
-    res = router.delete_api_route("/foo")
+    router.exclude_routes(["/foo"])
 
-    assert res is False
     assert len(router.routes) == 0
 
 
-def test_delete_api_route_method_3():
+def test_exclude_routes_method_3():
     # deleting a path which does not exist
     router = APIRouter()
 
@@ -46,11 +44,85 @@ def test_delete_api_route_method_3():
 
     assert len(router.routes) == 2
 
-    res = router.delete_api_route("/path-not-exists")
-    assert res is False
+    router.exclude_routes(["/path-not-exists_1", "/path-not-exists_2"])
     assert len(router.routes) == 2
 
     path1, path2 = router.routes[0].path, router.routes[1].path
 
     assert "/bar" in (path1, path2)
     assert "/foo" in (path1, path2)
+
+
+def test_exclude_routes_method_4():
+    # two paths, one of them does not exist
+    router = APIRouter()
+
+    @router.get("/foo")
+    async def foo():
+        return {"msg": "Hello from foo"}
+
+    @router.post("/bar")
+    async def bar():
+        return {"msg": "Hello from bar"}
+
+    assert len(router.routes) == 2
+
+    router.exclude_routes(["/foo", "/path-not-exists"])
+    assert len(router.routes) == 1
+
+    assert "/bar" == router.routes[0].path
+
+
+def test_exclude_routes_method_5():
+    # deleting two existing paths
+    router = APIRouter()
+
+    @router.get("/foo")
+    async def foo():
+        return {"msg": "Hello from foo"}
+
+    @router.post("/bar")
+    async def bar():
+        return {"msg": "Hello from bar"}
+
+    assert len(router.routes) == 2
+
+    router.exclude_routes(["/foo", "/bar"])
+    assert len(router.routes) == 0
+
+
+def test_exclude_routes_method_6():
+    # no path to exclude
+    router = APIRouter()
+
+    @router.get("/foo")
+    async def foo():
+        return {"msg": "Hello from foo"}
+
+    @router.post("/bar")
+    async def bar():
+        return {"msg": "Hello from bar"}
+
+    assert len(router.routes) == 2
+
+    router.exclude_routes([])
+    assert len(router.routes) == 2
+
+
+def test_exclude_routes_method_7():
+    # the same path multiple times
+    router = APIRouter()
+
+    @router.get("/foo")
+    async def foo():
+        return {"msg": "Hello from foo"}
+
+    @router.post("/bar")
+    async def bar():
+        return {"msg": "Hello from bar"}
+
+    assert len(router.routes) == 2
+
+    router.exclude_routes(["/foo", "/foo", "/foo"])
+    assert len(router.routes) == 1
+    assert router.routes[0].path == "/bar"
