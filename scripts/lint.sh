543abcd1cdb57bea46fb92b741b7229cf2ed049c
avi <>
2022-04-26 01:42:29 -0700

    revert script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-env/bin/python -m mypy fastapi
-env/bin/python -m flake8 fastapi tests
-env/bin/python -m black fastapi tests --check
-env/bin/python -m isort fastapi tests docs_src scripts --check-only
+mypy fastapi
+flake8 fastapi tests
+black fastapi tests --check
+isort fastapi tests docs_src scripts --check-only
