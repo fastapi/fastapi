247a255ef8996f11c3cae71642ae3142d5cbebe9
Urjeet Patel <violentmagician.opensource@gmail.com>
2022-05-04 12:42:27 -0500

    dummy_verify deprecated in 1.7.1. Change this to use the new recommendation

--- docs_src/security/tutorial005_py310.py
+++ docs_src/security/tutorial005_py310.py
@@ -83,6 +83,7 @@ def get_user(db, username: str):
 def authenticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
     if not user:
+        verify_password(password, None)  # Prevent Timing Attacks
         return False
     if not verify_password(password, user.hashed_password):
         return False
