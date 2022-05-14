ca437cdfabe7673b838cf42889ba1c653acd2228
Sebastián Ramírez <tiangolo@gmail.com>
2022-05-14 06:59:59 -0500

    📝 Add docs recommending `Union` over `Optional` and migrate source examples (#4908)
    * 📝 Add docs recommending Union over Optional
    
    * 📝 Update docs recommending Union over Optional
    
    * 📝 Update source examples for docs, recommend Union over Optional
    
    * 📝 Update highlighted lines with updated source examples
    
    * 📝 Update highlighted lines in Markdown with recent code changes
    
    * 📝 Update docs, use Union instead of Optional
    
    * ♻️ Update source examples to recommend Union over Optional
    
    * 🎨 Update highlighted code in Markdown after moving from Optional to Union

--- docs_src/dependencies/tutorial002.py
+++ docs_src/dependencies/tutorial002.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Union
 
 from fastapi import Depends, FastAPI
 
@@ -9,7 +9,7 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 
 class CommonQueryParams:
-    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
+    def __init__(self, q: Union[str, None] = None, skip: int = 0, limit: int = 100):
         self.q = q
         self.skip = skip
         self.limit = limit
