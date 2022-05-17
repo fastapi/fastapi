6403957bd8170bd9712e18979613fc8dfb2327b3
Carlos Castro <nycker@gmail.com>
2022-05-17 12:33:06 -0500

    fixed missing argument in header
    Fixed TypeError: Header() missing 1 required positional argument: 'default'
    Added request as paramerer in read_items so that Headers can be read and processed

--- docs_src/dependencies/tutorial006.py
+++ docs_src/dependencies/tutorial006.py
@@ -1,19 +1,19 @@
-from fastapi import Depends, FastAPI, Header, HTTPException
+from fastapi import Depends, FastAPI, Header, Request, HTTPException
 
 app = FastAPI()
 
 
-async def verify_token(x_token: str = Header()):
+async def verify_token(x_token: str = Header(default=None)):
     if x_token != "fake-super-secret-token":
         raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-async def verify_key(x_key: str = Header()):
+async def verify_key(x_key: str = Header(default=None)):
     if x_key != "fake-super-secret-key":
         raise HTTPException(status_code=400, detail="X-Key header invalid")
     return x_key
 
 
 @app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
-async def read_items():
+async def read_items(request:Request):
     return [{"item": "Foo"}, {"item": "Bar"}]
