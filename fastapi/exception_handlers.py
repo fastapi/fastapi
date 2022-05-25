b2d5b28b96b98f3a9767bbf3e095d4dae1fcb7c7
ben avrahami <ben.avrahami@biocatch.com>
2022-05-25 15:20:53 +0300

    fixed 204/304 HTTPException handling

--- fastapi/exception_handlers.py
+++ fastapi/exception_handlers.py
@@ -1,19 +1,29 @@
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
+from starlette.datastructures import MutableHeaders
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 
 async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
     headers = getattr(exc, "headers", None)
-    if headers:
-        return JSONResponse(
-            {"detail": exc.detail}, status_code=exc.status_code, headers=headers
-        )
+    if headers is not None:
+        headers = MutableHeaders(headers)
+        # we need to make sure that "content-length" and "content-type" are not taken from the exception,
+        # otherwise we'll have a mismatch between the headers sent and the message
+        del headers["content-length"]
+        del headers["content-type"]
+
+    if exc.status_code in {204, 304}:
+        content = None
+        response_cls = Response
     else:
-        return JSONResponse({"detail": exc.detail}, status_code=exc.status_code)
+        content = {"detail": exc.detail}
+        response_cls = JSONResponse
+
+    return response_cls(content, status_code=exc.status_code, headers=headers)
 
 
 async def request_validation_exception_handler(
