7961eed744c23380a8e5012739335f47b54f807a
Тарасов СЕРГЕЙ <104857185+tsv19su254052@users.noreply.github.com>
2022-06-09 19:59:34 +0300

    Update exception_handlers.py
    corrected indents and hyphens

--- fastapi/exception_handlers.py
+++ fastapi/exception_handlers.py
@@ -9,17 +9,11 @@ from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
     headers = getattr(exc, "headers", None)
     if headers:
-        return JSONResponse(
-            {"detail": exc.detail}, status_code=exc.status_code, headers=headers
-        )
+        return JSONResponse({"detail": exc.detail}, status_code=exc.status_code, headers=headers)
     else:
         return JSONResponse({"detail": exc.detail}, status_code=exc.status_code)
 
 
-async def request_validation_exception_handler(
-    request: Request, exc: RequestValidationError
-) -> JSONResponse:
-    return JSONResponse(
-        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-        content={"detail": jsonable_encoder(exc.errors())},
-    )
+async def request_validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    return JSONResponse(status_code=HTTP_422_UNPROCESSABLE_ENTITY, 
+                        content={"detail": jsonable_encoder(exc.errors())}, )
