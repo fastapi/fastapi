973e42f5726c44536bf909049814f2305f6beca0
Alexander Haas <mail@haas-alexander.com>
2022-06-16 14:25:02 +0200

    docs: :adhesive_bandage: Updated 400 to 401

--- docs_src/security/tutorial003.py
+++ docs_src/security/tutorial003.py
@@ -76,11 +76,19 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user_dict = fake_users_db.get(form_data.username)
     if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
     user = UserInDB(**user_dict)
     hashed_password = fake_hash_password(form_data.password)
     if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
     return {"access_token": user.username, "token_type": "bearer"}
 
