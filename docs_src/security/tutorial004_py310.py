ccd1f3d68e8e1eb34a983336270e9e0b3c78bee1
Alexander Haas <mail@haas-alexander.com>
2022-06-16 14:30:09 +0200

    docs: :adhesive_bandage: Changed Bearer to Basic

--- docs_src/security/tutorial004_py310.py
+++ docs_src/security/tutorial004_py310.py
@@ -118,7 +118,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
+            headers={"WWW-Authenticate": "Basic"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
