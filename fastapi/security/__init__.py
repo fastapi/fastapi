a6cf6eab3e84ceffdc19efa51b574e6138480c2a
Max <maximilianotorres@theluxergroup.com>
2022-06-21 15:40:18 -0300

    imports on secrutity init

--- fastapi/security/__init__.py
+++ fastapi/security/__init__.py
@@ -3,11 +3,17 @@ from .api_key import APIKeyHeader as APIKeyHeader
 from .api_key import APIKeyQuery as APIKeyQuery
 from .http import HTTPAuthorizationCredentials as HTTPAuthorizationCredentials
 from .http import HTTPBasic as HTTPBasic
+from .http import HTTPBasicClientCredentials as HTTPBasicClientCredentials
 from .http import HTTPBasicCredentials as HTTPBasicCredentials
 from .http import HTTPBearer as HTTPBearer
+from .http import HTTPClientCredentials as HTTPClientCredentials
 from .http import HTTPDigest as HTTPDigest
 from .oauth2 import OAuth2 as OAuth2
 from .oauth2 import OAuth2AuthorizationCodeBearer as OAuth2AuthorizationCodeBearer
+from .oauth2 import OAuth2ClientCredentials as OAuth2ClientCredentials
+from .oauth2 import (
+    OAuth2ClientCredentialsRequestForm as OAuth2ClientCredentialsRequestForm,
+)
 from .oauth2 import OAuth2PasswordBearer as OAuth2PasswordBearer
 from .oauth2 import OAuth2PasswordRequestForm as OAuth2PasswordRequestForm
 from .oauth2 import OAuth2PasswordRequestFormStrict as OAuth2PasswordRequestFormStrict
