48c1a9539cd22b9a13e11cbf6024f1c78a216868
Max <maximilianotorres@theluxergroup.com>
2022-06-25 11:42:30 -0300

    more tests

--- tests/test_security_oauth2_client_credentials.py
+++ tests/test_security_oauth2_client_credentials.py
@@ -1,17 +1,43 @@
 from typing import Optional
 
-from fastapi import FastAPI, Security
-from fastapi.security import OAuth2ClientCredentials
+from fastapi import FastAPI, Security, Depends, HTTPException
+from fastapi.security import (
+    OAuth2ClientCredentials,
+    OAuth2ClientCredentialsRequestForm,
+    HTTPClientCredentials,
+    HTTPBasicClientCredentials,
+)
 from fastapi.testclient import TestClient
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2ClientCredentials(tokenUrl="token", auto_error=True)
 
+token_scheme = HTTPBasicClientCredentials(
+    auto_error=False, scheme_name="oAuth2ClientCredentials"
+)
+
 
 @app.get("/items/")
 async def read_items(token: Optional[str] = Security(oauth2_scheme)):
-    return {"token": token}
+    if token:
+        return {"token": token}
+
+
+# @app.post("/token")
+# def create_access_token(
+#     form: OAuth2ClientCredentialsRequestForm = Depends(),
+#     basic_credentials: Optional[HTTPClientCredentials] = Depends(token_scheme),
+# ):
+#     if form.client_id and form.client_secret:
+#         client_id = form.client_id
+#         client_secret = form.client_secret
+#     elif basic_credentials:
+#         client_id = basic_credentials.client_id
+#         client_secret = basic_credentials.client_secret
+#     else:
+#         HTTPException(status_code=400, detail="Client credentials not provided")
+#     return {"token": "jwt_token"}
 
 
 client = TestClient(app)
@@ -72,3 +98,21 @@ def test_token():
     response = client.get("/items", headers={"Authorization": "Bearer testtoken"})
     assert response.status_code == 200, response.text
     assert response.json() == {"token": "testtoken"}
+
+
+def test_no_return_none():
+    oauth2_scheme.auto_error = False
+    response = client.get("/items")
+    assert response.status_code == 200, response.text
+    assert response.json() == None
+    oauth2_scheme.auto_error = True
+
+
+def test_client_credentials_form():
+    form = OAuth2ClientCredentialsRequestForm(
+        client_id="client_id", client_secret="client_secret", scope="profile"
+    )
+    assert form.grant_type
+    assert form.scopes
+    assert form.client_id
+    assert form.client_secret
