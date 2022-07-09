c09d5d55a32295e8656b778b3f6c13035a330d0f
Pete Jeffryes <pete.jeffryes@uplight.com>
2022-07-09 15:37:59 -0600

    adds empty list mutation tests

---
+++ tests/test_dict_body.py
@@ -0,0 +1,27 @@
+from typing import Dict
+
+from fastapi import Body, FastAPI, Query
+from fastapi.testclient import TestClient
+
+app = FastAPI()
+
+
+@app.post("/query")
+def read_query(q: Dict):
+    return q
+
+
+client = TestClient(app)
+
+def test_empty_list_invalid():
+    response = client.post("/query", json=[])
+    assert response.status_code == 422
+
+def test_empty_list():
+    response = client.post("/query", json=[])
+    assert response.json() == []
+
+def test_empty_list_does_not_mutate_to_dict():
+    response = client.post("/query", json=[])
+    assert response.json() != {}
+
