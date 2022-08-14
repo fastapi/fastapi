42421141c0761d9e6d017759623e39e76c79a2c4
András Zoller <56272960+huncut2016@users.noreply.github.com>
2022-08-14 13:36:40 +0200

    Update tutorial005_py39.py
    In line 146 the response mode is specified as "Token".  When we return a dict instead of a "Token" object, pydantic raise a "Validation error"

--- docs_src/security/tutorial005_py39.py
+++ docs_src/security/tutorial005_py39.py
@@ -153,7 +153,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={"sub": user.username, "scopes": form_data.scopes},
         expires_delta=access_token_expires,
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return Token(acces_token=acces_token, token_type="bearer")
 
 
 @app.get("/users/me/", response_model=User)
