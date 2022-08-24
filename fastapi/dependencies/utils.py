f7a2d42ad2a0ac4ce93a345c77c4c5eeb7ab9881
Thytu <valentin.de-matos@epitech.eu>
2022-08-24 07:32:55 +0000

    style(dependencies-utuils): line seperation between if statements

--- fastapi/dependencies/utils.py
+++ fastapi/dependencies/utils.py
@@ -727,12 +727,16 @@ def get_body_field(*, dependant: Dependant, name: str) -> Optional[ModelField]:
         setattr(param.field_info, "embed", True)
     model_name = "Body_" + name
     BodyModel: Type[BaseModel] = create_model(model_name)
+
     for f in flat_dependant.body_params:
         if getattr(f.field_info, "example", Undefined) is not Undefined:
             f.field_info.extra["example"] = getattr(f.field_info, "example", Undefined)
+
         if getattr(f.field_info, "examples", None) is not None:
             f.field_info.extra["examples"] = getattr(f.field_info, "examples", None)
+
         BodyModel.__fields__[f.name] = f
+
     required = any(True for f in flat_dependant.body_params if f.required)
 
     BodyFieldInfo_kwargs: Dict[str, Any] = dict(default=None)
