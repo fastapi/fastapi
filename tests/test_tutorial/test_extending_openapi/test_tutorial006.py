aacac6620387c5aa562b16845ee4a5558d39f899
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2022-08-24 22:36:44 +0300

    fix(test_tutorial): fix tests to improve coverage

---
+++ tests/test_tutorial/test_extending_openapi/test_tutorial006.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from docs_src.extending_openapi.tutorial006 import app
+
+client = TestClient(app)
+
+
+def test_swagger_ui():
+    response = client.get("/elements")
+    assert response.status_code == 200, response.text
+    assert 'router="history"' in response.text
+    assert 'layout="sidebar"' in response.text
+    assert 'tryItCredentialPolicy="omit"' in response.text
+    
+
+
+def test_get_users():
+    response = client.get("/users/foo")
+    assert response.status_code == 200, response.text
+    assert response.json() == {"message": "Hello foo"}
