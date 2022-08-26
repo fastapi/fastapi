880c8b37cf8ba88a7f4de40cf18dec49c9e71454
Ori Levari <ori.levari@microsoft.com>
2022-08-26 02:26:20 -0700

    🐛 Fix support for extending `openapi_extras` with parameter lists (#4267)
    Co-authored-by: Sebastián Ramírez <tiangolo@gmail.com>

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -161,6 +161,12 @@ def deep_dict_update(main_dict: Dict[Any, Any], update_dict: Dict[Any, Any]) -> 
             and isinstance(value, dict)
         ):
             deep_dict_update(main_dict[key], value)
+        elif (
+            key in main_dict
+            and isinstance(main_dict[key], list)
+            and isinstance(update_dict[key], list)
+        ):
+            main_dict[key] = main_dict[key] + update_dict[key]
         else:
             main_dict[key] = value
 
