7be7f87552f1109a381299450f35d5a0e98af50e
Pieter Ennes <pieter@ennes.nl>
2022-09-01 11:12:16 +0200

    Fix OIDC response code
    Using a 401 instead of 403 aligns with the HTTP standard when authentication is missing and with the existing OAuth2 dependency.

--- fastapi/security/open_id_connect_url.py
+++ fastapi/security/open_id_connect_url.py
@@ -27,7 +27,9 @@ class OpenIdConnect(SecurityBase):
         if not authorization:
             if self.auto_error:
                 raise HTTPException(
-                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
                 )
             else:
                 return None
