0dbc75c1f27fdbb75e56abec6a5ec049cb1ec0b0
Thomas Meckel <tmeckel@users.noreply.github.com>
2022-09-07 18:20:27 +0200

    Added tests/test_reponse_set_reponse_code.py

---
+++ tests/test_reponse_set_reponse_code.py
@@ -0,0 +1,31 @@
+from fastapi import Depends, FastAPI, Response
+from fastapi.params import Path
+from fastapi.testclient import TestClient
+
+msg = {"msg": "Status overwritten"}
+
+app = FastAPI()
+
+
+@app.delete(
+    "/{id}",
+    responses={
+        204: {"model": None, "description": "No Content"},
+    },
+    status_code=204,
+)
+async def delete_deployment(
+    id: str = Path(None),
+    response: Response = None,
+) -> None:
+    response.status_code = 400
+    return msg
+
+
+client = TestClient(app)
+
+
+def test_dependency_set_status_code():
+    response = client.delete("/1")
+    assert response.status_code == 400 and response.content
+    assert response.json() == msg
