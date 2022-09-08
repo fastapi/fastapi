f26af44027e588c10682a17f3acb8a69e12472d9
Irfanuddin Shafi Ahmed <irfanudeen08@gmail.com>
2022-09-09 03:01:27 +0530

    Update tests/test_starlette_exception.py
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- tests/test_starlette_exception.py
+++ tests/test_starlette_exception.py
@@ -20,9 +20,7 @@ async def read_item(item_id: str):
 
 @app.get("/http-no-body-statuscode-exception")
 async def no_body_status_code_exception():
-    raise HTTPException(
-        status_code=204,
-    )
+    raise HTTPException(status_code=204)
 
 
 @app.get("/http-no-body-statuscode-with-detail-exception")
