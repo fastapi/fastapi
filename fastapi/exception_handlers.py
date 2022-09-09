fda6d888ca6100c2f64c78c16bd3684ee844af36
Irfanuddin <irfanudeen08@gmail.com>
2022-09-09 07:21:34 +0530

    fix: used common headers

--- fastapi/exception_handlers.py
+++ fastapi/exception_handlers.py
@@ -10,7 +10,7 @@ from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 async def http_exception_handler(request: Request, exc: HTTPException) -> Response:
     headers = getattr(exc, "headers", None)
     if not is_body_allowed_for_status_code(exc.status_code):
-        return Response(status_code=exc.status_code, headers=exc.headers)
+        return Response(status_code=exc.status_code, headers=headers)
     if headers:
         return JSONResponse(
             {"detail": exc.detail}, status_code=exc.status_code, headers=headers
