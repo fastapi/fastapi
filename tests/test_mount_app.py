6442201ea52fdbc05fb9fec68d0d62a8be804997
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-09-11 12:21:57 +0300

    update and rename test_mount_app.py

--- tests/test_mount_app.py
+++ tests/test_mount_app.py
@@ -4,6 +4,7 @@ from fastapi.testclient import TestClient
 app = FastAPI()
 app2 = FastAPI()
 app3 = FastAPI()
+app4 = FastAPI()
 
 
 @app.get("/a")
@@ -29,6 +30,15 @@ router.mount("/3", app3)
 app.include_router(router)
 
 
+@app4.get("/")
+def d():
+    return "d"
+
+
+router2 = APIRouter()
+router2.mount("/", app4)
+app.include_router(router2)
+
 client = TestClient(app)
 
 
@@ -48,3 +58,10 @@ def test_c():
     response = client.get("/3/c")
     assert response.status_code == 200, response.text
     assert response.json() == "c"
+
+
+def test_mount():
+    response = client.get("/")
+    assert response.status_code == 200, response.text
+    assert response.json() == "d"
+    assert router2.routes[0].path == "/"
