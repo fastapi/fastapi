344c1ed2bc443ab51fb5560e5c40bb9177e5a4ed
Kristján Valur Jónsson <sweskman@gmail.com>
2022-09-26 13:18:53 +0000

    Add test for Security collapse, and "disable". Clean up.

--- tests/test_dependency_disable.py
+++ tests/test_dependency_disable.py
@@ -1,7 +1,8 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, call, patch
 
 import pytest
-from fastapi import APIRouter, Depends, FastAPI
+from fastapi import APIRouter, Depends, FastAPI, Security
+from fastapi.security import SecurityScopes
 from fastapi.testclient import TestClient
 
 
@@ -9,47 +10,86 @@ def app_dependency():
     return _app_dependency()
 
 
-def _app_dependency():
+def app_security(scopes: SecurityScopes):
+    _app_security(scopes.scopes)
+
+
+def _app_dependency():  # pragma: no cover
+    pass
+
+
+def _app_security(scopes):  # pragma: no cover
     pass
 
 
 @pytest.fixture
 def mocks():
-    with patch.dict(globals(), {"_app_dependency": Mock()}):
+    with patch.dict(globals(), {"_app_dependency": Mock(), "_app_security": Mock()}):
         yield
 
 
-app = FastAPI(dependencies=[Depends(app_dependency)])
+app = FastAPI(
+    dependencies=[Depends(app_dependency), Security(app_security, scopes=["b", "a"])]
+)
 
 router1 = APIRouter()
-disable_root = APIRouter(dependencies=[Depends(app_dependency, disable=True)])
+disable_root = APIRouter(
+    dependencies=[
+        Depends(app_dependency, disable=True),
+        Security(app_security, scopes=["a"], disable=True),
+    ]
+)
 
 
 @app.get("/root")
 def root():
     return {}
 
-@app.get("/root_disabled", dependencies=[Depends(app_dependency, disable=True)])
+
+@app.get(
+    "/root_disabled",
+    dependencies=[
+        Depends(app_dependency, disable=True),
+        Security(app_security, disable=True),
+    ],
+)
 def root_disabled():
     return {}
 
 
-
-@router1.get("/test1")
-def test1():
+@app.get(
+    "/root_double",
+    dependencies=[Depends(app_dependency), Security(app_security, scopes=["c"])],
+)
+def root_double():
     return {}
 
 
-@disable_root.get("/test2")
-def test2():
-    return {}
-
-@disable_root.get("/test2_re_enable", dependencies=[Depends(app_dependency)])
-def test2_re():
+@router1.get("/router1_test")
+def router1_test1():
     return {}
 
 
-app.include_router(router1, dependencies=[Depends(app_dependency, disable=True)])
+@disable_root.get("/disable_root_test")
+def disable_root_test():
+    return {}
+
+
+@disable_root.get(
+    "/disable_root_re_enable",
+    dependencies=[Depends(app_dependency), Security(app_security, scopes=["c"])],
+)
+def disable_root_re_enable():
+    return {}
+
+
+app.include_router(
+    router1,
+    dependencies=[
+        Depends(app_dependency, disable=True),
+        Security(app_security, scopes=["b"], disable=True),
+    ],
+)
 app.include_router(disable_root)
 client = TestClient(app)
 
@@ -57,17 +97,28 @@ client = TestClient(app)
 def test_root(mocks):
     client.get("root_disabled")
     _app_dependency.assert_not_called()
+    _app_security.assert_not_called()
     client.get("root")
-    _app_dependency.assert_called()
+    _app_dependency.assert_has_calls([call()])
+    _app_security.assert_has_calls([call(["b", "a"])])
+
+
+def test_root_collapse(mocks):
+    client.get("root_double")
+    _app_dependency.assert_has_calls([call()])
+    _app_security.assert_has_calls([call(["b", "a", "c"])])
 
 
 def test_router1(mocks):
-    client.get("test1")
+    client.get("router1_test")
     _app_dependency.assert_not_called()
+    _app_security.assert_has_calls([call(["a"])])
 
 
-def test_router2(mocks):
-    client.get("test2")
+def test_disable_root(mocks):
+    client.get("disable_root_test")
     _app_dependency.assert_not_called()
-    client.get("test2_re_enable")
-    _app_dependency.assert_called()
+    _app_security.assert_has_calls([call(["b"])])
+    client.get("disable_root_re_enable")
+    _app_dependency.assert_has_calls([call()])
+    _app_security.assert_has_calls([call(["b", "c"])])
