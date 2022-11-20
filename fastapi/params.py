c958e013bf3d5cfc8910d43c50ea7b2d0496b753
Kristján Valur Jónsson <sweskman@gmail.com>
2022-11-20 18:59:05 +0000

    Add scopes to Security repr()

--- fastapi/params.py
+++ fastapi/params.py
@@ -362,10 +362,13 @@ class Depends:
         self.dependency = dependency
         self.use_cache = use_cache
 
-    def __repr__(self) -> str:
+    def repr_helper(self, extra: str = "") -> str:
         attr = getattr(self.dependency, "__name__", type(self.dependency).__name__)
         cache = "" if self.use_cache else ", use_cache=False"
-        return f"{self.__class__.__name__}({attr}{cache})"
+        return f"{self.__class__.__name__}({attr}{extra}{cache})"
+
+    def __repr__(self) -> str:
+        return self.repr_helper()
 
 
 class Security(Depends):
@@ -378,3 +381,6 @@ class Security(Depends):
     ):
         super().__init__(dependency=dependency, use_cache=use_cache)
         self.scopes = scopes or []
+
+    def __repr__(self) -> str:
+        return self.repr_helper(f", scopes={self.scopes}")
