e24e0167873e589a07dc14103c6161840b3f439c
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-11-28 14:39:57 -0600

    chore: refactor concurrency.py to run sync context managers in a single thread

--- fastapi/concurrency.py
+++ fastapi/concurrency.py
@@ -1,40 +1,77 @@
-from contextlib import AsyncExitStack as AsyncExitStack  # noqa
+from contextlib import AsyncExitStack as AsyncExitStack
 from contextlib import asynccontextmanager as asynccontextmanager
-from typing import AsyncGenerator, ContextManager, TypeVar
-
-import anyio
-from anyio import CapacityLimiter
-from starlette.concurrency import iterate_in_threadpool as iterate_in_threadpool  # noqa
-from starlette.concurrency import run_in_threadpool as run_in_threadpool  # noqa
-from starlette.concurrency import (  # noqa
-    run_until_first_complete as run_until_first_complete,
+from contextvars import copy_context
+from functools import partial
+from typing import (
+    Any,
+    AsyncGenerator,
+    Awaitable,
+    Callable,
+    ContextManager,
+    Optional,
+    TypeVar,
 )
 
-_T = TypeVar("_T")
+import anyio
+from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
+from anyio.to_thread import run_sync
+
+T = TypeVar("T")
+
+
+def callable_in_thread_pool(
+    call: Callable[..., T], *, limiter: Optional[anyio.CapacityLimiter]
+) -> Callable[..., Awaitable[T]]:
+    def inner(*args: Any, **kwargs: Any) -> "Awaitable[T]":
+        return anyio.to_thread.run_sync(
+            copy_context().run, lambda: call(*args, **kwargs), limiter=limiter
+        )  # type: ignore[return-value]
+
+    return inner
+
+
+def _cm_thead_worker(
+    cm: ContextManager[T],
+    res_stream: MemoryObjectSendStream[T],
+    err_stream: MemoryObjectReceiveStream[Optional[Exception]],
+) -> None:
+    with cm as res:
+        anyio.from_thread.run(res_stream.send, res)
+        exc = anyio.from_thread.run(err_stream.receive)
+        if exc:
+            raise exc
+
+
+MaybeException = Optional[Exception]
 
 
 @asynccontextmanager
 async def contextmanager_in_threadpool(
-    cm: ContextManager[_T],
-) -> AsyncGenerator[_T, None]:
-    # blocking __exit__ from running waiting on a free thread
-    # can create race conditions/deadlocks if the context manager itself
-    # has it's own internal pool (e.g. a database connection pool)
-    # to avoid this we let __exit__ run without a capacity limit
-    # since we're creating a new limiter for each call, any non-zero limit
-    # works (1 is arbitrary)
-    exit_limiter = CapacityLimiter(1)
-    try:
-        yield await run_in_threadpool(cm.__enter__)
-    except Exception as e:
-        ok = bool(
-            await anyio.to_thread.run_sync(
-                cm.__exit__, type(e), e, None, limiter=exit_limiter
-            )
+    cm: ContextManager[T],
+    limiter: Optional[anyio.CapacityLimiter] = None,
+) -> AsyncGenerator[T, None]:
+    # streams for the data
+    send_res, rcv_res = anyio.create_memory_object_stream(  # type: ignore
+        0, item_type=Any
+    )
+    # streams for exceptions
+    send_err, rcv_err = anyio.create_memory_object_stream(  # type: ignore
+        0, item_type=MaybeException
+    )
+    async with AsyncExitStack() as stack:
+        stack.enter_context(rcv_res)
+        stack.enter_context(rcv_err)
+        stack.enter_context(send_res)
+        stack.enter_context(send_err)
+        tg = await stack.enter_async_context(anyio.create_task_group())
+        target = partial(
+            run_sync, _cm_thead_worker, cm, send_res, rcv_err, limiter=limiter
         )
-        if not ok:
-            raise e
-    else:
-        await anyio.to_thread.run_sync(
-            cm.__exit__, None, None, None, limiter=exit_limiter
-        )
+        tg.start_soon(target)
+        res = await rcv_res.receive()
+        try:
+            yield res
+        except Exception as e:
+            await send_err.send(e)
+        else:
+            await send_err.send(None)
