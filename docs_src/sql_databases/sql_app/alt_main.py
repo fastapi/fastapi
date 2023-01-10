7fffcd977f42d526ffccb0cbb2eea478e947e537
r0b2g1t <robert.neumann01@gmail.com>
2023-01-10 15:56:11 +0100

    remove the error 500 for sql middleware

--- docs_src/sql_databases/sql_app/alt_main.py
+++ docs_src/sql_databases/sql_app/alt_main.py
@@ -13,7 +13,6 @@ app = FastAPI()
 
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
-    response = Response("Internal server error", status_code=500)
     try:
         request.state.db = SessionLocal()
         response = await call_next(request)
