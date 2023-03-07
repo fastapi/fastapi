c842bdd39b1fbe8f217b7bfcfcbc40948612acee
Sebastián Ramírez <tiangolo@gmail.com>
2023-03-07 16:37:06 +0100

    📝 Update docs for Lifespan events

--- docs_src/events/tutorial003.py
+++ docs_src/events/tutorial003.py
@@ -3,16 +3,26 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 
+def fake_answer_to_everything_ml_model(x: float):
+    return x * 42
+
+
+ml_models = {}
+
+
 @asynccontextmanager
-async def lifespan(app):
-    print("startup")
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    ml_models["answer_to_everything"] = fake_answer_to_everything_ml_model
     yield
-    print("shutdown")
+    # Clean up the ML models and release the resources
+    ml_models.clear()
 
 
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
-async def hello():
-    return {"result": "hello world"}
+@app.get("/predict")
+async def predict(x: float):
+    result = ml_models["answer_to_everything"](x)
+    return {"result": result}
