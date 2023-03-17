bd9c681be25a5c5f58e5e61df9609bc648ff4106
Sebastián Ramírez <tiangolo@gmail.com>
2023-03-17 21:44:16 +0100

    🍱 Add new source examples with Annotated for Query Params and String Validations

---
+++ docs_src/query_params_str_validations/tutorial004_an.py
@@ -0,0 +1,19 @@
+from typing_extensions import Annotated
+
+from typing import Union
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        Union[str, None], Query(min_length=3, max_length=50, regex="^fixedquery$")
+    ] = None
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
