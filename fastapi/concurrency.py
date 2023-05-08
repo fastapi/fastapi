79cf8dde2dca1dd60bb0089af87287f89cbc42b4
zilinjak <67589939+zilinjak@users.noreply.github.com>
2023-05-09 00:02:18 +0200

    Update concurrency.py

--- fastapi/concurrency.py
+++ fastapi/concurrency.py
@@ -2,7 +2,7 @@ from contextlib import AsyncExitStack as AsyncExitStack  # noqa
 from contextlib import asynccontextmanager as asynccontextmanager
 from typing import AsyncGenerator, ContextManager, TypeVar
 
-import anyio
+import anyio, sys
 from anyio import CapacityLimiter
 from starlette.concurrency import iterate_in_threadpool as iterate_in_threadpool  # noqa
 from starlette.concurrency import run_in_threadpool as run_in_threadpool  # noqa
