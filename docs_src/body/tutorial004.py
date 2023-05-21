2bfa4588102e298c1cac848546993a003c33b78d
Andrey Otto <andrey.otto@saritasa.com>
2023-05-21 07:45:49 +0600

    Rename functions in code examples

--- docs_src/body/tutorial004.py
+++ docs_src/body/tutorial004.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 
 @app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
     result = {"item_id": item_id, **item.dict()}
     if q:
         result.update({"q": q})
