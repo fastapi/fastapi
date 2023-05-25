6bb2ac4328590fd6226f72dc3e25a094195df576
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-05-25 09:13:17 -0600

    Use `model_validate` instead of `from_orm`

--- tests/test_read_with_orm_mode.py
+++ tests/test_read_with_orm_mode.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 @app.post("/people/", response_model=PersonRead)
 def create_person(person: PersonCreate) -> Any:
-    db_person = Person.from_orm(person)
+    db_person = Person.model_validate(person)
     return db_person
 
 
