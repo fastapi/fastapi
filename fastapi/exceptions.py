81c47ff7c6089425375a1a59538646fc3608ec93
Sebastián Ramírez <tiangolo@gmail.com>
2023-05-26 09:11:50 +0200

    ♻️ Refactor exceptions to not depend on Pydantic ValidationError class

--- fastapi/exceptions.py
+++ fastapi/exceptions.py
@@ -1,12 +1,6 @@
 from typing import Any, Dict, Optional, Sequence, Type
 
-from pydantic import BaseModel, ValidationError, create_model
-from pydantic._internal._typing_extra import all_literal_values
-from pydantic_core import InitErrorDetails, PydanticCustomError
-from pydantic_core.core_schema import ErrorType
-
-# TODO (pv2)
-# from pydantic.error_wrappers import ErrorList
+from pydantic import BaseModel, create_model
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.exceptions import WebSocketException as WebSocketException  # noqa: F401
 
@@ -31,57 +25,27 @@ class FastAPIError(RuntimeError):
     """
 
 
-def _prepare_errors(errors: Sequence[Any]) -> list[InitErrorDetails]:
-    # return errors
-    result: list[InitErrorDetails] = []
-    known_errors = set(all_literal_values(ErrorType))
-    for error in errors:
-        assert isinstance(error, dict)
-        if error["type"] in known_errors:
-            result.append(error)
-        else:
-            new_error = error.copy()
-            new_error["type"] = PydanticCustomError(error["type"], error["msg"])
-            result.append(new_error)
-    return result
-
-
 class RequestValidationError(Exception):
-    # TODO (pv2)
-    # def __init__(self, errors: Sequence[ErrorList], *, body: Any = None) -> None:
     def __init__(self, errors: Sequence[Any], *, body: Any = None) -> None:
         self.body = body
-        self.pydantic_validation_error = ValidationError.from_exception_data(
-            "Request", _prepare_errors(errors)
-        )
+        self._errors = errors
 
     def errors(self) -> Sequence[Any]:
-        return self.pydantic_validation_error.errors()
+        return self._errors
 
 
 class WebSocketRequestValidationError(Exception):
-    # TODO (pv2)
-    # def __init__(self, errors: Sequence[ErrorList]) -> None:
     def __init__(self, errors: Sequence[Any]) -> None:
-        self.pydantic_validation_error = ValidationError.from_exception_data(
-            "WebSocket", errors
-        )
+        self._errors = errors
 
     def errors(self) -> Sequence[Any]:
-        return self.pydantic_validation_error.errors()
+        return self._errors
 
 
 class ResponseValidationError(Exception):
-    # TODO (pv2)
-    # def __init__(self, errors: Sequence[ErrorList], *, body: Any = None) -> None:
     def __init__(self, errors: Sequence[Any], *, body: Any = None) -> None:
         self.body = body
-        self.pydantic_validation_error = ValidationError.from_exception_data(
-            "Response", errors
-        )
+        self._errors = errors
 
     def errors(self) -> Sequence[Any]:
-        return self.pydantic_validation_error.errors()
-
-    def __repr__(self):
-        return repr(self.pydantic_validation_error)
+        return self._errors
