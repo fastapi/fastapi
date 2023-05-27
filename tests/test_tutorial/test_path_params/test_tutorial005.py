2ecafcafedcaa390a56f0b5ad576c52a761758cf
Sebastián Ramírez <tiangolo@gmail.com>
2023-05-27 16:13:17 +0200

    ✅ Update test for path_params with enums for compatibility with Pydantic v1 and v2

--- tests/test_tutorial/test_path_params/test_tutorial005.py
+++ tests/test_tutorial/test_path_params/test_tutorial005.py
@@ -27,16 +27,31 @@ def test_get_enums_resnet():
 def test_get_enums_invalid():
     response = client.get("/models/foo")
     assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "ctx": {"enum_values": ["alexnet", "resnet", "lenet"]},
-                "loc": ["path", "model_name"],
-                "msg": "value is not a valid enumeration member; permitted: 'alexnet', 'resnet', 'lenet'",
-                "type": "type_error.enum",
-            }
-        ]
-    }
+    assert response.json() == IsDict(
+        {
+            "detail": [
+                {
+                    "type": "enum",
+                    "loc": ["path", "model_name"],
+                    "msg": "Input should be 'alexnet','resnet' or 'lenet'",
+                    "input": "foo",
+                    "ctx": {"expected": "'alexnet','resnet' or 'lenet'"},
+                }
+            ]
+        }
+    ) | IsDict(
+        # TODO: remove when deprecating Pydantic v1
+        {
+            "detail": [
+                {
+                    "ctx": {"enum_values": ["alexnet", "resnet", "lenet"]},
+                    "loc": ["path", "model_name"],
+                    "msg": "value is not a valid enumeration member; permitted: 'alexnet', 'resnet', 'lenet'",
+                    "type": "type_error.enum",
+                }
+            ]
+        }
+    )
 
 
 def test_openapi_schema():
