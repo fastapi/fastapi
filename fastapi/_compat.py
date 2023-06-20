76648e00146a811e5d7946976cb73df51d01d381
Sebastián Ramírez <tiangolo@gmail.com>
2023-06-20 16:49:16 +0200

    ♻️ Refactor and tweak variables and types in _compat

--- fastapi/_compat.py
+++ fastapi/_compat.py
@@ -567,30 +567,30 @@ def is_uploadfile_or_nonable_uploadfile_annotation(annotation: Any) -> bool:
     return False
 
 
-def is_bytes_sequence_annotation(annotation: Union[Type[Any], None]) -> bool:
+def is_bytes_sequence_annotation(annotation: Any) -> bool:
     origin = get_origin(annotation)
     if origin is Union or origin is UnionType:
-        at_least_one_bytes_sequence = False
+        at_least_one = False
         for arg in get_args(annotation):
             if is_bytes_sequence_annotation(arg):
-                at_least_one_bytes_sequence = True
+                at_least_one = True
                 continue
-        return at_least_one_bytes_sequence
+        return at_least_one
     return field_annotation_is_sequence(annotation) and all(
         is_bytes_or_nonable_bytes_annotation(sub_annotation)
         for sub_annotation in get_args(annotation)
     )
 
 
-def is_uploadfile_sequence_annotation(annotation: Union[Type[Any], None]) -> bool:
+def is_uploadfile_sequence_annotation(annotation: Any) -> bool:
     origin = get_origin(annotation)
     if origin is Union or origin is UnionType:
-        at_least_one_bytes_sequence = False
+        at_least_one = False
         for arg in get_args(annotation):
             if is_uploadfile_sequence_annotation(arg):
-                at_least_one_bytes_sequence = True
+                at_least_one = True
                 continue
-        return at_least_one_bytes_sequence
+        return at_least_one
     return field_annotation_is_sequence(annotation) and all(
         is_uploadfile_or_nonable_uploadfile_annotation(sub_annotation)
         for sub_annotation in get_args(annotation)
