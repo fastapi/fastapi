428a7bf3bca58c1ba2d8d90bb7834a6d3453c567
Sebastián Ramírez <tiangolo@gmail.com>
2023-06-25 14:24:24 +0200

    ⏪️ Restore and refactor new-lang command

--- scripts/docs.py
+++ scripts/docs.py
@@ -57,6 +57,31 @@ def complete_existing_lang(incomplete: str):
 
 
 @app.command()
+def new_lang(lang: str = typer.Argument(..., callback=lang_callback)):
+    """
+    Generate a new docs translation directory for the language LANG.
+
+    LANG should be a 2-letter language code, like: en, es, de, pt, etc.
+    """
+    new_path: Path = Path("docs") / lang
+    if new_path.exists():
+        typer.echo(f"The language was already created: {lang}")
+        raise typer.Abort()
+    new_path.mkdir()
+    new_config_path: Path = Path(new_path) / mkdocs_name
+    new_config_path.write_text("INHERIT: ../en/mkdocs.yml\n", encoding="utf-8")
+    new_config_docs_path: Path = new_path / "docs"
+    new_config_docs_path.mkdir()
+    en_index_path: Path = en_docs_path / "docs" / "index.md"
+    new_index_path: Path = new_config_docs_path / "index.md"
+    en_index_content = en_index_path.read_text(encoding="utf-8")
+    new_index_content = f"{missing_translation_snippet}\n\n{en_index_content}"
+    new_index_path.write_text(new_index_content, encoding="utf-8")
+    typer.secho(f"Successfully initialized: {new_path}", color=typer.colors.GREEN)
+    update_languages()
+
+
+@app.command()
 def build_lang(
     lang: str = typer.Argument(
         ..., callback=lang_callback, autocompletion=complete_existing_lang
