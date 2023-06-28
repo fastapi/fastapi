b7a733367d47fcebd6f09751bf1a26b660bed7e4
Erik Wienhold <ewie@ewie.name>
2023-06-29 00:39:50 +0200

    Create primary key without extra index in docs
    Primary key constraint implies a unique index.  An additional index is
    not necessary.

--- docs_src/sql_databases/sql_app_py39/models.py
+++ docs_src/sql_databases/sql_app_py39/models.py
@@ -7,7 +7,7 @@ from .database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
@@ -18,7 +18,7 @@ class User(Base):
 class Item(Base):
     __tablename__ = "items"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
