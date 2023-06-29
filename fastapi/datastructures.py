d119dcac79fa0505bfddfe4f8e3e4b4250834962
Sebastián Ramírez <tiangolo@gmail.com>
2023-06-29 17:05:58 +0200

    ✨ Update JSON Schema for UploadFiles

--- fastapi/datastructures.py
+++ fastapi/datastructures.py
@@ -22,7 +22,9 @@ class UploadFile(StarletteUploadFile):
 
     @classmethod
     def __modify_schema__(cls, field_schema: Dict[str, Any]) -> None:
-        field_schema.update({"type": "string", "format": "binary"})
+        field_schema.update(
+            {"type": "string", "contentMediaType": "application/octet-stream"}
+        )
 
 
 class DefaultPlaceholder:
