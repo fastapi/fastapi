92d4645fb66f35e7faf1c08faefd6d1d09f060c9
Sebastián Ramírez <tiangolo@gmail.com>
2023-07-04 17:30:56 +0200

    ✅ Add tests for Pydantic v1 and v2 for advanced configs for path operations

--- tests/test_tutorial/test_path_operation_advanced_configurations/test_tutorial007_pv1.py
+++ tests/test_tutorial/test_path_operation_advanced_configurations/test_tutorial007_pv1.py
@@ -1,18 +1,18 @@
 import pytest
 from fastapi.testclient import TestClient
 
-from ...utils import needs_pydanticv2
+from ...utils import needs_pydanticv1
 
 
 @pytest.fixture(name="client")
 def get_client():
-    from docs_src.path_operation_advanced_configuration.tutorial007 import app
+    from docs_src.path_operation_advanced_configuration.tutorial007_pv1 import app
 
     client = TestClient(app)
     return client
 
 
-@needs_pydanticv2
+@needs_pydanticv1
 def test_post(client: TestClient):
     yaml_data = """
         name: Deadpoolio
@@ -29,7 +29,7 @@ def test_post(client: TestClient):
     }
 
 
-@needs_pydanticv2
+@needs_pydanticv1
 def test_post_broken_yaml(client: TestClient):
     yaml_data = """
         name: Deadpoolio
@@ -43,7 +43,7 @@ def test_post_broken_yaml(client: TestClient):
     assert response.json() == {"detail": "Invalid YAML"}
 
 
-@needs_pydanticv2
+@needs_pydanticv1
 def test_post_invalid(client: TestClient):
     yaml_data = """
         name: Deadpoolio
@@ -62,7 +62,7 @@ def test_post_invalid(client: TestClient):
     }
 
 
-@needs_pydanticv2
+@needs_pydanticv1
 def test_openapi_schema(client: TestClient):
     response = client.get("/openapi.json")
     assert response.status_code == 200, response.text
