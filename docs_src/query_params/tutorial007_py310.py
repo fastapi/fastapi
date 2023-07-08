c020f9c8a92b0882f936b816bb418ef2d6d1c8ff
JONEMI19 <michael.jones4@kingfisher.com>
2023-07-08 10:26:53 +0000

    add docs

---
+++ docs_src/query_params/tutorial007_py310.py
@@ -0,0 +1,20 @@
+from typing import Mapping, List
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/query/mixed-type-params")
+def get_mixed_mapping_mixed_type_query_params(
+    query: int = Query(),
+    mapping_query_str: Mapping[str, str] = Query({}),
+    mapping_query_int: Mapping[str, int] = Query({}),
+    sequence_mapping_queries: Mapping[str, List[int]] = Query({}),
+):
+    return {
+        "query":query, 
+        "string_mapping": mapping_query_str, 
+        "mapping_query_int": mapping_query_int, 
+        "sequence_mapping_queries": sequence_mapping_queries
+        }
