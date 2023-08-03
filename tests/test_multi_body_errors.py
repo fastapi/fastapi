b0e4cdb8afa1c4f8b4f4fe6d886b32c8fed48d2c
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-08-03 16:46:57 +0100

    ✅ Fix tests for compatibility with pydantic 2.1.1 (#9943)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Sebastián Ramírez <tiangolo@gmail.com>

--- tests/test_multi_body_errors.py
+++ tests/test_multi_body_errors.py
@@ -51,7 +51,7 @@ def test_jsonable_encoder_requiring_error():
                     "loc": ["body", 0, "age"],
                     "msg": "Input should be greater than 0",
                     "input": -1.0,
-                    "ctx": {"gt": 0.0},
+                    "ctx": {"gt": "0"},
                     "url": match_pydantic_error_url("greater_than"),
                 }
             ]
@@ -85,8 +85,22 @@ def test_put_incorrect_body_multiple():
                     "url": match_pydantic_error_url("missing"),
                 },
                 {
+                    "ctx": {"class": "Decimal"},
+                    "input": "five",
+                    "loc": ["body", 0, "age", "is-instance[Decimal]"],
+                    "msg": "Input should be an instance of Decimal",
+                    "type": "is_instance_of",
+                    "url": match_pydantic_error_url("is_instance_of"),
+                },
+                {
                     "type": "decimal_parsing",
-                    "loc": ["body", 0, "age"],
+                    "loc": [
+                        "body",
+                        0,
+                        "age",
+                        "function-after[to_decimal(), "
+                        "union[int,constrained-str,function-plain[str()]]]",
+                    ],
                     "msg": "Input should be a valid decimal",
                     "input": "five",
                 },
@@ -98,8 +112,22 @@ def test_put_incorrect_body_multiple():
                     "url": match_pydantic_error_url("missing"),
                 },
                 {
+                    "ctx": {"class": "Decimal"},
+                    "input": "six",
+                    "loc": ["body", 1, "age", "is-instance[Decimal]"],
+                    "msg": "Input should be an instance of Decimal",
+                    "type": "is_instance_of",
+                    "url": match_pydantic_error_url("is_instance_of"),
+                },
+                {
                     "type": "decimal_parsing",
-                    "loc": ["body", 1, "age"],
+                    "loc": [
+                        "body",
+                        1,
+                        "age",
+                        "function-after[to_decimal(), "
+                        "union[int,constrained-str,function-plain[str()]]]",
+                    ],
                     "msg": "Input should be a valid decimal",
                     "input": "six",
                 },
