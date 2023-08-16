a346cd8b706f532e8602b3275c28b892bffdea8c
mxp <maxplayer@free.fr>
2023-08-16 23:49:31 +0200

    update other python file as well to get the right import for declarative_base
    The declarative_base() function is now available as sqlalchemy.orm.declarative_base(). (deprecated since: 2.0) (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9)
    Base = declarative_base()

--- docs_src/sql_databases/sql_app/database.py
+++ docs_src/sql_databases/sql_app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
