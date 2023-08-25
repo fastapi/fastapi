704b44d7e089346630c43bf38d59a3c9700d5d41
Sebastián Ramírez <tiangolo@gmail.com>
2023-08-25 17:59:46 +0200

    📝 Update tutorial to use Union instead of Optional

--- docs_src/separate_openapi_schemas/tutorial001.py
+++ docs_src/separate_openapi_schemas/tutorial001.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 class Item(BaseModel):
     name: str
-    description: Optional[str] = None
+    description: Union[str, None] = None
 
 
 app = FastAPI()
