1d3bdfa296dc51c172aa10a0d1fc2870b8ed7e1d
ManiMozaffar <mani.mozaffar@gmail.com>
2023-08-27 12:12:37 +0300

    🎨 Add prototype suggestion for ApiEnum

--- docs_src/dependencies/tutorial005.py
+++ docs_src/dependencies/tutorial005.py
@@ -1,6 +1,6 @@
 from typing import Union
-
-from fastapi import Cookie, Depends, FastAPI
+from enum import Enum, auto
+from fastapi import Cookie, Depends, FastAPI, EndPoint
 
 app = FastAPI()
 
@@ -9,15 +9,25 @@ def query_extractor(q: Union[str, None] = None):
     return q
 
 
+class EndPointEnum(Enum):
+    READ_ITEMS = auto()
+
+
 def query_or_cookie_extractor(
+    endpoint: EndPointEnum = EndPoint(),
     q: str = Depends(query_extractor),
     last_query: Union[str, None] = Cookie(default=None),
 ):
+    assert endpoint is EndPointEnum.READ_ITEMS  # for read_query endpoint!
     if not q:
         return last_query
     return q
 
 
-@app.get("/items/")
-async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
+@app.get("/items/", endpoint_enum=EndPointEnum.READ_ITEMS)
+async def read_query(
+    query_or_default: str = Depends(query_or_cookie_extractor),
+    endpoint: EndPointEnum = EndPoint(),
+):
+    assert endpoint is EndPointEnum.READ_ITEMS  # for read_query endpoint!
     return {"q_or_cookie": query_or_default}
