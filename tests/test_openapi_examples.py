37d46e6b6c58c0e38747d91c4a0cdc0ea76c9d40
Sebastián Ramírez <tiangolo@gmail.com>
2023-09-01 23:36:08 +0200

    ✅ Add missing test for OpenAPI examples, it was missing in coverage (#10188)
    ✅ Add missing test for OpenAPI examples, it seems it was discovered in coverage by an upgrade in AnyIO

--- tests/test_openapi_examples.py
+++ tests/test_openapi_examples.py
@@ -125,6 +125,9 @@ client = TestClient(app)
 
 
 def test_call_api():
+    response = client.post("/examples/", json={"data": "example1"})
+    assert response.status_code == 200, response.text
+
     response = client.get("/path_examples/foo")
     assert response.status_code == 200, response.text
 
