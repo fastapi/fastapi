62530df4d7bc3a8830a34eefc247063500d26eef
ordinary-jamie <jamie@ordinarylab.dev>
2023-09-12 17:16:59 +1000

    Update HTTPBearer to return 401 on error
    HTTPBearer scheme should return a 401 in the case of missing or
    malformed authorization header, consistent with RFC 6750 Section 3.1

--- fastapi/security/http.py
+++ fastapi/security/http.py
@@ -118,14 +118,14 @@ class HTTPBearer(HTTPBase):
         if not (authorization and scheme and credentials):
             if self.auto_error:
                 raise HTTPException(
-                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
+                    status_code=HTTP_401_UNAUTHORIZED, detail="Not authenticated"
                 )
             else:
                 return None
         if scheme.lower() != "bearer":
             if self.auto_error:
                 raise HTTPException(
-                    status_code=HTTP_403_FORBIDDEN,
+                    status_code=HTTP_401_UNAUTHORIZED,
                     detail="Invalid authentication credentials",
                 )
             else:
