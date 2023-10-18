587d42a39060b26857e0135a67e40288d5476f77
bilal.alpaslan <bilal.alpaslan@glomil.com>
2023-10-18 21:24:39 +0300

    update openapi version

--- tests/test_security_api_key_multiple_header.py
+++ tests/test_security_api_key_multiple_header.py
@@ -38,7 +38,7 @@ def read_current_user_another_key(
 client = TestClient(app)
 
 openapi_schema = {
-    "openapi": "3.0.2",
+    "openapi": "3.1.0",
     "info": {"title": "FastAPI", "version": "0.1.0"},
     "paths": {
         "/users/me": {
