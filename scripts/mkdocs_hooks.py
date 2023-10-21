a77f5f88a6e7f7f23f56870b3679d03dae720c21
Sebastián Ramírez <tiangolo@gmail.com>
2023-10-21 11:03:18 +0400

    🐛 Fix overriding MKDocs theme lang in hook

--- scripts/mkdocs_hooks.py
+++ scripts/mkdocs_hooks.py
@@ -24,7 +24,7 @@ def get_missing_translation_content(docs_dir: str) -> str:
 @lru_cache()
 def get_mkdocs_material_langs() -> List[str]:
     material_path = Path(material.__file__).parent
-    material_langs_path = material_path / "partials" / "languages"
+    material_langs_path = material_path / "templates" / "partials" / "languages"
     langs = [file.stem for file in material_langs_path.glob("*.html")]
     return langs
 
