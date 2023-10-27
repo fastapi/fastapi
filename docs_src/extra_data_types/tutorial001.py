da837b9bd0a5176b22a5308806483d59bc56b565
Nils Lindemann <nilslindemann@tutanota.com>
2023-10-27 17:55:13 +0200

    Make type checker happy in examples under  `docs_src/extra_data_types`
    Before it would complain when we added/substracted these values, as they can be None.

--- docs_src/extra_data_types/tutorial001.py
+++ docs_src/extra_data_types/tutorial001.py
@@ -10,10 +10,10 @@ app = FastAPI()
 @app.put("/items/{item_id}")
 async def read_items(
     item_id: UUID,
-    start_datetime: Union[datetime, None] = Body(default=None),
-    end_datetime: Union[datetime, None] = Body(default=None),
+    start_datetime: datetime = Body(),
+    end_datetime: datetime = Body(),
+    process_after: timedelta = Body(),
     repeat_at: Union[time, None] = Body(default=None),
-    process_after: Union[timedelta, None] = Body(default=None),
 ):
     start_process = start_datetime + process_after
     duration = end_datetime - start_process
@@ -21,8 +21,8 @@ async def read_items(
         "item_id": item_id,
         "start_datetime": start_datetime,
         "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
         "process_after": process_after,
+        "repeat_at": repeat_at,
         "start_process": start_process,
         "duration": duration,
     }
--- docs_src/extra_data_types/tutorial001_py310.py
+++ docs_src/extra_data_types/tutorial001_py310.py
@@ -9,10 +9,10 @@ app = FastAPI()
 @app.put("/items/{item_id}")
 async def read_items(
     item_id: UUID,
-    start_datetime: datetime | None = Body(default=None),
-    end_datetime: datetime | None = Body(default=None),
+    start_datetime: datetime = Body(),
+    end_datetime: datetime = Body(),
+    process_after: timedelta = Body(),
     repeat_at: time | None = Body(default=None),
-    process_after: timedelta | None = Body(default=None),
 ):
     start_process = start_datetime + process_after
     duration = end_datetime - start_process
@@ -20,8 +20,8 @@ async def read_items(
         "item_id": item_id,
         "start_datetime": start_datetime,
         "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
         "process_after": process_after,
+        "repeat_at": repeat_at,
         "start_process": start_process,
         "duration": duration,
     }
