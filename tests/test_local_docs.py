dd752aee921a87ee8950417d71d843b52a537a48
Kshitij Mishra <141047744+kshitim12@users.noreply.github.com>
2023-11-01 21:17:49 +0530

    Update test_local_docs.py

--- tests/test_local_docs.py
+++ tests/test_local_docs.py
@@ -65,3 +65,9 @@ def test_google_fonts_in_generated_redoc():
         openapi_url="/docs", title="title", with_google_fonts=False
     ).body.decode()
     assert "fonts.googleapis.com" not in body_without_google_fonts
+     assert "fonts.googleapis.com" in body_with_google_fonts
+    body_without_google_fonts = get_redoc_html(
+        openapi_url="/docs", title="title", with_google_fonts=False
+    ).body.decode()
+    assert "fonts.googleapis.com" not in body_without_google_fonts
+    
