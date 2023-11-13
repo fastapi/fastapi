20bcbc8752e07606a1ccbffea1ac4df1e94df604
MarcGugg <meir.guggenheim@gmail.com>
2023-11-12 22:09:48 -0500

    ✏️  Add user validation to create_item_for_user

--- docs_src/sql_databases/sql_app_py39/main.py
+++ docs_src/sql_databases/sql_app_py39/main.py
@@ -44,6 +44,9 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
