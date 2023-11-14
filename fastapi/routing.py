9846605462a2810b32855ac3f9e6724bfa994ac2
heysaeid <heysaeid92@gmail.com>
2023-11-14 17:52:00 +0330

    📝 Update documentation for deprecated on_startup and on_shutdown parameters

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -690,6 +690,14 @@ class APIRouter(routing.Router):
                 Read more in the [FastAPI docs for `lifespan`](https://fastapi.tiangolo.com/advanced/events/).
                 """
             ),
+            deprecated(
+                """
+                **Attention**: This parameter is deprecated.
+
+                The recommended approach for managing startup and shutdown 
+                is by utilizing the lifespan parameter within the APIRouter.
+                """
+            ),
         ] = None,
         on_shutdown: Annotated[
             Optional[Sequence[Callable[[], Any]]],
@@ -701,6 +709,14 @@ class APIRouter(routing.Router):
 
                 Read more in the
                 [FastAPI docs for `lifespan`](https://fastapi.tiangolo.com/advanced/events/).
+                """
+            ),
+            deprecated(
+                """
+                **Attention**: This parameter is deprecated.
+
+                The recommended approach for managing startup and shutdown 
+                is by utilizing the lifespan parameter within the APIRouter.
                 """
             ),
         ] = None,
