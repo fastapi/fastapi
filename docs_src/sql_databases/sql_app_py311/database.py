3eec2f203ef3561e0083677bbf74afd43e16a66a
David J Galvan Jr <145246781+djgalvan@users.noreply.github.com>
2024-02-07 04:55:46 -0500

    Use declarative_base() from sqlalchemy.orm

--- docs_src/sql_databases/sql_app_py311/database.py
+++ docs_src/sql_databases/sql_app_py311/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
