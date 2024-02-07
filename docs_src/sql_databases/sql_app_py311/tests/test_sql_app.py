818d27e5425b9b8180765d77bfe5f56ef49f8016
David J Galvan Jr <145246781+djgalvan@users.noreply.github.com>
2024-02-07 06:18:15 -0500

    Fixed imports in test_sql_app.py

--- docs_src/sql_databases/sql_app_py311/tests/test_sql_app.py
+++ docs_src/sql_databases/sql_app_py311/tests/test_sql_app.py
@@ -2,8 +2,8 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from ..database import Base
-from ..main import app, get_db
+from database import Base
+from main import app, get_db
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
