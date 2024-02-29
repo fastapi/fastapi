850750d8d856070621c4b105d375ad678be989f9
JONEMI19 <michael.jones4@kingfisher.com>
2024-02-29 08:07:14 +0000

    update tutorial

--- docs_src/query_params/tutorial007_py310.py
+++ docs_src/query_params/tutorial007_py310.py
@@ -1,6 +1,7 @@
 from typing import List, Mapping
 
 from fastapi import FastAPI, Query
+from fastapi.types import FFQuery
 
 app = FastAPI()
 
@@ -8,9 +9,9 @@ app = FastAPI()
 @app.get("/query/mixed-type-params")
 def get_mixed_mapping_mixed_type_query_params(
     query: int = Query(),
-    mapping_query_str: Mapping[str, str] = Query({}),
-    mapping_query_int: Mapping[str, int] = Query({}),
-    sequence_mapping_queries: Mapping[str, List[int]] = Query({}),
+    mapping_query_str: FFQuery[str, str] = Query({}),
+    mapping_query_int: FFQuery[str, int] = Query({}),
+    sequence_mapping_queries: FFQuery[str, List[int]] = Query({}),
 ):
     return {
         "query": query,
