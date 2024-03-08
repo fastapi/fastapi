7f87c0f5d516107d97efc4a36238990c18221357
Alejandra <90076947+alejsdev@users.noreply.github.com>
2024-03-08 20:45:49 +0100

    🔥 Remove unused schemas (#656)

--- backend/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .item import Item, ItemCreate, ItemInDB, ItemUpdate
-from .msg import Msg
-from .token import Token, TokenPayload
-from .user import User, UserCreate, UserInDB, UserUpdate
--- backend/app/schemas/msg.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Msg(BaseModel):
-    msg: str
