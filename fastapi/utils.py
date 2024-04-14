aa1a15996caaf9448cf21705ab7c3cc8286b59a5
JD Solanki <jdsolanki0001@gmail.com>
2024-04-14 17:26:26 +0530

    perf: instead of creating list of method use iterator
    Changes according to [this](https://docs.astral.sh/ruff/rules/unnecessary-iterable-allocation-for-first-element/)

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -183,7 +183,7 @@ def generate_unique_id(route: "APIRoute") -> str:
     operation_id = f"{route.name}{route.path_format}"
     operation_id = re.sub(r"\W", "_", operation_id)
     assert route.methods
-    operation_id = f"{operation_id}_{list(route.methods)[0].lower()}"
+    operation_id = f"{operation_id}_{next(iter(route.methods)).lower()}"
     return operation_id
 
 
