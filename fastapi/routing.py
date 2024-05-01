033e2cff11beb1c7fa702f32e99005eed786674a
reton2 <rafaymsgo@gmail.com>
2024-05-01 11:51:22 +0100

    fix: linting issue

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -211,6 +211,7 @@ def get_request_handler(
     is_coroutine = (
         asyncio.iscoroutinefunction(dependant.call)
         or callable(dependant.call)
+        and hasattr(dependant.call, "__call__")
         and inspect.iscoroutinefunction(dependant.call.__call__)
     )
     is_body_form = body_field and isinstance(body_field.field_info, params.Form)
