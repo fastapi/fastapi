071ec3f322114530c9dca1525b7d3b11cdccee01
Gabriel <gabriel59kg@gmail.com>
2024-05-03 21:24:51 -0400

    Update applications.py

--- fastapi/applications.py
+++ fastapi/applications.py
@@ -4523,7 +4523,10 @@ class FastAPI(Starlette):
 
 
         @app.middleware("http")
-        async def add_process_time_header(request: Request, call_next: Callable[[Request], Awaitable[Response]):
+        async def add_process_time_header(
+                request: Request,
+                call_next: Callable[[Request], Awaitable[Response]
+            ) -> Response:
             start_time = time.time()
             response = await call_next(request)
             process_time = time.time() - start_time
