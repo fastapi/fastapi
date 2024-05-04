83375dcdbee97af93a7cf9f20a1ef5b476c7015c
Gabriel <gabriel59kg@gmail.com>
2024-05-03 21:41:49 -0400

    Apply suggestions from code review

--- fastapi/applications.py
+++ fastapi/applications.py
@@ -4524,9 +4524,8 @@ class FastAPI(Starlette):
 
         @app.middleware("http")
         async def add_process_time_header(
-                request: Request,
-                call_next: Callable[[Request], Awaitable[Response]
-            ) -> Response:
+            request: Request, call_next: Callable[[Request], Awaitable[Response]]
+        ) -> Response:
             start_time = time.time()
             response = await call_next(request)
             process_time = time.time() - start_time
