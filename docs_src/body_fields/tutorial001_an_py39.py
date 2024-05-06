88a7f70e8e7a18e3f832c80cda7361afedf71148
Dauletkhan Assanov <dauletkhan@DauletkhansMac.bbrouter>
2024-05-06 10:35:00 +0500

    📝Fixed typo in documentation - Body Fields page

--- docs_src/body_fields/tutorial001_an_py39.py
+++ docs_src/body_fields/tutorial001_an_py39.py
@@ -9,7 +9,7 @@ app = FastAPI()
 class Item(BaseModel):
     name: str
     description: Union[str, None] = Field(
-        default=None, title="The description of the item", max_length=300
+        default=None, title="The title of the item", max_length=300
     )
     price: float = Field(gt=0, description="The price must be greater than zero")
     tax: Union[float, None] = None
