44ed5b178381e182f610f9bad16b50b3a2448b22
Alejandra <90076947+alejsdev@users.noreply.github.com>
2024-05-20 13:15:23 -0500

    🚚 Move assets to public folder (#1206)

--- frontend/src/components/Common/Sidebar.tsx
+++ frontend/src/components/Common/Sidebar.tsx
@@ -15,7 +15,7 @@ import {
 import { useQueryClient } from "@tanstack/react-query"
 import { FiLogOut, FiMenu } from "react-icons/fi"
 
-import Logo from "../../assets/images/fastapi-logo.svg"
+import Logo from "/assets/images/fastapi-logo.svg"
 import type { UserPublic } from "../../client"
 import useAuth from "../../hooks/useAuth"
 import SidebarItems from "./SidebarItems"
--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -20,7 +20,7 @@ import {
 } from "@tanstack/react-router"
 import { type SubmitHandler, useForm } from "react-hook-form"
 
-import Logo from "../assets/images/fastapi-logo.svg"
+import Logo from "/assets/images/fastapi-logo.svg"
 import type { Body_login_login_access_token as AccessToken } from "../client"
 import useAuth, { isLoggedIn } from "../hooks/useAuth"
 import { emailPattern } from "../utils"
