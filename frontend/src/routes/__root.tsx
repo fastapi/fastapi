1bf057bd1989f38b2a8989aa9945734749f0a4a3
Tomer Barletz <barletz@gmail.com>
2024-06-02 01:34:41 +0300

    ✨ Add TanStack React Query devtools in dev build (#1217)

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,21 +3,32 @@ import React, { Suspense } from "react"
 
 import NotFound from "../components/Common/NotFound"
 
-const TanStackRouterDevtools =
+const loadDevtools = () =>
+  Promise.all([
+    import("@tanstack/router-devtools"),
+    import("@tanstack/react-query-devtools")
+  ]).then(([routerDevtools, reactQueryDevtools]) => {
+    return {
+      default: () => (
+        <>
+          <routerDevtools.TanStackRouterDevtools />
+          <reactQueryDevtools.ReactQueryDevtools />
+        </>
+      )
+    };
+  });
+
+const TanStackDevtools =
   process.env.NODE_ENV === "production"
     ? () => null
-    : React.lazy(() =>
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-        })),
-      )
+    : React.lazy(loadDevtools);
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
       <Suspense>
-        <TanStackRouterDevtools />
+        <TanStackDevtools />
       </Suspense>
     </>
   ),
