e9a9aca04eabd841bb030e1424a6a54877a9386c
LainezDev <camilolainez@gmail.com>
2024-06-04 23:47:14 -0500

    Add CookieToAuth2 middleware to convert cookies to Authorization header

---
+++ fastapi/middleware/cookietoauth2.py
@@ -0,0 +1,21 @@
+from fastapi import Request
+from app.core.config import settings
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.datastructures import MutableHeaders
+from starlette.requests import Request
+from starlette.types import ASGIApp
+
+class CookieToAuth2Middleware(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp) -> None:
+        super().__init__(app)
+        self.app = app
+
+    async def dispatch(self, request: Request, call_next) -> None:
+        token = request.cookies.get(settings.COOKIE_NAME)
+        if token:
+            new_headers = MutableHeaders(request._headers)
+            new_headers["Authorization"] = f"Bearer {token}"
+            request._headers=new_headers
+            request.scope.update(headers=request.headers.raw)            
+        response = await call_next(request)
+        return response
