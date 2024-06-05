806ead79149a4046d97fd1085843c0a309295cb2
LainezDev <camilolainez@gmail.com>
2024-06-05 00:27:31 -0500

    CookieToAuth2

--- fastapi/middleware/cookietoauth2.py
+++ fastapi/middleware/cookietoauth2.py
@@ -1,17 +1,18 @@
 from fastapi import Request
-from app.core.config import settings
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.datastructures import MutableHeaders
 from starlette.requests import Request
 from starlette.types import ASGIApp
+from starlette.responses import Response
 
 class CookieToAuth2Middleware(BaseHTTPMiddleware):
-    def __init__(self, app: ASGIApp) -> None:
+    def __init__(self, app: ASGIApp, cookie_name: str) -> None:
         super().__init__(app)
         self.app = app
+        self.cookie_name = cookie_name
 
-    async def dispatch(self, request: Request, call_next) -> None:
-        token = request.cookies.get(settings.COOKIE_NAME)
+    async def dispatch(self, request: Request, call_next) -> Response:
+        token = request.cookies.get(self.cookie_name)
         if token:
             new_headers = MutableHeaders(request._headers)
             new_headers["Authorization"] = f"Bearer {token}"
