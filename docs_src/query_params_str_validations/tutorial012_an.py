fc9f584985fd30f8d1ba41fb33a8f71e3e0cb447
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-01 06:21:40 +0600

    Update tutorial012_an.py

--- docs_src/query_params_str_validations/tutorial012_an.py
+++ docs_src/query_params_str_validations/tutorial012_an.py
@@ -7,6 +7,8 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[List[str], Query()] = ["foo", "bar"]):
+async def read_items(q: Annotated[List[str], Query()] = None):
+    if q is None:
+        q = ["foo", "bar"]
     query_items = {"q": q}
     return query_items
