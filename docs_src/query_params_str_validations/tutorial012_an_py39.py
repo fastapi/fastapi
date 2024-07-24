8f968ea6146b74841df4dd4b3508441795e5e931
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-24 20:25:28 +0600

    Dangerous default argument

--- docs_src/query_params_str_validations/tutorial012_an_py39.py
+++ docs_src/query_params_str_validations/tutorial012_an_py39.py
@@ -6,6 +6,8 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[list[str], Query()] = ["foo", "bar"]):
+async def read_items(q: Annotated[list[str], Query()] = None):
+    if q is None:
+        q = ["foo", "bar"]
     query_items = {"q": q}
     return query_items
