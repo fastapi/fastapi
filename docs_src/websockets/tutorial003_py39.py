caa46eb5e373cd6f03bd592c850010a4b2daaa53
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-24 19:40:36 +0600

    Consider decorating method with @staticmethod

--- docs_src/websockets/tutorial003_py39.py
+++ docs_src/websockets/tutorial003_py39.py
@@ -52,7 +52,8 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
+    @staticmethod
+    async def send_personal_message(message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
