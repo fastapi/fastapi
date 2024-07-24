b46ab9dd1d355cbc053ad86bff46a645be57f9c4
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-24 21:58:26 +0600

    Update tutorial001_an_py39.py

--- docs_src/dependency_testing/tutorial001_an_py39.py
+++ docs_src/dependency_testing/tutorial001_an_py39.py
@@ -34,26 +34,32 @@ app.dependency_overrides[common_parameters] = override_dependency
 
 def test_override_in_items():
     response = client.get("/items/")
-    assert response.status_code == 200
-    assert response.json() == {
+    if response.status_code != 200:
+        raise AssertionError
+    if response.json() != {
         "message": "Hello Items!",
         "params": {"q": None, "skip": 5, "limit": 10},
-    }
+    }:
+        raise AssertionError
 
 
 def test_override_in_items_with_q():
     response = client.get("/items/?q=foo")
-    assert response.status_code == 200
-    assert response.json() == {
+    if response.status_code != 200:
+        raise AssertionError
+    if response.json() != {
         "message": "Hello Items!",
         "params": {"q": "foo", "skip": 5, "limit": 10},
-    }
+    }:
+        raise AssertionError
 
 
 def test_override_in_items_with_params():
     response = client.get("/items/?q=foo&skip=100&limit=200")
-    assert response.status_code == 200
-    assert response.json() == {
+    if response.status_code != 200:
+        raise AssertionError
+    if response.json() != {
         "message": "Hello Items!",
         "params": {"q": "foo", "skip": 5, "limit": 10},
-    }
+    }:
+        raise AssertionError
