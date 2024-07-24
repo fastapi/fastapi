c0a0050b87896c79625556b4961b54e5f1cdd3b0
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-25 01:08:49 +0600

    Update utils.py

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -182,7 +182,8 @@ def generate_operation_id_for_path(
 def generate_unique_id(route: "APIRoute") -> str:
     operation_id = f"{route.name}{route.path_format}"
     operation_id = re.sub(r"\W", "_", operation_id)
-    assert route.methods
+    if not route.methods:
+        raise AssertionError
     operation_id = f"{operation_id}_{list(route.methods)[0].lower()}"
     return operation_id
 
