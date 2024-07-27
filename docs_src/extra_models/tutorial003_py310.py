85f5d8489deee2c1789c28f1884aba9285eb1fc9
Xinhe Wang <wangxinhe06@gmail.com>
2024-07-27 13:55:20 +0800

    Explicitly tell the readers not to use `|`
    `extra_models/tutorial003_py310.py` was exactly the same as `extra_models/tutorial003.py`
    This may confuse first-time readers

--- docs_src/extra_models/tutorial003_py310.py
+++ docs_src/extra_models/tutorial003_py310.py
@@ -30,6 +30,9 @@ items = {
 }
 
 
-@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+@app.get(
+    "/items/{item_id}",
+    response_model=Union[PlaneItem, CarItem],  # cannot use `PlaneItem | CarItem`
+)
 async def read_item(item_id: str):
     return items[item_id]
