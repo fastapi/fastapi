cf2b1d817b4bf2fc72575464de0e890fe00ff634
Sebastián Ramírez <tiangolo@gmail.com>
2024-07-30 21:32:58 -0500

    ♻️ Update comment-docs-preview-in-pr to not depend on a GitHub workflow_run event

--- .github/actions/comment-docs-preview-in-pr/app/main.py
+++ .github/actions/comment-docs-preview-in-pr/app/main.py
@@ -1,12 +1,11 @@
 import logging
 import sys
-from pathlib import Path
 from typing import Union
 
 import httpx
 from github import Github
 from github.PullRequest import PullRequest
-from pydantic import BaseModel, SecretStr, ValidationError
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 
 github_api = "https://api.github.com"
@@ -14,22 +13,9 @@ github_api = "https://api.github.com"
 
 class Settings(BaseSettings):
     github_repository: str
-    github_event_path: Path
-    github_event_name: Union[str, None] = None
     input_token: SecretStr
     input_deploy_url: str
-
-
-class PartialGithubEventHeadCommit(BaseModel):
-    id: str
-
-
-class PartialGithubEventWorkflowRun(BaseModel):
-    head_commit: PartialGithubEventHeadCommit
-
-
-class PartialGithubEvent(BaseModel):
-    workflow_run: PartialGithubEventWorkflowRun
+    input_commit_sha: str
 
 
 if __name__ == "__main__":
@@ -38,18 +24,13 @@ if __name__ == "__main__":
     logging.info(f"Using config: {settings.json()}")
     g = Github(settings.input_token.get_secret_value())
     repo = g.get_repo(settings.github_repository)
-    try:
-        event = PartialGithubEvent.parse_file(settings.github_event_path)
-    except ValidationError as e:
-        logging.error(f"Error parsing event file: {e.errors()}")
-        sys.exit(0)
     use_pr: Union[PullRequest, None] = None
     for pr in repo.get_pulls():
-        if pr.head.sha == event.workflow_run.head_commit.id:
+        if pr.head.sha == settings.input_commit_sha:
             use_pr = pr
             break
     if not use_pr:
-        logging.error(f"No PR found for hash: {event.workflow_run.head_commit.id}")
+        logging.error(f"No PR found for hash: {settings.input_commit_sha}")
         sys.exit(0)
     github_headers = {
         "Authorization": f"token {settings.input_token.get_secret_value()}"
