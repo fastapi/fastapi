f23c81ceb124e683ebf8507a0b2f0294775947a9
Sebastián Ramírez <tiangolo@gmail.com>
2024-07-31 18:09:57 -0500

    🚚 Move custom GitHub Action to a top-level script

--- scripts/comment_docs_deploy_url_in_pr.py
+++ scripts/comment_docs_deploy_url_in_pr.py
@@ -10,26 +10,26 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     github_repository: str
-    input_token: SecretStr
-    input_deploy_url: str
-    input_commit_sha: str
+    github_token: SecretStr
+    deploy_url: str
+    commit_sha: str
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     settings = Settings()
     logging.info(f"Using config: {settings.json()}")
-    g = Github(settings.input_token.get_secret_value())
+    g = Github(settings.github_token.get_secret_value())
     repo = g.get_repo(settings.github_repository)
     use_pr: Union[PullRequest, None] = None
     for pr in repo.get_pulls():
-        if pr.head.sha == settings.input_commit_sha:
+        if pr.head.sha == settings.commit_sha:
             use_pr = pr
             break
     if not use_pr:
-        logging.error(f"No PR found for hash: {settings.input_commit_sha}")
+        logging.error(f"No PR found for hash: {settings.commit_sha}")
         sys.exit(0)
     use_pr.as_issue().create_comment(
-        f"📝 Docs preview for commit {settings.input_commit_sha} at: {settings.input_deploy_url}"
+        f"📝 Docs preview for commit {settings.commit_sha} at: {settings.deploy_url}"
     )
     logging.info("Finished")
