08134ac19c9380d0b58420d0dcea4a8cbd531c10
Sebastián Ramírez <tiangolo@gmail.com>
2024-07-31 18:34:49 -0500

    ♻️ Refactor and simplify types and loop

--- scripts/comment_docs_deploy_url_in_pr.py
+++ scripts/comment_docs_deploy_url_in_pr.py
@@ -1,9 +1,7 @@
 import logging
 import sys
-from typing import Union
 
 from github import Github
-from github.PullRequest import PullRequest
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 
@@ -21,11 +19,9 @@ if __name__ == "__main__":
     logging.info(f"Using config: {settings.json()}")
     g = Github(settings.github_token.get_secret_value())
     repo = g.get_repo(settings.github_repository)
-    use_pr: Union[PullRequest, None] = None
-    for pr in repo.get_pulls():
-        if pr.head.sha == settings.commit_sha:
-            use_pr = pr
-            break
+    use_pr = next(
+        (pr for pr in repo.get_pulls() if pr.head.sha == settings.commit_sha), None
+    )
     if not use_pr:
         logging.error(f"No PR found for hash: {settings.commit_sha}")
         sys.exit(0)
