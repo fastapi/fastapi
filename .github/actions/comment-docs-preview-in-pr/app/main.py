3d5043ff5e8b578e0c45511e3507e21f86a12ed2
Sebastián Ramírez <tiangolo@gmail.com>
2024-07-31 17:51:41 -0500

    ♻️ Simplify Python code to comment on PR

--- .github/actions/comment-docs-preview-in-pr/app/main.py
+++ .github/actions/comment-docs-preview-in-pr/app/main.py
@@ -2,13 +2,10 @@ import logging
 import sys
 from typing import Union
 
-import httpx
 from github import Github
 from github.PullRequest import PullRequest
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings
-
-github_api = "https://api.github.com"
 
 
 class Settings(BaseSettings):
@@ -32,19 +29,7 @@ if __name__ == "__main__":
     if not use_pr:
         logging.error(f"No PR found for hash: {settings.input_commit_sha}")
         sys.exit(0)
-    github_headers = {
-        "Authorization": f"token {settings.input_token.get_secret_value()}"
-    }
-    url = f"{github_api}/repos/{settings.github_repository}/issues/{use_pr.number}/comments"
-    logging.info(f"Using comments URL: {url}")
-    response = httpx.post(
-        url,
-        headers=github_headers,
-        json={
-            "body": f"📝 Docs preview for commit {use_pr.head.sha} at: {settings.input_deploy_url}"
-        },
+    use_pr.as_issue().create_comment(
+        f"📝 Docs preview for commit {settings.input_commit_sha} at: {settings.input_deploy_url}"
     )
-    if not (200 <= response.status_code <= 300):
-        logging.error(f"Error posting comment: {response.text}")
-        sys.exit(1)
     logging.info("Finished")
