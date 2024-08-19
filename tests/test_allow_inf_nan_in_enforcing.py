975d0adb9c12c4de8214d13fbc0c438b37396144
svlandeg <svlandeg@github.com>
2024-08-19 15:42:02 +0200

    extend and parametrize the tests

--- tests/test_allow_inf_nan_in_enforcing.py
+++ tests/test_allow_inf_nan_in_enforcing.py
@@ -1,5 +1,4 @@
-from typing import Union
-
+import pytest
 from fastapi import Body, FastAPI, Query
 from fastapi.testclient import TestClient
 from typing_extensions import Annotated
@@ -9,9 +8,10 @@ app = FastAPI()
 
 @app.post("/")
 async def get(
-    x: Annotated[Union[float, None], Query(gt=0, description="x")] = 1,
-    y: Annotated[Union[float, None], Query(allow_inf_nan=False, description="y")] = 0,
-    z: Annotated[Union[float, None], Body(allow_inf_nan=False, description="z")] = 0,
+    x: Annotated[float, Query(allow_inf_nan=True)] = 0,
+    y: Annotated[float, Query(allow_inf_nan=False)] = 0,
+    z: Annotated[float, Query()] = 0,
+    b: Annotated[float, Body(allow_inf_nan=False)] = 0,
 ) -> str:
     return "OK"
 
@@ -19,24 +19,65 @@ async def get(
 client = TestClient(app)
 
 
-def test_allow_inf_nan_not_ignored_when_enforcing_params():
-    response = client.post("/?x=-1")
-    assert response.status_code == 422, response.text
+@pytest.mark.parametrize(
+    "value,code",
+    [
+        ("-1", 200),
+        ("inf", 200),
+        ("-inf", 200),
+        ("nan", 200),
+        ("0", 200),
+        ("342", 200),
+    ],
+)
+def test_allow_inf_nan_param_true(value: str, code: int):
+    response = client.post(f"/?x={value}")
+    assert response.status_code == code, response.text
 
-    response = client.post("/?y=inf")
-    assert response.status_code == 422, response.text
 
-    response = client.post("/?y=5")
-    assert response.status_code == 200, response.text
+@pytest.mark.parametrize(
+    "value,code",
+    [
+        ("-1", 200),
+        ("inf", 422),
+        ("-inf", 422),
+        ("nan", 422),
+        ("0", 200),
+        ("342", 200),
+    ],
+)
+def test_allow_inf_nan_param_false(value: str, code: int):
+    response = client.post(f"/?y={value}")
+    assert response.status_code == code, response.text
 
 
-def test_allow_inf_nan_not_ignored_when_enforcing_body():
-    response = client.post("/", json="nan")
-    assert response.status_code == 422, response.text
-    print(response.text)
+@pytest.mark.parametrize(
+    "value,code",
+    [
+        ("-1", 200),
+        ("inf", 200),
+        ("-inf", 200),
+        ("nan", 200),
+        ("0", 200),
+        ("342", 200),
+    ],
+)
+def test_allow_inf_nan_param_default(value: str, code: int):
+    response = client.post(f"/?z={value}")
+    assert response.status_code == code, response.text
 
-    response = client.post("/", json="inf")
-    assert response.status_code == 422, response.text
 
-    response = client.post("/", json="4")
-    assert response.status_code == 200, response.text
+@pytest.mark.parametrize(
+    "value,code",
+    [
+        ("-1", 200),
+        ("inf", 422),
+        ("-inf", 422),
+        ("nan", 422),
+        ("0", 200),
+        ("342", 200),
+    ],
+)
+def test_allow_inf_nan_body(value: str, code: int):
+    response = client.post("/", json=value)
+    assert response.status_code == code, response.text
