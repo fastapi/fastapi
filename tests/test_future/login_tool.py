9413db6bd9a69f0829334ea93e38f564b8b82ad6
svlandeg <svlandeg@github.com>
2024-08-23 17:00:35 +0200

    remove unnecessary login print

--- tests/test_future/login_tool.py
+++ tests/test_future/login_tool.py
@@ -4,7 +4,7 @@ from functools import wraps
 def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print("login")
+        # login functionality could come here
         return func(*args, **kwargs)
 
     return wrapper
