7c7e763aaf7a8cb643ae426d9a2393701a20cf13
Sebastián Ramírez <tiangolo@gmail.com>
2024-09-02 21:17:59 +0200

    ♻️ Do not override ModelFields.field_info.embed, use a top-level setting per route

--- fastapi/dependencies/utils.py
+++ fastapi/dependencies/utils.py
@@ -744,8 +744,7 @@ async def request_body_to_args(
     if body_fields:
         field = body_fields[0]
         field_info = field.field_info
-        embed = getattr(field_info, "embed", None)
-        field_alias_omitted = len(body_fields) == 1 and not embed
+        field_alias_omitted = len(body_fields) == 1 and not embed_body_fields
         if field_alias_omitted:
             received_body = {field.alias: received_body}
 
@@ -833,16 +832,9 @@ def get_body_field(
     if not flat_dependant.body_params:
         return None
     first_param = flat_dependant.body_params[0]
-    field_info = first_param.field_info
-    embed = getattr(field_info, "embed", None)
     body_param_names_set = {param.name for param in flat_dependant.body_params}
-    if len(body_param_names_set) == 1 and not embed:
+    if len(body_param_names_set) == 1 and not embed_body_fields:
         return first_param
-    # If one field requires to embed, all have to be embedded
-    # in case a sub-dependency is evaluated with a single unique body field
-    # That is combined (embedded) with other body fields
-    for param in flat_dependant.body_params:
-        setattr(param.field_info, "embed", True)  # noqa: B010
     model_name = "Body_" + name
     BodyModel = create_body_model(
         fields=flat_dependant.body_params, model_name=model_name
