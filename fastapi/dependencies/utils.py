5adfe7184d50a0829cc7e7f281b80ce8c95e0278
Sebastián Ramírez <tiangolo@gmail.com>
2024-09-03 21:20:50 +0200

    ♻️ Refactor `request_body_to_args()` to handle single not embedded field apart

--- fastapi/dependencies/utils.py
+++ fastapi/dependencies/utils.py
@@ -813,32 +813,31 @@ async def request_body_to_args(
     errors: List[Dict[str, Any]] = []
     if not body_fields:
         return values, errors
-    first_field = body_fields[0]
     single_not_embedded_field = len(body_fields) == 1 and not embed_body_fields
     body_to_process = received_body
     if isinstance(received_body, FormData):
         body_to_process = await _extract_form_body(body_fields, received_body)
 
+    if single_not_embedded_field:
+        first_field = body_fields[0]
+        loc = ("body",)
+        v_, errors_ = _validate_value_with_model_field(
+            field=first_field, value=body_to_process, values=values, loc=loc
+        )
+        return {first_field.name: v_}, errors_
     for field in body_fields:
-        loc: Tuple[str, ...]
-        if single_not_embedded_field:
-            loc = ("body",)
-            v_, errors_ = _validate_value_with_model_field(
-                field=first_field, value=body_to_process, values=values, loc=loc
-            )
-        else:
-            loc = ("body", field.alias)
-            value: Optional[Any] = None
-            if body_to_process is not None:
-                try:
-                    value = body_to_process.get(field.alias)
-                except AttributeError:
-                    errors.append(get_missing_field_error(loc))
-                    continue
-            v_, errors_ = _validate_value_with_model_field(
-                field=field, value=value, values=values, loc=loc
-            )
-
+        loc = ("body", field.alias)
+        value: Optional[Any] = None
+        if body_to_process is not None:
+            try:
+                value = body_to_process.get(field.alias)
+            # If the received body is a list, not a dict
+            except AttributeError:
+                errors.append(get_missing_field_error(loc))
+                continue
+        v_, errors_ = _validate_value_with_model_field(
+            field=field, value=value, values=values, loc=loc
+        )
         if errors_:
             errors.extend(errors_)
         else:
