49ca08a16a8632953d78257480a67fef88522780
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-09-07 03:59:22 +0600

    Unnecessary use `getattr`

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -1259,7 +1259,7 @@ class APIRouter(routing.Router):
             ), "A path prefix must not end with '/', as the routes will start with '/'"
         else:
             for r in router.routes:
-                path = getattr(r, "path")  # noqa: B009
+                path = r.path  # noqa: B009
                 name = getattr(r, "name", "unknown")
                 if path is not None and not path:
                     raise FastAPIError(
