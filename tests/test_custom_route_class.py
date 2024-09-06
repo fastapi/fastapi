bea0725a30acf96a7a14b358f1d86756445777c7
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-09-07 04:03:08 +0600

    Update test_custom_route_class.py

--- tests/test_custom_route_class.py
+++ tests/test_custom_route_class.py
@@ -66,9 +66,9 @@ def test_route_classes():
     for r in app.router.routes:
         assert isinstance(r, Route)
         routes[r.path] = r
-    assert getattr(routes["/a/"], "x_type") == "A"  # noqa: B009
-    assert getattr(routes["/a/b/"], "x_type") == "B"  # noqa: B009
-    assert getattr(routes["/a/b/c/"], "x_type") == "C"  # noqa: B009
+    assert routes["/a/"].x_type == "A"
+    assert routes["/a/b/"].x_type == "B"  # noqa: B009
+    assert routes["/a/b/c/"].x_type == "C"  # noqa: B009
 
 
 def test_openapi_schema():
