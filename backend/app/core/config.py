d2020c1a37efd368afee4d3e56897fc846614f80
Sebastián Ramírez <tiangolo@gmail.com>
2024-09-23 22:09:11 +0200

    ♻️ Update settings to use top level `.env` file (#1359)

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -25,7 +25,10 @@ def parse_cors(v: Any) -> list[str] | str:
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_ignore_empty=True, extra="ignore"
+        # Use top level .env file (one level above ./backend/)
+        env_file="../.env",
+        env_ignore_empty=True,
+        extra="ignore",
     )
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
