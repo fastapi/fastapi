9a173e6f16ea2300386bd11a82c2436b4e336637
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2024-10-05 21:00:13 +0500

    rollback

--- fastapi/_compat.py
+++ fastapi/_compat.py
@@ -156,6 +156,12 @@ def get_schema_from_model_field(
     )
     # This expects that GenerateJsonSchema was already used to generate the definitions
     json_schema = field_mapping[(field, override_mode or field.mode)]
+    if "$ref" not in json_schema:
+        # TODO remove when deprecating Pydantic v1
+        # Ref: https://github.com/pydantic/pydantic/blob/d61792cc42c80b13b23e3ffa74bc37ec7c77f7d1/pydantic/schema.py#L207
+        json_schema["title"] = field.field_info.title or field.alias.title().replace(
+            "_", " "
+        )
     return json_schema
 
 
