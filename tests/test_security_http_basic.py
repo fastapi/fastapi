c3904fdc8ca781ac0c54c956b6dc21e1a280a0eb
Filip Palmqvist <fille.palmqvist@icloud.com>
2024-10-23 07:36:45 +0200

    fixed copy paste coverage miss

--- tests/test_security_http_basic.py
+++ tests/test_security_http_basic.py
@@ -12,8 +12,6 @@ security = HTTPBasic()
 
 @app.get("/users/me")
 def read_current_user(credentials: Optional[HTTPBasicCredentials] = Security(security)):
-    if credentials is None:
-        return {"msg": "Create an account first"}
     return {"username": credentials.username, "password": credentials.password}
 
 
