f0f83d3963f418de30ad2a20a76dc7b3e4e5dce9
Abdul Hadi <ahadi.bese17seecs@seecs.edu.pk>
2024-10-27 15:04:26 +0500

    ensure_multipart_is_installed simplified

--- fastapi/dependencies/utils.py
+++ fastapi/dependencies/utils.py
@@ -90,18 +90,8 @@ multipart_incorrect_install_error = (
 
 def ensure_multipart_is_installed() -> None:
     try:
-        # __version__ is available in both multiparts, and can be mocked
-        from multipart import __version__  # type: ignore[import-untyped]
-
+        from python_multipart import __version__
         assert __version__
-        try:
-            # parse_options_header is only available in the right multipart
-            from python_multipart.multipart import parse_options_header
-
-            assert parse_options_header  # type: ignore[truthy-function]
-        except ImportError:
-            logger.error(multipart_incorrect_install_error)
-            raise RuntimeError(multipart_incorrect_install_error) from None
     except ImportError:
         logger.error(multipart_not_installed_error)
         raise RuntimeError(multipart_not_installed_error) from None
