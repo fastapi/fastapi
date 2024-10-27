eec436837b91853b03f47e35f6018db3c3c37d8f
Sebastián Ramírez <tiangolo@gmail.com>
2024-10-27 21:41:12 +0000

    ✅ Add test for coverage

--- tests/test_multipart_installation.py
+++ tests/test_multipart_installation.py
@@ -136,3 +136,14 @@ def test_no_multipart_installed_form_file(monkeypatch):
             @app.post("/")
             async def root(username: str = Form(), f: UploadFile = File()):
                 return username  # pragma: nocover
+
+
+def test_old_multipart_installed(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        app = FastAPI()
+
+        @app.post("/")
+        async def root(username: str = Form()):
+            return username  # pragma: nocover
