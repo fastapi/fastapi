de16e923bda3b98941be24e4743e32aa8a08ccc8
Sebastián Ramírez <tiangolo@gmail.com>
2024-10-27 15:59:37 +0000

    ✅ Update tests for python-multipart

--- tests/test_multipart_installation.py
+++ tests/test_multipart_installation.py
@@ -1,63 +1,12 @@
 import pytest
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.dependencies.utils import (
-    multipart_incorrect_install_error,
     multipart_not_installed_error,
 )
 
 
-def test_incorrect_multipart_installed_form(monkeypatch):
-    monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
-        app = FastAPI()
-
-        @app.post("/")
-        async def root(username: str = Form()):
-            return username  # pragma: nocover
-
-
-def test_incorrect_multipart_installed_file_upload(monkeypatch):
-    monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
-        app = FastAPI()
-
-        @app.post("/")
-        async def root(f: UploadFile = File()):
-            return f  # pragma: nocover
-
-
-def test_incorrect_multipart_installed_file_bytes(monkeypatch):
-    monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
-        app = FastAPI()
-
-        @app.post("/")
-        async def root(f: bytes = File()):
-            return f  # pragma: nocover
-
-
-def test_incorrect_multipart_installed_multi_form(monkeypatch):
-    monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
-        app = FastAPI()
-
-        @app.post("/")
-        async def root(username: str = Form(), password: str = Form()):
-            return username  # pragma: nocover
-
-
-def test_incorrect_multipart_installed_form_file(monkeypatch):
-    monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
-        app = FastAPI()
-
-        @app.post("/")
-        async def root(username: str = Form(), f: UploadFile = File()):
-            return username  # pragma: nocover
-
-
 def test_no_multipart_installed(monkeypatch):
-    monkeypatch.delattr("multipart.__version__", raising=False)
+    monkeypatch.delattr("python_multipart.__version__", raising=False)
     with pytest.raises(RuntimeError, match=multipart_not_installed_error):
         app = FastAPI()
 
@@ -67,7 +16,7 @@ def test_no_multipart_installed(monkeypatch):
 
 
 def test_no_multipart_installed_file(monkeypatch):
-    monkeypatch.delattr("multipart.__version__", raising=False)
+    monkeypatch.delattr("python_multipart.__version__", raising=False)
     with pytest.raises(RuntimeError, match=multipart_not_installed_error):
         app = FastAPI()
 
@@ -77,7 +26,7 @@ def test_no_multipart_installed_file(monkeypatch):
 
 
 def test_no_multipart_installed_file_bytes(monkeypatch):
-    monkeypatch.delattr("multipart.__version__", raising=False)
+    monkeypatch.delattr("python_multipart.__version__", raising=False)
     with pytest.raises(RuntimeError, match=multipart_not_installed_error):
         app = FastAPI()
 
@@ -87,7 +36,7 @@ def test_no_multipart_installed_file_bytes(monkeypatch):
 
 
 def test_no_multipart_installed_multi_form(monkeypatch):
-    monkeypatch.delattr("multipart.__version__", raising=False)
+    monkeypatch.delattr("python_multipart.__version__", raising=False)
     with pytest.raises(RuntimeError, match=multipart_not_installed_error):
         app = FastAPI()
 
@@ -97,7 +46,7 @@ def test_no_multipart_installed_multi_form(monkeypatch):
 
 
 def test_no_multipart_installed_form_file(monkeypatch):
-    monkeypatch.delattr("multipart.__version__", raising=False)
+    monkeypatch.delattr("python_multipart.__version__", raising=False)
     with pytest.raises(RuntimeError, match=multipart_not_installed_error):
         app = FastAPI()
 
