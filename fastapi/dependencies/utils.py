5269b1868428e562b4c4161f4d46e3b477cbff96
Sebastián Ramírez <tiangolo@gmail.com>
2024-10-27 21:19:45 +0000

    ♻️ Update import and check multipart with compatibility for new python-multipart

--- fastapi/dependencies/utils.py
+++ fastapi/dependencies/utils.py
@@ -74,9 +74,17 @@ from starlette.websockets import WebSocket
 from typing_extensions import Annotated, get_args, get_origin
 
 multipart_not_installed_error = (
-    'Form data requires a recent version of "python-multipart" to be installed. \n'
+    'Form data requires "python-multipart" to be installed. \n'
     'You can install "python-multipart" with: \n\n'
-    "pip install --upgrade python-multipart\n"
+    "pip install python-multipart\n"
+)
+multipart_incorrect_install_error = (
+    'Form data requires "python-multipart" to be installed. '
+    'It seems you installed "multipart" instead. \n'
+    'You can remove "multipart" with: \n\n'
+    "pip uninstall multipart\n\n"
+    'And then install "python-multipart" with: \n\n'
+    "pip install python-multipart\n"
 )
 
 
@@ -85,10 +93,24 @@ def ensure_multipart_is_installed() -> None:
         from python_multipart import __version__
 
         # Import an attribute that can be mocked/deleted in testing
-        assert __version__
-    except ImportError:
-        logger.error(multipart_not_installed_error)
-        raise RuntimeError(multipart_not_installed_error) from None
+        assert __version__ > "0.0.12"
+    except (ImportError, AssertionError):
+        try:
+            # __version__ is available in both multiparts, and can be mocked
+            from multipart import __version__
+
+            assert __version__
+            try:
+                # parse_options_header is only available in the right multipart
+                from multipart.multipart import parse_options_header
+
+                assert parse_options_header  # type: ignore[truthy-function]
+            except ImportError:
+                logger.error(multipart_incorrect_install_error)
+                raise RuntimeError(multipart_incorrect_install_error) from None
+        except ImportError:
+            logger.error(multipart_not_installed_error)
+            raise RuntimeError(multipart_not_installed_error) from None
 
 
 def get_param_sub_dependant(
