2c681a71a6ea9820b9522073538eacde47f81409
Sebastián Ramírez <tiangolo@gmail.com>
2024-10-27 21:19:28 +0000

    ✅ Update tests

--- tests/test_multipart_installation.py
+++ tests/test_multipart_installation.py
@@ -1,13 +1,19 @@
+import warnings
+
 import pytest
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.dependencies.utils import (
+    multipart_incorrect_install_error,
     multipart_not_installed_error,
 )
 
 
-def test_no_multipart_installed(monkeypatch):
-    monkeypatch.delattr("python_multipart.__version__", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+def test_incorrect_multipart_installed_form(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
+    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
         app = FastAPI()
 
         @app.post("/")
@@ -15,9 +21,12 @@ def test_no_multipart_installed(monkeypatch):
             return username  # pragma: nocover
 
 
-def test_no_multipart_installed_file(monkeypatch):
-    monkeypatch.delattr("python_multipart.__version__", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+def test_incorrect_multipart_installed_file_upload(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
+    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
         app = FastAPI()
 
         @app.post("/")
@@ -25,9 +34,12 @@ def test_no_multipart_installed_file(monkeypatch):
             return f  # pragma: nocover
 
 
-def test_no_multipart_installed_file_bytes(monkeypatch):
-    monkeypatch.delattr("python_multipart.__version__", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+def test_incorrect_multipart_installed_file_bytes(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
+    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
         app = FastAPI()
 
         @app.post("/")
@@ -35,9 +47,12 @@ def test_no_multipart_installed_file_bytes(monkeypatch):
             return f  # pragma: nocover
 
 
-def test_no_multipart_installed_multi_form(monkeypatch):
-    monkeypatch.delattr("python_multipart.__version__", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+def test_incorrect_multipart_installed_multi_form(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
+    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
         app = FastAPI()
 
         @app.post("/")
@@ -45,11 +60,79 @@ def test_no_multipart_installed_multi_form(monkeypatch):
             return username  # pragma: nocover
 
 
-def test_no_multipart_installed_form_file(monkeypatch):
-    monkeypatch.delattr("python_multipart.__version__", raising=False)
-    with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+def test_incorrect_multipart_installed_form_file(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.multipart.parse_options_header", raising=False)
+    with pytest.raises(RuntimeError, match=multipart_incorrect_install_error):
         app = FastAPI()
 
         @app.post("/")
         async def root(username: str = Form(), f: UploadFile = File()):
             return username  # pragma: nocover
+
+
+def test_no_multipart_installed(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.__version__", raising=False)
+        with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+            app = FastAPI()
+
+            @app.post("/")
+            async def root(username: str = Form()):
+                return username  # pragma: nocover
+
+
+def test_no_multipart_installed_file(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.__version__", raising=False)
+        with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+            app = FastAPI()
+
+            @app.post("/")
+            async def root(f: UploadFile = File()):
+                return f  # pragma: nocover
+
+
+def test_no_multipart_installed_file_bytes(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.__version__", raising=False)
+        with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+            app = FastAPI()
+
+            @app.post("/")
+            async def root(f: bytes = File()):
+                return f  # pragma: nocover
+
+
+def test_no_multipart_installed_multi_form(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.__version__", raising=False)
+        with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+            app = FastAPI()
+
+            @app.post("/")
+            async def root(username: str = Form(), password: str = Form()):
+                return username  # pragma: nocover
+
+
+def test_no_multipart_installed_form_file(monkeypatch):
+    monkeypatch.setattr("python_multipart.__version__", "0.0.12")
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        monkeypatch.delattr("multipart.__version__", raising=False)
+        with pytest.raises(RuntimeError, match=multipart_not_installed_error):
+            app = FastAPI()
+
+            @app.post("/")
+            async def root(username: str = Form(), f: UploadFile = File()):
+                return username  # pragma: nocover
