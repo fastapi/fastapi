85a538b8e24805fdc48600469512da675a981f09
Michael Lin <60127987+michaelwlin@users.noreply.github.com>
2024-12-04 11:24:23 -0800

    Fix: Remove unnecessary whitespace flagged by flake8

--- docs_src/dependencies/tutorial002_an_py39.py
+++ docs_src/dependencies/tutorial002_an_py39.py
@@ -20,6 +20,6 @@ async def read_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryPa
     response = {}
     if commons.q:
         response.update({"q": commons.q})
-    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    items = fake_items_db[commons.skip:commons.skip + commons.limit]
     response.update({"items": items})
     return response
--- docs_src/generate_clients/tutorial004.py
+++ docs_src/generate_clients/tutorial004.py
@@ -9,7 +9,7 @@ for path_data in openapi_content["paths"].values():
         tag = operation["tags"][0]
         operation_id = operation["operationId"]
         to_remove = f"{tag}-"
-        new_operation_id = operation_id[len(to_remove) :]
+        new_operation_id = operation_id[len(to_remove):]
         operation["operationId"] = new_operation_id
 
 file_path.write_text(json.dumps(openapi_content))
