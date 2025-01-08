119f4dfa08a34dd7059583261c2748922408a418
User <alejsdev@gmail.com>
2025-01-08 19:03:07 +0000

    Update tests

--- tests/test_tutorial/test_request_files/test_tutorial003.py
+++ tests/test_tutorial/test_request_files/test_tutorial003.py
@@ -1,11 +1,46 @@
+import importlib
+
+import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from docs_src.request_files.tutorial003 import app
-
-client = TestClient(app)
+from ...utils import needs_py39
 
 
-def test_post_files(tmp_path):
+@pytest.fixture(
+    name="app",
+    params=[
+        "tutorial003",
+        "tutorial003_an",
+        pytest.param("tutorial003_py39", marks=needs_py39),
+        pytest.param("tutorial003_an_py39", marks=needs_py39),
+    ],
+)
+def get_app(request: pytest.FixtureRequest):
+    mod = importlib.import_module(f"docs_src.request_files.{request.param}")
+
+    return mod.app
+
+
+@pytest.fixture(name="client")
+def get_client(app: FastAPI):
+    client = TestClient(app)
+    return client
+
+
+file_required = {
+    "detail": [
+        {
+            "loc": ["body", "files"],
+            "msg": "field required",
+            "type": "value_error.missing",
+        }
+    ]
+}
+
+
+@needs_py39
+def test_post_files(tmp_path, app: FastAPI):
     path = tmp_path / "test.txt"
     path.write_bytes(b"<file content>")
     path2 = tmp_path / "test2.txt"
@@ -24,7 +59,8 @@ def test_post_files(tmp_path):
     assert response.json() == {"file_sizes": [14, 15]}
 
 
-def test_post_upload_file(tmp_path):
+@needs_py39
+def test_post_upload_file(tmp_path, app: FastAPI):
     path = tmp_path / "test.txt"
     path.write_bytes(b"<file content>")
     path2 = tmp_path / "test2.txt"
@@ -43,14 +79,16 @@ def test_post_upload_file(tmp_path):
     assert response.json() == {"filenames": ["test.txt", "test2.txt"]}
 
 
-def test_get_root():
+@needs_py39
+def test_get_root(app: FastAPI):
     client = TestClient(app)
     response = client.get("/")
     assert response.status_code == 200, response.text
     assert b"<form" in response.content
 
 
-def test_openapi_schema():
+@needs_py39
+def test_openapi_schema(client: TestClient):
     response = client.get("/openapi.json")
     assert response.status_code == 200, response.text
     assert response.json() == {
