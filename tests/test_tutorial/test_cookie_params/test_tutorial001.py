ad2bc66d753c10541ff7f932ec231c4e3a75a461
User <alejsdev@gmail.com>
2025-01-08 11:01:30 +0000

    Update

--- tests/test_tutorial/test_cookie_params/test_tutorial001.py
+++ tests/test_tutorial/test_cookie_params/test_tutorial001.py
@@ -39,7 +39,7 @@ def get_mod(request: pytest.FixtureRequest):
     ],
 )
 def test(path, cookies, expected_status, expected_response, mod: ModuleType):
-    client = TestClient(mod.app, cookies)
+    client = TestClient(mod.app, cookies=cookies)
     response = client.get(path)
     assert response.status_code == expected_status
     assert response.json() == expected_response
