7adbb1ff016ed194f998d85b5cedcddb2935e7a2
User <alejsdev@gmail.com>
2025-01-12 19:06:58 +0000

    Update

--- tests/test_tutorial/test_schema_extra_example/test_tutorial001_pv1.py
+++ tests/test_tutorial/test_schema_extra_example/test_tutorial001_pv1.py
@@ -14,7 +14,7 @@ from ...utils import needs_py310, needs_pydanticv1
     ],
 )
 def get_client(request: pytest.FixtureRequest):
-    mod = importlib.import_module(f"docs.src.schema_extra_example.{request.param}")
+    mod = importlib.import_module(f"docs_src.schema_extra_example.{request.param}")
 
     client = TestClient(mod.app)
     return client
