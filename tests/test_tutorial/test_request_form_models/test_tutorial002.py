10037ca9baf8f563591901f63a347a88d1ce1212
Alejandra <90076947+alejsdev@users.noreply.github.com>
2025-01-13 13:23:40 +0000

    Update tests/test_tutorial/test_request_form_models/test_tutorial002.py
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- tests/test_tutorial/test_request_form_models/test_tutorial002.py
+++ tests/test_tutorial/test_request_form_models/test_tutorial002.py
@@ -3,7 +3,7 @@ import importlib
 import pytest
 from fastapi.testclient import TestClient
 
-from tests.utils import needs_py39, needs_pydanticv2
+from ...utils import needs_py39, needs_pydanticv2
 
 
 @pytest.fixture(
