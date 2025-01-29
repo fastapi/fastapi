813d2369ce21c7386c54f0bb642d718f2a17d5bb
Sebastián Ramírez <tiangolo@gmail.com>
2025-01-29 13:35:16 +0000

    ♻️ Refactor tests for websockets to use a context manager client

--- tests/test_tutorial/test_websockets/test_tutorial002.py
+++ tests/test_tutorial/test_websockets/test_tutorial002.py
@@ -32,76 +32,80 @@ def test_main(app: FastAPI):
 
 
 def test_websocket_with_cookie(app: FastAPI):
-    client = TestClient(app, cookies={"session": "fakesession"})
-    with pytest.raises(WebSocketDisconnect):
-        with client.websocket_connect("/items/foo/ws") as websocket:
-            message = "Message one"
-            websocket.send_text(message)
-            data = websocket.receive_text()
-            assert data == "Session cookie or query token value is: fakesession"
-            data = websocket.receive_text()
-            assert data == f"Message text was: {message}, for item ID: foo"
-            message = "Message two"
-            websocket.send_text(message)
-            data = websocket.receive_text()
-            assert data == "Session cookie or query token value is: fakesession"
-            data = websocket.receive_text()
-            assert data == f"Message text was: {message}, for item ID: foo"
+    with TestClient(app, cookies={"session": "fakesession"}) as client:
+        with pytest.raises(WebSocketDisconnect):
+            with client.websocket_connect("/items/foo/ws") as websocket:
+                message = "Message one"
+                websocket.send_text(message)
+                data = websocket.receive_text()
+                assert data == "Session cookie or query token value is: fakesession"
+                data = websocket.receive_text()
+                assert data == f"Message text was: {message}, for item ID: foo"
+                message = "Message two"
+                websocket.send_text(message)
+                data = websocket.receive_text()
+                assert data == "Session cookie or query token value is: fakesession"
+                data = websocket.receive_text()
+                assert data == f"Message text was: {message}, for item ID: foo"
 
 
 def test_websocket_with_header(app: FastAPI):
-    client = TestClient(app)
-    with pytest.raises(WebSocketDisconnect):
-        with client.websocket_connect("/items/bar/ws?token=some-token") as websocket:
-            message = "Message one"
-            websocket.send_text(message)
-            data = websocket.receive_text()
-            assert data == "Session cookie or query token value is: some-token"
-            data = websocket.receive_text()
-            assert data == f"Message text was: {message}, for item ID: bar"
-            message = "Message two"
-            websocket.send_text(message)
-            data = websocket.receive_text()
-            assert data == "Session cookie or query token value is: some-token"
-            data = websocket.receive_text()
-            assert data == f"Message text was: {message}, for item ID: bar"
+    with TestClient(app) as client:
+        with pytest.raises(WebSocketDisconnect):
+            with client.websocket_connect(
+                "/items/bar/ws?token=some-token"
+            ) as websocket:
+                message = "Message one"
+                websocket.send_text(message)
+                data = websocket.receive_text()
+                assert data == "Session cookie or query token value is: some-token"
+                data = websocket.receive_text()
+                assert data == f"Message text was: {message}, for item ID: bar"
+                message = "Message two"
+                websocket.send_text(message)
+                data = websocket.receive_text()
+                assert data == "Session cookie or query token value is: some-token"
+                data = websocket.receive_text()
+                assert data == f"Message text was: {message}, for item ID: bar"
 
 
 def test_websocket_with_header_and_query(app: FastAPI):
-    client = TestClient(app)
-    with pytest.raises(WebSocketDisconnect):
-        with client.websocket_connect("/items/2/ws?q=3&token=some-token") as websocket:
-            message = "Message one"
-            websocket.send_text(message)
-            data = websocket.receive_text()
-            assert data == "Session cookie or query token value is: some-token"
-            data = websocket.receive_text()
-            assert data == "Query parameter q is: 3"
-            data = websocket.receive_text()
-            assert data == f"Message text was: {message}, for item ID: 2"
-            message = "Message two"
-            websocket.send_text(message)
-            data = websocket.receive_text()
-            assert data == "Session cookie or query token value is: some-token"
-            data = websocket.receive_text()
-            assert data == "Query parameter q is: 3"
-            data = websocket.receive_text()
-            assert data == f"Message text was: {message}, for item ID: 2"
+    with TestClient(app) as client:
+        with pytest.raises(WebSocketDisconnect):
+            with client.websocket_connect(
+                "/items/2/ws?q=3&token=some-token"
+            ) as websocket:
+                message = "Message one"
+                websocket.send_text(message)
+                data = websocket.receive_text()
+                assert data == "Session cookie or query token value is: some-token"
+                data = websocket.receive_text()
+                assert data == "Query parameter q is: 3"
+                data = websocket.receive_text()
+                assert data == f"Message text was: {message}, for item ID: 2"
+                message = "Message two"
+                websocket.send_text(message)
+                data = websocket.receive_text()
+                assert data == "Session cookie or query token value is: some-token"
+                data = websocket.receive_text()
+                assert data == "Query parameter q is: 3"
+                data = websocket.receive_text()
+                assert data == f"Message text was: {message}, for item ID: 2"
 
 
 def test_websocket_no_credentials(app: FastAPI):
-    client = TestClient(app)
-    with pytest.raises(WebSocketDisconnect):
-        with client.websocket_connect("/items/foo/ws"):
-            pytest.fail(
-                "did not raise WebSocketDisconnect on __enter__"
-            )  # pragma: no cover
+    with TestClient(app) as client:
+        with pytest.raises(WebSocketDisconnect):
+            with client.websocket_connect("/items/foo/ws"):
+                pytest.fail(
+                    "did not raise WebSocketDisconnect on __enter__"
+                )  # pragma: no cover
 
 
 def test_websocket_invalid_data(app: FastAPI):
-    client = TestClient(app)
-    with pytest.raises(WebSocketDisconnect):
-        with client.websocket_connect("/items/foo/ws?q=bar&token=some-token"):
-            pytest.fail(
-                "did not raise WebSocketDisconnect on __enter__"
-            )  # pragma: no cover
+    with TestClient(app) as client:
+        with pytest.raises(WebSocketDisconnect):
+            with client.websocket_connect("/items/foo/ws?q=bar&token=some-token"):
+                pytest.fail(
+                    "did not raise WebSocketDisconnect on __enter__"
+                )  # pragma: no cover
