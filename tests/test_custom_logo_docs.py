73d563d95b5b5b5cc78002bcc86cf615d0f933d7
Valentyn Druzhynin <v.druzhynin@zakaz.global>
2025-02-05 18:28:14 -0500

    Fixes after review

--- tests/test_custom_logo_docs.py
+++ tests/test_custom_logo_docs.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-FAVICON_URL = "https://www.google.com/favicon.ico"
+FAVICON_URL = "data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22"
+"http%3A//www.w3.org/2000/svg%22%20width%3D%2232%22%20height%3D%2232%"
+"22%20viewBox%3D%220%200%2032%2032%22%3E%3Ccircle%20cx%3D%2216%22%20c"
+"y%3D%2216%22%20r%3D%2214%22%20fill%3D%22%23009688%22/%3E%3Ctext%20x%"
+"3D%2250%25%22%20y%3D%2255%25%22%20font-size%3D%2216%22%20font-family"
+"%3D%22Arial%2C%20sans-serif%22%20fill%3D%22white%22%20text-anchor%3D"
+"%22middle%22%20dominant-baseline%3D%22middle%22%3EF%3C/text%3E%3C/svg%3E"
 app = FastAPI(favicon_url=FAVICON_URL)
 
 
@@ -16,14 +22,12 @@ client = TestClient(app)
 def test_swagger_ui_loads_custom_logo():
     response = client.get("/docs")
     assert response.status_code == 200, response.text
-    print(response.text)
     assert FAVICON_URL in response.text
 
 
 def test_redoc_ui_loads_custom_logo():
     response = client.get("/redoc")
     assert response.status_code == 200, response.text
-    print(response.text)
     assert FAVICON_URL in response.text
 
 
