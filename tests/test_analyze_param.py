dc0144c3f70159f80e59b20458955152661aa88a
svlandeg <svlandeg@github.com>
2025-02-05 15:41:13 +0100

    fix Annotated import

--- tests/test_analyze_param.py
+++ tests/test_analyze_param.py
@@ -1,16 +1,15 @@
 from inspect import signature
-from typing import Annotated
 
 from fastapi.dependencies.utils import ParamDetails, analyze_param
 from pydantic import Field
+from typing_extensions import Annotated
 
-from .utils import needs_py39, needs_pydanticv2
+from .utils import needs_pydanticv2
 
 
 def func(user: Annotated[int, Field(strict=True)]): ...
 
 
-@needs_py39
 @needs_pydanticv2
 def test_analyze_param():
     result = analyze_param(
