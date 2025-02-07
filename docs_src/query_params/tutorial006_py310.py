29e265b3541d69ef6865b1d6a522464b47c69673
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-02-07 10:49:20 +0100

    no need to assign instantly returned dict

--- docs_src/query_params/tutorial006_py310.py
+++ docs_src/query_params/tutorial006_py310.py
@@ -7,5 +7,4 @@ app = FastAPI()
 async def read_user_item(
     item_id: str, needy: str, skip: int = 0, limit: int | None = None
 ):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+    return {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
