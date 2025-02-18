fba66f4985c3988ad859b165edd7f93e8152421d
Valentyn Druzhynin <v.druzhynin@zakaz.global>
2025-02-18 18:59:45 -0500

    Fix review comments

--- tests/test_custom_logo_docs.py
+++ tests/test_custom_logo_docs.py
@@ -1,22 +1,28 @@
-from pathlib import Path
-
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 TEST_HTTP_FAVICON_URL = "http://favicon-url.com/favicon.png"
 TEST_HTTPS_FAVICON_URL = "http://favicon-url.com/favicon.png"
+LOCAL_FILE_FAVICON_URL = "/static/favicon.ico"
 DEFAULT_FASTAPI_FAVICON = "https://fastapi.tiangolo.com/img/favicon.png"
 
 
-def get_client_with_custom_docs_logo(favicon_url):
-    app = FastAPI(favicon_url=favicon_url)
+@pytest.fixture
+def app_with_custom_docs_logo():
+    def app_wrapper(favicon_url=None, skip_favicon=False):
+        if skip_favicon:
+            app = FastAPI()
+        else:
+            app = FastAPI(favicon_url=favicon_url)
 
-    @app.get("/items/")
-    async def read_items():
-        return {"id": "foo"}
+        @app.get("/items/")
+        async def read_items():
+            return {"id": "foo"}
 
-    return TestClient(app)
+        return app
+
+    return app_wrapper
 
 
 @pytest.mark.parametrize(
@@ -24,11 +30,14 @@ def get_client_with_custom_docs_logo(favicon_url):
     [
         [TEST_HTTP_FAVICON_URL, TEST_HTTP_FAVICON_URL],
         [TEST_HTTPS_FAVICON_URL, TEST_HTTPS_FAVICON_URL],
+        [LOCAL_FILE_FAVICON_URL, LOCAL_FILE_FAVICON_URL],
         [None, DEFAULT_FASTAPI_FAVICON],
     ],
 )
-def test_docs_ui_loads_custom_logo(input_favicon_url, generated_favicon_url):
-    client = get_client_with_custom_docs_logo(input_favicon_url)
+def test_docs_ui_loads_custom_logo(
+    input_favicon_url, generated_favicon_url, app_with_custom_docs_logo
+):
+    client = TestClient(app_with_custom_docs_logo(input_favicon_url))
     response = client.get("/docs")
     redoc_response = client.get("/redoc")
 
@@ -38,22 +47,14 @@ def test_docs_ui_loads_custom_logo(input_favicon_url, generated_favicon_url):
     assert generated_favicon_url in redoc_response.text
 
 
-def test_docs_ui_load_file_as_custom_logo(tmp_path):
-    favicon_filename = "custom-favicon.png"
-    file_path: Path = tmp_path / favicon_filename
-    test_content = b"Fake favicon bytes"
-    file_path.write_bytes(test_content)
-    client = get_client_with_custom_docs_logo(str(file_path))
+def test_docs_ui_loads_default_logo_when_no_logo_set(app_with_custom_docs_logo):
+    client = TestClient(app_with_custom_docs_logo(skip_favicon=True))
     response = client.get("/docs")
     redoc_response = client.get("/redoc")
+    items_response = client.get("/items/")
 
+    assert items_response.json() == {"id": "foo"}
     assert response.status_code == 200, response.text
     assert redoc_response.status_code == 200, redoc_response.text
-    assert favicon_filename in response.text
-    assert favicon_filename in redoc_response.text
-
-
-def test_items_response():
-    client = get_client_with_custom_docs_logo(None)
-    response = client.get("/items/")
-    assert response.json() == {"id": "foo"}
+    assert DEFAULT_FASTAPI_FAVICON in response.text
+    assert DEFAULT_FASTAPI_FAVICON in redoc_response.text
