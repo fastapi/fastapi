a0dcdbff23235fc985fd2b5271a267f82892f294
Kilian Hu <kilian@hu101.com>
2025-02-20 19:21:51 +0100

    Fix test compatibility python < 3.12

--- tests/test_type_alias_depends.py
+++ tests/test_type_alias_depends.py
@@ -1,50 +1,48 @@
 from fastapi import Depends, FastAPI
 from fastapi.testclient import TestClient
-from typing_extensions import Annotated
-
-from .utils import needs_py312
-
-pytestmark = needs_py312
-
-app = FastAPI()
+from typing_extensions import Annotated, TypeAliasType
 
 
 async def get_foo1() -> str:
     return "foo1"
 
 
-async def get_foo2() -> str:
-    return "foo2"
-
-
-async def get_foo3(x: str = "x") -> str:
-    return f"foo3_{x}"
-
-
-async def get_foo4(y: str = "y") -> str:
-    return f"foo4_{y}"
+async def get_foo2(x: str = "x") -> str:
+    return f"foo2_{x}"
 
 
 Foo1 = Annotated[str, Depends(get_foo1)]
-type Foo2 = Annotated[str, Depends(get_foo2)]
-Foo3 = Annotated[str, Depends(get_foo3)]
-type Foo4 = Annotated[str, Depends(get_foo4)]
+Foo2 = Annotated[str, Depends(get_foo2)]
 
 
-@app.get("/")
-async def root(foo1: Foo1, foo2: Foo2, foo3: Foo3, foo4: Foo4):
-    return {"foo1": foo1, "foo2": foo2, "foo3": foo3, "foo4": foo4}
+async def get_bar1() -> str:
+    return "bar1"
 
 
-client = TestClient(app)
+async def get_bar2(y: str = "y") -> str:
+    return f"bar2_{y}"
+
+
+# Equivalent to:
+# type Bar1 = Annotated[str, Depends(get_bar1)]
+# type Bar2 = Annotated[str, Depends(get_bar2)]
+Bar1 = TypeAliasType("Bar1", Annotated[str, Depends(get_bar1)])
+Bar2 = TypeAliasType("Bar2", Annotated[str, Depends(get_bar2)])
 
 
 def test_type_alias_depends():
+    app = FastAPI()
+
+    @app.get("/")
+    async def root(foo1: Foo1, foo2: Foo2, bar1: Bar1, bar2: Bar2):
+        return {"foo1": foo1, "foo2": foo2, "bar1": bar1, "bar2": bar2}
+
+    client = TestClient(app)
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {
         "foo1": "foo1",
-        "foo2": "foo2",
-        "foo3": "foo3_x",
-        "foo4": "foo4_y",
+        "foo2": "foo2_x",
+        "bar1": "bar1",
+        "bar2": "bar2_y",
     }
