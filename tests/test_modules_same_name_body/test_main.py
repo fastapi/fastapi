8747cf497ac49fd77ac7c7b22de422a6072b607a
alv2017 <v.alishauskaite@gmail.com>
2025-02-23 01:40:22 +0200

    fixing a minor bug in test: test_modules_same_name_body

--- tests/test_modules_same_name_body/test_main.py
+++ tests/test_modules_same_name_body/test_main.py
@@ -9,7 +9,7 @@ def test_post_a():
     data = {"a": 2, "b": "foo"}
     response = client.post("/a/compute", json=data)
     assert response.status_code == 200, response.text
-    data = response.json()
+    assert data == response.json()
 
 
 def test_post_a_invalid():
@@ -22,7 +22,7 @@ def test_post_b():
     data = {"a": 2, "b": "foo"}
     response = client.post("/b/compute/", json=data)
     assert response.status_code == 200, response.text
-    data = response.json()
+    assert data == response.json()
 
 
 def test_post_b_invalid():
