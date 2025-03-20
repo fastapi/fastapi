6670b3761ac2df01d1e7334c2f702b829c1d6681
Valentyn Druzhynin <v.druzhynin@zakaz.global>
2025-03-19 22:59:31 -0400

    Simplify tests for websockets tutorial #13150

--- tests/test_tutorial/test_websockets/test_tutorial003.py
+++ tests/test_tutorial/test_websockets/test_tutorial003.py
@@ -1,16 +1,33 @@
+import importlib
+
+import pytest
 from fastapi.testclient import TestClient
 
-from docs_src.websockets.tutorial003 import app, html
+from docs_src.websockets.tutorial003 import html
 
-client = TestClient(app)
+from ...utils import needs_py39
 
 
-def test_get():
+@pytest.fixture(
+    name="app",
+    params=[
+        "tutorial003",
+        pytest.param("tutorial003_py39", marks=needs_py39),
+    ],
+)
+def get_app(request: pytest.FixtureRequest):
+    mod = importlib.import_module(f"docs_src.websockets.{request.param}")
+    return mod.app
+
+
+def test_get(app):
+    client = TestClient(app)
     response = client.get("/")
     assert response.text == html
 
 
-def test_websocket_handle_disconnection():
+def test_websocket_handle_disconnection(app):
+    client = TestClient(app)
     with client.websocket_connect("/ws/1234") as connection, client.websocket_connect(
         "/ws/5678"
     ) as connection_two:
