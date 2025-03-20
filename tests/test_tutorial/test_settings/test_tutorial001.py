0d881322db9ca13bcd1f2a21b60277a1b42c9462
Valentyn Druzhynin <v.druzhynin@zakaz.global>
2025-03-19 23:14:18 -0400

    Simplify tests for settings tutorial #13504

--- tests/test_tutorial/test_settings/test_tutorial001.py
+++ tests/test_tutorial/test_settings/test_tutorial001.py
@@ -1,14 +1,30 @@
+import importlib
+
+import pytest
 from fastapi.testclient import TestClient
 from pytest import MonkeyPatch
 
-from ...utils import needs_pydanticv2
+from ...utils import needs_pydanticv1, needs_pydanticv2
 
 
-@needs_pydanticv2
-def test_settings(monkeypatch: MonkeyPatch):
+@pytest.fixture(
+    params=[
+        pytest.param("tutorial001", marks=needs_pydanticv2),
+        pytest.param("tutorial001_pv1", marks=needs_pydanticv1),
+    ],
+)
+def get_app(request: pytest.FixtureRequest):
+    def app_wrapper():
+        mod = importlib.import_module(f"docs_src.settings.{request.param}")
+        return mod.app
+
+    return app_wrapper
+
+
+def test_settings(get_app, monkeypatch: MonkeyPatch):
     monkeypatch.setenv("ADMIN_EMAIL", "admin@example.com")
-    from docs_src.settings.tutorial001 import app
 
+    app = get_app()
     client = TestClient(app)
     response = client.get("/info")
     assert response.status_code == 200, response.text
