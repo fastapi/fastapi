64225070595d5038a871d149a47240e2283e6a6b
iwannaefraimia <t8210054@aueb.gr>
2025-05-04 20:05:05 +0300

    Move tests in a new file

--- tests_export/test_export.py
+++ tests_export/test_export.py
@@ -17,10 +17,7 @@ if missing_deps:
 from httpx import AsyncClient, ASGITransport
 from fastapi import FastAPI
 from fastapi.contrib.export.routes import router as export_router
-
-
-
-    
+  
 app = FastAPI()
 app.include_router(export_router)
 
