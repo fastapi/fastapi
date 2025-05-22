139c02c39dae74a3233e61cdf78fd8d93f70fb38
Vincenzo Incutti <vincenzo.incutti@gmail.com>
2025-05-22 14:48:26 -0700

    Remove import

--- fastapi/security/http.py
+++ fastapi/security/http.py
@@ -7,7 +7,6 @@ from fastapi.openapi.models import HTTPBase as HTTPBaseModel
 from fastapi.openapi.models import HTTPBearer as HTTPBearerModel
 from fastapi.security.base import SecurityBase
 from fastapi.security.utils import get_authorization_scheme_param
-from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
 from typing_extensions import Annotated, Doc
