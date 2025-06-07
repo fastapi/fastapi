5f35edbf8ac017e33d05c5990a010ab77340a241
TsakalosGeo <t8200259@aueb.gr>
2025-06-07 12:30:48 +0300

    Update dbase_online_import.py

--- export_app/dbase_online_import.py
+++ export_app/dbase_online_import.py
@@ -5,10 +5,10 @@ from fastapi.responses import JSONResponse
 from export_app.dbase_converter import fetch_from_sqlite, fetch_from_mysql
 from typing import Literal
 
-router = APIRouter(title="Online Database Import API")
+router = APIRouter()
 
 
-@router.get("/import-online-db", summary="Import data from an online database", description="Connect to an online SQLite or MySQL database and fetch the contents of a specific table. Provide connection details via query parameters.")
+@router.get("/import-online-db", summary="Import data from an online database", tags=["Connect to an online SQLite or MySQL database and fetch the contents of a specific table."])
 async def import_from_database(
     db_type: Literal["sqlite", "mysql"] = Query(..., description="Type of database"),
     host: str = Query(None, description="Database host (for MySQL)"),
