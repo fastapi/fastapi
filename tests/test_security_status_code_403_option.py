6c53a67422431d076e50e5d52946dec8062a7564
Yurii Motov <yurii.motov.monte@gmail.com>
2025-06-11 12:53:27 +0200

    Fix `HTTPBearer` security scheme status code on "Not authenticated" error

--- tests/test_security_status_code_403_option.py
+++ tests/test_security_status_code_403_option.py
@@ -1,6 +1,8 @@
 import pytest
 from fastapi import FastAPI, Security
+from fastapi.openapi.models import HTTPBase
 from fastapi.security.api_key import APIKeyBase, APIKeyCookie, APIKeyHeader, APIKeyQuery
+from fastapi.security.http import HTTPBearer, HTTPDigest
 from fastapi.testclient import TestClient
 
 
@@ -42,6 +44,58 @@ def test_apikey_status_code_403_on_auth_error_no_auto_error(auth: APIKeyBase):
     """
     Test temporary `not_authenticated_status_code` parameter for APIKey** classes with
     `auto_error=False`.
+    """
+
+    app = FastAPI()
+
+    @app.get("/")
+    async def protected(_: str = Security(auth)):
+        pass  # pragma: no cover
+
+    client = TestClient(app)
+
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "auth",
+    [
+        HTTPBearer(not_authenticated_status_code=403),
+    ],
+)
+def test_oauth2_status_code_403_on_auth_error(auth: HTTPBase):
+    """
+    Test temporary `not_authenticated_status_code` parameter for security classes that
+    follow rfc6750.
+    """
+
+    app = FastAPI()
+
+    @app.get("/")
+    async def protected(_: str = Security(auth)):
+        pass  # pragma: no cover
+
+    client = TestClient(app)
+
+    response = client.get("/")
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Not authenticated"}
+
+
+@pytest.mark.parametrize(
+    "auth",
+    [
+        HTTPBearer(not_authenticated_status_code=403, auto_error=False),
+    ],
+)
+def test_oauth2_status_code_403_on_auth_error_no_auto_error(
+    auth: HTTPBase,
+):
+    """
+    Test temporary `not_authenticated_status_code` parameter for security classes that
+    follow rfc6750.
+    With `auto_error=False`. Response code should be 200
     """
 
     app = FastAPI()
