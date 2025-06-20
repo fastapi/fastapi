cdc85ba280e356eea1ca9eb490496cbd1a9af222
WrldEngine <kamscienceit123.kamran@gmail.com>
2025-06-20 15:00:23 +0500

    hotfix: tests for python 3.8

--- tests/test_bsonjs_response_class.py
+++ tests/test_bsonjs_response_class.py
@@ -1,33 +1,28 @@
 import json
-
-# Because of python 3.8 doesn't support this library
-try:
-    import bsonjs  # type: ignore
-except ImportError:  # pragma: nocover
-    bsonjs = None
-
-from fastapi import FastAPI
-from fastapi.responses import BSONJSResponse
-from fastapi.testclient import TestClient
-
-app = FastAPI(default_response_class=BSONJSResponse)
+import pytest
+import sys
 
 
-@app.get("/bsonjs_keys")
-def get_bsonjs_serialized_data():
-    return {"key": "Hello World", 1: 1}
-
-
-client = TestClient(app)
-
-
+@pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="requires minimum python3.9 or higher"
+)
 def test_bsonjs_serialized_data():
+    import bsonjs
+
+    from fastapi import FastAPI
+    from fastapi.responses import BSONJSResponse
+    from fastapi.testclient import TestClient
+
+    app = FastAPI(default_response_class=BSONJSResponse)
+
+    @app.get("/bsonjs_keys")
+    def get_bsonjs_serialized_data():
+        return {"key": "Hello World"}
+
+    client = TestClient(app)
     with client:
         response = client.get("/bsonjs_keys")
 
-    if bsonjs is not None:
         assert response.content == bsonjs.loads(
             json.dumps({"key": "Hello World", 1: 1})
         )
-
-    assert True
