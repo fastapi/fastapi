6b817dc9307f25d7cbd1b958fa2b9d1138687202
Motov Yurii <109919500+YuriiMotov@users.noreply.github.com>
2025-06-24 12:29:24 +0200

    Update tests/test_openapi_async.py

--- tests/test_openapi_async.py
+++ tests/test_openapi_async.py
@@ -6,7 +6,7 @@ def test_allow_async_openapi():
     async def async_openapi():
         return {"foo": "bar"}
 
-    mod_app = FastAPI()  # use fresh instance to not affect other tests
+    mod_app = FastAPI()
     mod_app.openapi = async_openapi
     mod_client = TestClient(mod_app)
     response = mod_client.get("/openapi.json")
