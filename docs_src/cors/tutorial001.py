56b3bb8aec267822e6c88b07b917d5819af4b150
Santiago Garcia <santiago.garcia@posteo.de>
2025-06-28 11:34:26 +0200

    Fix CORSMiddleware example

--- docs_src/cors/tutorial001.py
+++ docs_src/cors/tutorial001.py
@@ -11,11 +11,12 @@ origins = [
 ]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware(
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
 )
 
 
