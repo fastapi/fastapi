1fd463d5346a4472d08681efb5dfc71341a7dee1
amogha-rao <amogha.rao@cloudera.com>
2025-07-05 10:48:37 +0530

    ✨ Update test case for header parameters in `test_tutorial003.py` to include multiple `x-token` values

--- tests/test_tutorial/test_header_params/test_tutorial003.py
+++ tests/test_tutorial/test_header_params/test_tutorial003.py
@@ -2,6 +2,7 @@ import importlib
 
 import pytest
 from dirty_equals import IsDict
+
 from fastapi.testclient import TestClient
 
 from ...utils import needs_py39, needs_py310
@@ -29,8 +30,8 @@ def get_client(request: pytest.FixtureRequest):
     [
         ("/items", None, 200, {"X-Token values": None}),
         ("/items", {"x-token": "foo"}, 200, {"X-Token values": ["foo"]}),
-        # TODO: fix this, is it a bug?
-        # ("/items", [("x-token", "foo"), ("x-token", "bar")], 200, {"X-Token values": ["foo", "bar"]}),
+        ("/items", [("x-token", "foo"), ("x-token", "bar")],
+         200, {"X-Token values": ["foo", "bar"]}),
     ],
 )
 def test(path, headers, expected_status, expected_response, client: TestClient):
