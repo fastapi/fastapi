bcac4cc1a808c91dfadcf033712aed13fff183b5
Yurii Motov <yurii.motov.monte@gmail.com>
2025-07-10 22:06:07 +0200

    Fix openapi version in test

--- tests/test_additional_response_empty.py
+++ tests/test_additional_response_empty.py
@@ -20,7 +20,7 @@ client = TestClient(app)
 
 
 openapi_schema = {
-    "openapi": "3.0.2",
+    "openapi": "3.1.0",
     "info": {"title": "FastAPI", "version": "0.1.0"},
     "paths": {
         "/foo": {
