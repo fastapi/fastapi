ebb168a7a25fbeeb9d8007961f8f932faa967df2
Yurii Motov <yurii.motov.monte@gmail.com>
2025-07-10 11:15:23 +0200

    Fix test (openapi version)

--- tests/test_get_openapi_schema.py
+++ tests/test_get_openapi_schema.py
@@ -21,7 +21,7 @@ client = TestClient(app)
 
 
 openapi_schema = {
-    "openapi": "3.0.2",
+    "openapi": "3.1.0",
     "info": {"title": "FastAPI", "version": "0.1.0"},
     "paths": {
         "/items": {
