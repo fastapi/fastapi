783861781d990fffbd97f75e77f34ae03ee173fa
Nikolaus Z <8322965+nikmaxott@users.noreply.github.com>
2025-08-08 11:25:11 +0100

    Update tutorial006_py310.py

--- docs_src/query_params/tutorial006_py310.py
+++ docs_src/query_params/tutorial006_py310.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_user_item(
+async def read_item(
     item_id: str, needy: str, skip: int = 0, limit: int | None = None
 ):
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
