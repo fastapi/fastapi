97ce555b418488e2a986aedd84157508cf1bce49
Ben Brady <benbrady04.bb@gmail.com>
2025-08-09 13:14:45 +0100

    🔨 removed unused test code

--- tests/test_jsonable_encoder.py
+++ tests/test_jsonable_encoder.py
@@ -95,9 +95,6 @@ def test_encode_dict_with_nonprimative_keys():
         def __init__(self, value: str) -> None:
             self.value = value
 
-        def __eq__(self, other) -> bool:
-            return isinstance(other, CustomString) and self.value == other.value
-
         def __hash__(self):
             return hash(self.value)
 
