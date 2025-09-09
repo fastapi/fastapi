f4ebfadcf9cb0060927a6294e42cb77b0ab2d867
Ben Brady <benbrady04.bb@gmail.com>
2025-09-09 21:57:24 +0100

    🔨 removed encoders_by_class_tuples

--- fastapi/encoders.py
+++ fastapi/encoders.py
@@ -82,20 +82,6 @@ ENCODERS_BY_TYPE: Dict[Type[Any], Callable[[Any], Any]] = {
     AnyUrl: str,
 }
 
-
-def generate_encoders_by_class_tuples(
-    type_encoder_map: Dict[Any, Callable[[Any], Any]],
-) -> Dict[Callable[[Any], Any], Tuple[Any, ...]]:
-    encoders_by_class_tuples: Dict[Callable[[Any], Any], Tuple[Any, ...]] = defaultdict(
-        tuple
-    )
-    for type_, encoder in type_encoder_map.items():
-        encoders_by_class_tuples[encoder] += (type_,)
-    return encoders_by_class_tuples
-
-
-encoders_by_class_tuples = generate_encoders_by_class_tuples(ENCODERS_BY_TYPE)
-
 NoneType = type(None)
 
 
