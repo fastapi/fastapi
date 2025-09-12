93aac3b7c5271c71097dc36373c6d1651a1b83f0
chris <chris@csie.io>
2025-09-13 02:40:52 +0800

    test: parametrize response model tutorial websockets

--- tests/test_tutorial/test_websockets/test_tutorial003.py
+++ tests/test_tutorial/test_websockets/test_tutorial003.py
@@ -1,16 +1,32 @@
+import importlib
+
+import pytest
 from fastapi.testclient import TestClient
 
-from docs_src.websockets.tutorial003 import app, html
-
-client = TestClient(app)
+from ...utils import needs_py39
 
 
-def test_get():
+@pytest.fixture(
+    name="client",
+    params=[
+        "tutorial003",
+        pytest.param("tutorial003_py39", marks=needs_py39),
+    ],
+)
+def get_client(request: pytest.FixtureRequest):
+    mod = importlib.import_module(f"docs_src.websockets.{request.param}")
+
+    client = TestClient(mod.app)
+    client.html = mod.html
+    return client
+
+
+def test_get(client: TestClient):
     response = client.get("/")
-    assert response.text == html
+    assert response.text == client.html
 
 
-def test_websocket_handle_disconnection():
+def test_websocket_handle_disconnection(client: TestClient):
     with client.websocket_connect("/ws/1234") as connection, client.websocket_connect(
         "/ws/5678"
     ) as connection_two:
