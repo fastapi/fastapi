b51ec36f2e442495de5e4c22ae51a8f5c44917eb
Robert Hofer <1058012+hofrob@users.noreply.github.com>
2025-09-20 20:44:43 +0200

    ✨  Allow `None` as return type for bodiless responses (#9425)
    Co-authored-by: Sebastián Ramírez <tiangolo@gmail.com>

--- fastapi/dependencies/utils.py
+++ fastapi/dependencies/utils.py
@@ -254,6 +254,8 @@ def get_typed_annotation(annotation: Any, globalns: Dict[str, Any]) -> Any:
     if isinstance(annotation, str):
         annotation = ForwardRef(annotation)
         annotation = evaluate_forwardref(annotation, globalns, globalns)
+        if annotation is type(None):
+            return None
     return annotation
 
 
