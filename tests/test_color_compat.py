6361fb6e1d834ee10597e5422e149c31e2504c32
SaisakthiM <saisakthicloud@gmail.com>
2025-09-21 14:50:53 +0530

    Removed m

--- tests/test_color_compat.py
+++ tests/test_color_compat.py
@@ -12,6 +12,6 @@ class Model(pydantic.BaseModel):
 def test_color_deprecation_warning():
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter("always")
-        m = Model(c="#FF0000")
+        Model(c="#FF0000")
         dep_warnings = [warn for warn in w if issubclass(warn.category, DeprecationWarning)]
         assert len(dep_warnings) == 0, "DeprecationWarning raised! Fixed in this PR"
