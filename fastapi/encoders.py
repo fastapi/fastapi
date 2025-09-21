0f317d3e1e789894c2b4feb5f6db986e78c817be
SaisakthiM <saisakthicloud@gmail.com>
2025-09-21 12:29:40 +0530

    changed the Color type to any

--- fastapi/encoders.py
+++ fastapi/encoders.py
@@ -28,7 +28,7 @@ try:
     from pydantic_extra_types.color import Color
 except ImportError:
     # Fallback for Pydantic v1
-    Color = type("DummyColor", (), {})
+    Color: Type[Any]
 
 from ._compat import PYDANTIC_V2, UndefinedType, Url, _model_dump
 
