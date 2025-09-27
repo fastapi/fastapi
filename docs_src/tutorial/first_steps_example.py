1311952a53bc754a5cbdb8b541e8466b0435b0d1
msrajput08 <msrajput8112004@gmail.com>
2025-09-27 20:30:20 +0530

    Move tutorial example to .py with response_model and proper location

---
+++ docs_src/tutorial/first_steps_example.py
@@ -0,0 +1,18 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+@app.get("/items/", response_model=List[Item])
+async def read_items():
+    return [
+        {"name": "Item 1", "description": "Desc 1", "price": 10.0, "tax": 0.5},
+        {"name": "Item 2", "description": "Desc 2", "price": 20.0, "tax": 1.0},
+    ]
