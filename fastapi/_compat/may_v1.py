4e6a59b85a33f126564614f7bd9973a5511e718a
svlandeg <sofie.vanlandeghem@gmail.com>
2025-10-14 17:38:15 +0200

    add back definitions that got accidentally removed

--- fastapi/_compat/may_v1.py
+++ fastapi/_compat/may_v1.py
@@ -5,31 +5,40 @@ from fastapi.types import ModelNameMap
 
 if sys.version_info >= (3, 14):
 
-    class BaseConfig:
+    class AnyUrl:
         pass
 
-    class FieldInfo:
+    class BaseConfig:
         pass
 
     class BaseModel:
         pass
 
-    class UndefinedType:
+    class Color:
+        pass
+
+    class CoreSchema:
         pass
 
     class ErrorWrapper:
         pass
 
-    class ModelField:
+    class FieldInfo:
+        pass
+
+    class GetJsonSchemaHandler:
         pass
 
     class JsonSchemaValue:
         pass
 
-    class Color:
+    class ModelField:
         pass
 
     class NameEmail:
+        pass
+
+    class RequiredParam:
         pass
 
     class SecretBytes:
@@ -38,22 +47,13 @@ if sys.version_info >= (3, 14):
     class SecretStr:
         pass
 
-    class AnyUrl:
-        pass
-
-    class Url:
-        pass
-
-    class CoreSchema:
-        pass
-
-    class GetJsonSchemaHandler:
-        pass
-
     class Undefined:
         pass
 
-    class RequiredParam:
+    class UndefinedType:
+        pass
+
+    class Url:
         pass
 
     def get_definitions(
@@ -69,20 +69,33 @@ if sys.version_info >= (3, 14):
     ]:
         return {}, {}
 
+    def _normalize_errors(errors: Sequence[Any]) -> List[Dict[str, Any]]:
+        return []
+
     def _regenerate_error_with_loc(
         *, errors: Sequence[Any], loc_prefix: Tuple[Union[str, int], ...]
     ) -> List[Dict[str, Any]]:
         return []
 
-    def _normalize_errors(errors: Sequence[Any]) -> List[Dict[str, Any]]:
-        return []
 
 else:
+    from .v1 import AnyUrl as AnyUrl  # type: ignore[assignment]
     from .v1 import BaseConfig as BaseConfig  # type: ignore[assignment]
-    from .v1 import CoreSchema as CoreSchema
-    from .v1 import FieldInfo as FieldInfo
-    from .v1 import GetJsonSchemaHandler as GetJsonSchemaHandler
-    from .v1 import JsonSchemaValue as JsonSchemaValue
-    from .v1 import ModelField
-    from .v1 import UndefinedType as UndefinedType
-    from .v1 import _normalize_errors as _normalize_errors
+    from .v1 import BaseModel as BaseModel  # type: ignore[assignment]
+    from .v1 import Color as Color  # type: ignore[assignment]
+    from .v1 import CoreSchema as CoreSchema   # type: ignore[assignment]
+    from .v1 import ErrorWrapper as ErrorWrapper  # type: ignore[assignment]
+    from .v1 import FieldInfo as FieldInfo  # type: ignore[assignment]
+    from .v1 import GetJsonSchemaHandler as GetJsonSchemaHandler  # type: ignore[assignment]
+    from .v1 import JsonSchemaValue as JsonSchemaValue  # type: ignore[assignment]
+    from .v1 import ModelField  # type: ignore[assignment]
+    from .v1 import NameEmail as NameEmail  # type: ignore[assignment]
+    from .v1 import RequiredParam as RequiredParam  # type: ignore[assignment]
+    from .v1 import SecretBytes as SecretBytes  # type: ignore[assignment]
+    from .v1 import SecretStr as SecretStr  # type: ignore[assignment]
+    from .v1 import Undefined as Undefined  # type: ignore[assignment]
+    from .v1 import UndefinedType as UndefinedType  # type: ignore[assignment]
+    from .v1 import Url as Url  # type: ignore[assignment]
+    from .v1 import get_definitions  # type: ignore[assignment]
+    from .v1 import _normalize_errors as _normalize_errors  # type: ignore[assignment]
+    from .v1 import _regenerate_error_with_loc  # type: ignore[assignment]
