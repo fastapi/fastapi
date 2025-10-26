325d0d817dcb870c0afe00be31dd19c5ec955498
Evgeny Bokshitsky <e.bokshitskiy@hh.ru>
2025-10-26 21:49:39 +0400

    🔨 rewrite status_code select logic

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -370,13 +370,10 @@ def get_request_handler(
         }
         # If status_code was set, use it, otherwise use the default from the
         # response class, in the case of redirect it's 307
-        current_status_code = (
-            status_code if status_code else solved_result.response.status_code
-        )
-        if current_status_code is not None:
-            response_args["status_code"] = current_status_code
         if solved_result.response.status_code:
             response_args["status_code"] = solved_result.response.status_code
+        elif status_code is not None:
+            response_args["status_code"] = status_code
         content = await serialize_response(
             field=response_field,
             response_content=raw_response,
