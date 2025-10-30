6762879a3a238c882f56b36a60b0f80c1798944c
Javier Sánchez <javier.sanchez.castro@bookline.ai>
2025-10-30 13:10:11 +0100

    new msg error

--- tests/test_router_circular_import.py
+++ tests/test_router_circular_import.py
@@ -5,5 +5,5 @@ from fastapi import APIRouter
 def test_router_circular_import():
     router = APIRouter()
 
-    with pytest.raises(AssertionError, match="Cannot include router into itself"):
+    with pytest.raises(AssertionError, match="Cannot include the same APIRouter instance into itself. Did you mean to include a different router?"):
         router.include_router(router)
