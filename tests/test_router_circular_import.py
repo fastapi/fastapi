28fa21fd1a1d08c2e6ee715feb3d979bf2662cf7
Javier Sánchez Castro <72013291+JavierSanchezCastro@users.noreply.github.com>
2025-10-30 13:05:57 +0100

    Update tests/test_router_circular_import.py
    Co-authored-by: Motov Yurii <109919500+YuriiMotov@users.noreply.github.com>

--- tests/test_router_circular_import.py
+++ tests/test_router_circular_import.py
@@ -1,11 +1,9 @@
 import pytest
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter
 
 
 def test_router_circular_import():
-    app = FastAPI()
     router = APIRouter()
 
-    app.include_router(router)
     with pytest.raises(AssertionError, match="Cannot include router into itself"):
         router.include_router(router)
