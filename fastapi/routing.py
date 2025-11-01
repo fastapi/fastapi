1c084887c175e595acda8470d662a74e148b2f10
Sebastián Ramírez <tiangolo@gmail.com>
2025-11-01 17:08:32 +0100

    🐛 Fix running WebSocket function inside both async exit stacks

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -146,7 +146,7 @@ def websocket_session(
                 scope["fastapi_inner_astack"] = request_stack
                 async with AsyncExitStack() as function_stack:
                     scope["fastapi_function_astack"] = function_stack
-                await func(session)
+                    await func(session)
 
         # Same as in Starlette
         await wrap_app_handling_exceptions(app, session)(scope, receive, send)
