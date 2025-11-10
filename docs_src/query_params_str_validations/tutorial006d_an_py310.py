9b8beb4700996a3833ce25568a631abdd3a23b1e
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-11-10 15:05:37 +0530

    docs: finalize Required, can be None example with test verification

--- docs_src/query_params_str_validations/tutorial006d_an_py310.py
+++ docs_src/query_params_str_validations/tutorial006d_an_py310.py
@@ -1,12 +1,11 @@
-from typing import Annotated
-
 from fastapi import FastAPI, Query
+from typing import Annotated, Optional
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query()] = ...):
+async def read_items(q: Annotated[Optional[str], Query()] = ...):
     if q in ("None", "", "null"):
         q = None
     return {"q": q}
