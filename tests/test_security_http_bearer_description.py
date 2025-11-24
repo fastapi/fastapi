3fbf5c98ffab5ce674433a070168487ff9145e8b
Sebastián Ramírez <tiangolo@gmail.com>
2025-11-24 19:13:09 +0100

    ✅ Update tests to check the same error for HTTP security utils

--- tests/test_security_http_bearer_description.py
+++ tests/test_security_http_bearer_description.py
@@ -31,7 +31,7 @@ def test_security_http_bearer_no_credentials():
 def test_security_http_bearer_incorrect_scheme_credentials():
     response = client.get("/users/me", headers={"Authorization": "Basic notreally"})
     assert response.status_code == 401, response.text
-    assert response.json() == {"detail": "Invalid authentication credentials"}
+    assert response.json() == {"detail": "Not authenticated"}
     assert response.headers["WWW-Authenticate"] == "Bearer"
 
 
--- tests/test_security_http_digest.py
+++ tests/test_security_http_digest.py
@@ -33,7 +33,7 @@ def test_security_http_digest_incorrect_scheme_credentials():
         "/users/me", headers={"Authorization": "Other invalidauthorization"}
     )
     assert response.status_code == 401, response.text
-    assert response.json() == {"detail": "Invalid authentication credentials"}
+    assert response.json() == {"detail": "Not authenticated"}
     assert response.headers["WWW-Authenticate"] == "Digest"
 
 
