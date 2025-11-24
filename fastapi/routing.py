5302f988cf45c066904de5b7d85b6b2043247fdb
Javier Sánchez <javier.sanchez.castro@bookline.ai>
2025-11-24 12:30:50 +0100

    linter

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -351,7 +351,7 @@ def get_request_handler(
                         else:
                             body = body_bytes
         except json.JSONDecodeError as e:
-            validation_error = RequestMalformedError(
+            raise RequestMalformedError(
                 [
                     {
                         "type": "json_invalid",
@@ -362,8 +362,7 @@ def get_request_handler(
                     }
                 ],
                 body=e.doc,
-            )
-            raise validation_error from e
+            ) from e
         except HTTPException:
             # If a middleware raises an HTTPException, it should be raised again
             raise
