372049d60ddd3c32c73c99f472a3dc7404c396e5
Nancy Wang <jxw1156@case.edu>
2025-11-25 17:26:34 +0000

    test example

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
