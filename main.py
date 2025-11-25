66bc00ca83b3db6bf6505c46890c6a7d9ca2e838
Nancy Wang <jxw1156@case.edu>
2025-11-25 17:40:13 +0000

    updated example

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
@@ -13,3 +20,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
