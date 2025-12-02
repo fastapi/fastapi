b9c2452e9d99f42c04379477e252d222730ceb6d
Sebastián Ramírez <tiangolo@gmail.com>
2025-12-02 18:16:13 +0100

    ✅ Update test comment

--- tests/test_wrapped_method_forward_reference.py
+++ tests/test_wrapped_method_forward_reference.py
@@ -17,8 +17,8 @@ def passthrough(f):
 def test_wrapped_method_type_inference():
     """
     Regression test ensuring that when a method imported from another module
-    is decorated with something that sets the __wrapped__ attribute, then
-    the types are still processed correctly, including dereferencing of forward
+    is decorated with something that sets the __wrapped__ attribute (functools.wraps),
+    then the types are still processed correctly, including dereferencing of forward
     references.
     """
     app = FastAPI()
