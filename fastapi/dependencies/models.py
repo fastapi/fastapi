0f8c4e82008d13a78b04683b091e64e69d88fb6d
Sebastián Ramírez <tiangolo@gmail.com>
2025-12-02 14:25:30 +0100

    ♻️ Make new attribute Dependant._unwrapped_call private

--- fastapi/dependencies/models.py
+++ fastapi/dependencies/models.py
@@ -76,32 +76,32 @@ class Dependant:
         return False
 
     @cached_property
-    def unwrapped_call(self) -> Any:
+    def _unwrapped_call(self) -> Any:
         if self.call is None:
             return self.call  # pragma: no cover
         return inspect.unwrap(self.call)
 
     @cached_property
     def is_gen_callable(self) -> bool:
-        if inspect.isgeneratorfunction(self.unwrapped_call):
+        if inspect.isgeneratorfunction(self._unwrapped_call):
             return True
-        dunder_call = getattr(self.unwrapped_call, "__call__", None)  # noqa: B004
+        dunder_call = getattr(self._unwrapped_call, "__call__", None)  # noqa: B004
         return inspect.isgeneratorfunction(dunder_call)
 
     @cached_property
     def is_async_gen_callable(self) -> bool:
-        if inspect.isasyncgenfunction(self.unwrapped_call):
+        if inspect.isasyncgenfunction(self._unwrapped_call):
             return True
-        dunder_call = getattr(self.unwrapped_call, "__call__", None)  # noqa: B004
+        dunder_call = getattr(self._unwrapped_call, "__call__", None)  # noqa: B004
         return inspect.isasyncgenfunction(dunder_call)
 
     @cached_property
     def is_coroutine_callable(self) -> bool:
-        if inspect.isroutine(self.unwrapped_call):
-            return iscoroutinefunction(self.unwrapped_call)
-        if inspect.isclass(self.unwrapped_call):
+        if inspect.isroutine(self._unwrapped_call):
+            return iscoroutinefunction(self._unwrapped_call)
+        if inspect.isclass(self._unwrapped_call):
             return False
-        dunder_call = getattr(self.unwrapped_call, "__call__", None)  # noqa: B004
+        dunder_call = getattr(self._unwrapped_call, "__call__", None)  # noqa: B004
         return iscoroutinefunction(dunder_call)
 
     @cached_property
