95b7e824bf8de5bfaa5995b25a8d7f0bcad96eae
Sebastián Ramírez <tiangolo@gmail.com>
2025-12-02 05:26:59 +0100

    ✅ Tweak tests to keep the same conventions as the rest of the tests

--- tests/test_form_default.py
+++ tests/test_form_default.py
@@ -13,21 +13,21 @@ async def post_url_encoded(age: Annotated[Optional[int], Form()] = None):
     return age
 
 
+@app.post("/multipart")
+async def post_multi_part(
+    age: Annotated[Optional[int], Form()] = None,
+    file: Annotated[Optional[bytes], File()] = None,
+):
+    return {"file": file, "age": age}
+
+
 def test_form_default_url_encoded():
     response = client.post("/urlencoded", data={"age": ""})
     assert response.status_code == 200
     assert response.text == "null"
 
 
-@app.post("/multipart")
-async def post_multi_part(
-    age: Annotated[Optional[int], Form()] = None,
-    file: Annotated[Optional[bytes], File()] = None,
-):
-    assert file is None
-    assert age is None
-
-
 def test_form_default_multi_part():
     response = client.post("/multipart", data={"age": ""})
     assert response.status_code == 200
+    assert response.json() == {"file": None, "age": None}
