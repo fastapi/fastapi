6b68de7f4231d73cab3bd751fe655b4facb68b8e
Sebastián Ramírez <tiangolo@gmail.com>
2025-12-03 14:38:48 +0100

    ✅ Tweak Python 3.10-only test to not break below 3.10

--- tests/test_stringified_annotations_simple.py
+++ tests/test_stringified_annotations_simple.py
@@ -6,22 +6,20 @@ from typing_extensions import Annotated
 
 from .utils import needs_py310
 
-app = FastAPI()
-
-client = TestClient(app)
-
-
-class Dep:
-    def __call__(self, request: Request):
-        return "test"
-
-
-@app.get("/test/")
-def call(test: Annotated[str, Depends(Dep())]):
-    return {"test": test}
-
 
 @needs_py310
 def test_stringified_annotations():
+    app = FastAPI()
+
+    client = TestClient(app)
+
+    class Dep:
+        def __call__(self, request: Request):
+            return "test"
+
+    @app.get("/test/")
+    def call(test: Annotated[str, Depends(Dep())]):
+        return {"test": test}
+
     response = client.get("/test")
     assert response.status_code == 200
