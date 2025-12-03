8aae553b89a08741006a48607559d35c1b964ab4
Sebastián Ramírez <tiangolo@gmail.com>
2025-12-03 14:43:59 +0100

    ✅ Tweak tests

--- tests/test_stringified_annotations_simple.py
+++ tests/test_stringified_annotations_simple.py
@@ -7,15 +7,16 @@ from typing_extensions import Annotated
 from .utils import needs_py310
 
 
+class Dep:
+    def __call__(self, request: Request):
+        return "test"
+
+
 @needs_py310
 def test_stringified_annotations():
     app = FastAPI()
 
     client = TestClient(app)
-
-    class Dep:
-        def __call__(self, request: Request):
-            return "test"
 
     @app.get("/test/")
     def call(test: Annotated[str, Depends(Dep())]):
