8ac7263f3044891571afa63f741588cddfa3411d
Max Chis <maxachis@gmail.com>
2025-12-03 19:15:55 -0500

    Add tests for new logic

---
+++ tests/test_app_init.py
@@ -0,0 +1,19 @@
+"""Tests for initialization of FastAPI app instance."""
+import pytest
+
+from fastapi import FastAPI
+
+
+def test_open_api_url_no_title():
+    """An error should be raised if an openapi URL is provided without a title."""
+    with pytest.raises(ValueError):
+        FastAPI(openapi_url="/openapi.json", title=None)
+
+def test_open_api_url_no_version():
+    """An error should be raised if an openapi URL is provided without a version."""
+    with pytest.raises(ValueError):
+        FastAPI(openapi_url="/openapi.json", version=None)
+
+def test_open_api_url_title_and_version():
+    """No error should be raised if an openapi URL is provided with a title and version."""
+    FastAPI(openapi_url="/openapi.json", title="Title", version="0.1")
