2e6fd3104547da9a016fdd06016311c81279f6eb
Max Chis <maxachis@gmail.com>
2025-12-03 19:16:10 -0500

    Reformat with ruff

--- tests/test_app_init.py
+++ tests/test_app_init.py
@@ -1,4 +1,5 @@
 """Tests for initialization of FastAPI app instance."""
+
 import pytest
 
 from fastapi import FastAPI
@@ -9,10 +10,12 @@ def test_open_api_url_no_title():
     with pytest.raises(ValueError):
         FastAPI(openapi_url="/openapi.json", title=None)
 
+
 def test_open_api_url_no_version():
     """An error should be raised if an openapi URL is provided without a version."""
     with pytest.raises(ValueError):
         FastAPI(openapi_url="/openapi.json", version=None)
+
 
 def test_open_api_url_title_and_version():
     """No error should be raised if an openapi URL is provided with a title and version."""
