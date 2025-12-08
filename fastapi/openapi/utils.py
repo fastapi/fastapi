aa87c18e114ac0d3c08dfda2bcd4f046084e935b
Simon Huang <simon.huang@telecom-st-etienne.fr>
2025-12-08 19:27:50 +0100

    Make the logic more explicit in validation error insertion in get_open_api_path

--- fastapi/openapi/utils.py
+++ fastapi/openapi/utils.py
@@ -417,10 +417,11 @@ def get_openapi_path(
                     openapi_response["description"] = description
             http422 = "422"
             all_route_params = get_flat_params(route.dependant)
-            if (all_route_params or route.body_field) and not any(
-                status in operation["responses"]
-                for status in [http422, "4XX", "default"]
-            ):
+            requires_validation_error = bool(all_route_params or route.body_field)
+            has_validation_response = any(
+                status in operation["responses"] for status in ("422", "4XX", "default")
+            )
+            if requires_validation_error and not has_validation_response:
                 operation["responses"][http422] = {
                     "description": "Validation Error",
                     "content": {
