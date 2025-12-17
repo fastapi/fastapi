9c1d8d645e27d633fec5d2fcfd3942c146e11f93
Yurii Motov <yurii.motov.monte@gmail.com>
2025-12-17 11:36:33 +0100

    Skip not supported langs in `langs-json` command

--- scripts/docs.py
+++ scripts/docs.py
@@ -428,7 +428,7 @@ def verify_docs():
 def langs_json():
     langs = []
     for lang_path in get_lang_paths():
-        if lang_path.is_dir():
+        if lang_path.is_dir() and lang_path.name in SUPPORTED_LANGS:
             langs.append(lang_path.name)
     print(json.dumps(langs))
 
