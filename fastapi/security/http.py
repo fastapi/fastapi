bf7e2b7cabff4870976d270a6e7a3b884ed17078
philipmunch <p.muench@tum.de>
2025-12-17 20:19:06 +0100

    Security dependencies now working with websockets

--- fastapi/security/http.py
+++ fastapi/security/http.py
@@ -9,7 +9,7 @@ from fastapi.openapi.models import HTTPBearer as HTTPBearerModel
 from fastapi.security.base import SecurityBase
 from fastapi.security.utils import get_authorization_scheme_param
 from pydantic import BaseModel
-from starlette.requests import Request
+from starlette.requests import HTTPConnection
 from starlette.status import HTTP_401_UNAUTHORIZED
 from typing_extensions import Annotated
 
@@ -93,7 +93,7 @@ class HTTPBase(SecurityBase):
         )
 
     async def __call__(
-        self, request: Request
+        self, request: HTTPConnection
     ) -> Optional[HTTPAuthorizationCredentials]:
         authorization = request.headers.get("Authorization")
         scheme, credentials = get_authorization_scheme_param(authorization)
@@ -203,7 +203,7 @@ class HTTPBasic(HTTPBase):
         return {"WWW-Authenticate": "Basic"}
 
     async def __call__(  # type: ignore
-        self, request: Request
+        self, request: HTTPConnection
     ) -> Optional[HTTPBasicCredentials]:
         authorization = request.headers.get("Authorization")
         scheme, param = get_authorization_scheme_param(authorization)
@@ -304,7 +304,7 @@ class HTTPBearer(HTTPBase):
         self.auto_error = auto_error
 
     async def __call__(
-        self, request: Request
+        self, request: HTTPConnection
     ) -> Optional[HTTPAuthorizationCredentials]:
         authorization = request.headers.get("Authorization")
         scheme, credentials = get_authorization_scheme_param(authorization)
@@ -407,7 +407,7 @@ class HTTPDigest(HTTPBase):
         self.auto_error = auto_error
 
     async def __call__(
-        self, request: Request
+        self, request: HTTPConnection
     ) -> Optional[HTTPAuthorizationCredentials]:
         authorization = request.headers.get("Authorization")
         scheme, credentials = get_authorization_scheme_param(authorization)
---
+++ tests/test_security_openid_connect_websocket.py
@@ -0,0 +1,53 @@
+import pytest
+from fastapi import Depends, FastAPI, Security
+from fastapi.security.open_id_connect_url import OpenIdConnect
+from fastapi.testclient import TestClient
+from pydantic import BaseModel
+from starlette.testclient import WebSocketDenialResponse
+from starlette.websockets import WebSocket
+
+app = FastAPI()
+
+oid = OpenIdConnect(openIdConnectUrl="/openid")
+
+
+class User(BaseModel):
+    username: str
+
+
+def get_current_user(oauth_header: str = Security(oid)):
+    user = User(username=oauth_header)
+    return user
+
+
+@app.websocket("/ws/users/me")
+async def read_current_user(
+    websocket: WebSocket, current_user: User = Depends(get_current_user)
+):
+    await websocket.accept()
+    await websocket.send_json({"username": current_user.username})
+
+
+client = TestClient(app)
+
+
+def test_security_openid_connect_ws():
+    with client.websocket_connect(
+        "/ws/users/me", headers={"Authorization": "Bearer footokenbar"}
+    ) as websocket:
+        data = websocket.receive_json()
+        assert data == {"username": "Bearer footokenbar"}
+
+
+def test_security_openid_connect_other_header_ws():
+    with client.websocket_connect(
+        "/ws/users/me", headers={"Authorization": "Other footokenbar"}
+    ) as websocket:
+        data = websocket.receive_json()
+        assert data == {"username": "Other footokenbar"}
+
+
+def test_security_openid_connect_no_header_ws():
+    with pytest.raises(WebSocketDenialResponse):
+        with client.websocket_connect("/ws/users/me"):
+            pass
