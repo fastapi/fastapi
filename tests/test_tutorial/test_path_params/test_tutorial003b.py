f24f82bb066096b58059f9600fe66fa53379050a
Yurii Motov <yurii.motov.monte@gmail.com>
2025-12-18 14:32:41 +0100

    Fix coverage for `docs_src.path_params.tutorial003b`

--- tests/test_tutorial/test_path_params/test_tutorial003b.py
+++ tests/test_tutorial/test_path_params/test_tutorial003b.py
@@ -1,6 +1,8 @@
+import asyncio
+
 from fastapi.testclient import TestClient
 
-from docs_src.path_params.tutorial003b_py39 import app
+from docs_src.path_params.tutorial003b_py39 import app, read_users2
 
 client = TestClient(app)
 
@@ -9,6 +11,10 @@ def test_get_users():
     response = client.get("/users")
     assert response.status_code == 200, response.text
     assert response.json() == ["Rick", "Morty"]
+
+
+def test_read_users2():  # Just for coverage
+    assert asyncio.run(read_users2()) == ["Bean", "Elfo"]
 
 
 def test_openapi_schema():
