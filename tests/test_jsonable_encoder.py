88d5ab5d30154f1d8b004d3b76aec7403bd56693
Sebastián Ramírez <tiangolo@gmail.com>
2025-12-20 16:17:17 +0100

    ✅ Add tests for jsonable_encoder, covered before by Pydantic v1

--- tests/test_jsonable_encoder.py
+++ tests/test_jsonable_encoder.py
@@ -84,6 +84,18 @@ def test_encode_dict():
     }
 
 
+def test_encode_dict_include_exclude_list():
+    pet = {"name": "Firulais", "owner": {"name": "Foo"}}
+    assert jsonable_encoder(pet) == {"name": "Firulais", "owner": {"name": "Foo"}}
+    assert jsonable_encoder(pet, include=["name"]) == {"name": "Firulais"}
+    assert jsonable_encoder(pet, exclude=["owner"]) == {"name": "Firulais"}
+    assert jsonable_encoder(pet, include=[]) == {}
+    assert jsonable_encoder(pet, exclude=[]) == {
+        "name": "Firulais",
+        "owner": {"name": "Foo"},
+    }
+
+
 def test_encode_class():
     person = Person(name="Foo")
     pet = Pet(owner=person, name="Firulais")
