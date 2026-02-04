af3bf874c16541d4e7c978c6a134ac9ac41c37c0
Yurii Motov <yurii.motov.monte@gmail.com>
2026-02-04 13:18:30 +0100

    Move `translation-banner.md` inside `docs` directory

--- scripts/mkdocs_hooks.py
+++ scripts/mkdocs_hooks.py
@@ -29,10 +29,10 @@ def get_missing_translation_content(docs_dir: str) -> str:
 @lru_cache
 def get_translation_banner_content(docs_dir: str) -> str:
     docs_dir_path = Path(docs_dir)
-    translation_banner_path = docs_dir_path.parent / "translation-banner.md"
+    translation_banner_path = docs_dir_path / "translation-banner.md"
     if not translation_banner_path.is_file():
         translation_banner_path = (
-            docs_dir_path.parent.parent / "en" / "translation-banner.md"
+            docs_dir_path.parent.parent / "en" / "docs" / "translation-banner.md"
         )
     return translation_banner_path.read_text(encoding="utf-8")
 
