e03019ef4c080e8a0009d179fe9a2cd6917a39af
Sebastián Ramírez <tiangolo@gmail.com>
2026-02-17 10:50:46 +0100

    🎨 Tweak types

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -125,7 +125,7 @@ def test_serialize_sequence_value_with_none_first_in_union():
     from fastapi._compat import v2
 
     # Use Union[None, list[str]] to ensure None comes first in the union args
-    field_info = FieldInfo(annotation=Union[None, list[str]])
+    field_info = FieldInfo(annotation=Union[None, list[str]])  # noqa: UP007
     field = v2.ModelField(name="items", field_info=field_info)
     result = v2.serialize_sequence_value(field=field, value=["x", "y"])
     assert result == ["x", "y"]
