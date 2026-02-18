a0d45bb29bde3c980d9cbda9f423d242a6b459b5
Sebastián Ramírez <tiangolo@gmail.com>
2026-02-18 14:31:06 +0100

    🔨 Add Kapa.ai widget

---
+++ docs/en/docs/js/init_kapa_widget.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var script = document.createElement("script");
+  script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+  script.setAttribute("data-website-id", "91f47f27-b405-4299-bf5f-a1c0ec07b3cc");
+  script.setAttribute("data-project-name", "FastAPI");
+  script.setAttribute("data-project-color", "#009485");
+  script.setAttribute("data-project-logo", "https://fastapi.tiangolo.com/img/favicon.png");
+  script.async = true;
+  document.head.appendChild(script);
+});
