88cf069a985b80f0842e29db2db8c4b5c1030eb2
Yurii Motov <yurii.motov.monte@gmail.com>
2026-02-24 21:32:54 +0100

    Add `test-cov.sh`

--- scripts/test-cov-html.sh
+++ scripts/test-cov-html.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-bash scripts/test.sh --cov --cov-context=test --cov-report=term-missing --cov-report=html ${@}
+bash scripts/test-cov.sh --cov-report=term-missing --cov-report=html ${@}
