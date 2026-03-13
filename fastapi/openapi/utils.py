b2798dab90507ce04b98233c21fd77420061d989
svlandeg <sofie.vanlandeghem@gmail.com>
2026-03-13 15:32:18 +0100

    provide a default name for an endpoint without __name__ attribute

--- fastapi/openapi/utils.py
+++ fastapi/openapi/utils.py
@@ -245,10 +245,8 @@ def get_openapi_operation_metadata(
         operation["description"] = route.description
     operation_id = route.operation_id or route.unique_id
     if operation_id in operation_ids:
-        message = (
-            f"Duplicate Operation ID {operation_id} for function "
-            + f"{route.endpoint.__name__}"  # ty: ignore[unresolved-attribute]
-        )
+        endpoint_name = getattr(route.endpoint, "__name__", "<unnamed_endpoint>")
+        message = f"Duplicate Operation ID {operation_id} for function {endpoint_name}"
         file_name = getattr(route.endpoint, "__globals__", {}).get("__file__")
         if file_name:
             message += f" at {file_name}"
