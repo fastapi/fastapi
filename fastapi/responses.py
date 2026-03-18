e3c13b00f3ac95ad45d12defe7557ee19dbe19c4
svlandeg <svlandeg@github.com>
2026-03-18 15:05:46 +0100

    fix type checker

--- fastapi/responses.py
+++ fastapi/responses.py
@@ -1,4 +1,5 @@
-from typing import Any
+import importlib
+from typing import Any, Protocol, cast
 
 from fastapi.exceptions import FastAPIDeprecationWarning
 from fastapi.sse import EventSourceResponse as EventSourceResponse  # noqa
@@ -17,10 +18,17 @@ except ImportError:  # pragma: nocover
     ujson = None  # type: ignore
 
 
+class _OrjsonModule(Protocol):
+    OPT_NON_STR_KEYS: int
+    OPT_SERIALIZE_NUMPY: int
+
+    def dumps(self, __obj: Any, *, option: int = ...) -> bytes: ...
+
+
 try:
-    import orjson
-except ImportError:  # pragma: nocover
-    orjson = None  # type: ignore
+    orjson = cast(_OrjsonModule, importlib.import_module("orjson"))
+except ModuleNotFoundError:  # pragma: nocover
+    orjson = None  # type: ignore  # ty: ignore[unused-ignore-comment]
 
 
 @deprecated(
