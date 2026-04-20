3140dc8b2ae89496d54df31d6a2c8956aba5f525
oliver <oliver.margetts@gmail.com>
2026-04-20 17:02:23 +0100

    🐛 fix deadlocks in depends by using a separate CapacityLimiter for teardown
    If there is a limited pool of resources, like database connections, we need separate pools of threads for acquiring and releasing those resources, otherwise there will always be deadlocks.

--- fastapi/concurrency.py
+++ fastapi/concurrency.py
@@ -1,7 +1,8 @@
-from collections.abc import AsyncGenerator
+import functools
+from collections.abc import AsyncGenerator, Callable
 from contextlib import AbstractContextManager
 from contextlib import asynccontextmanager as asynccontextmanager
-from typing import TypeVar
+from typing import ParamSpec, TypeVar
 
 import anyio.to_thread
 from anyio import CapacityLimiter
@@ -11,31 +12,44 @@ from starlette.concurrency import (  # noqa
     run_until_first_complete as run_until_first_complete,
 )
 
+_P = ParamSpec("_P")
 _T = TypeVar("_T")
+
+# Blocking __exit__ and other teardown operations from running can create race
+# conditions/deadlocks if the context manager itself has its own internal pool
+# (e.g. a database connection pool).
+# To avoid this maintain a separate limiter for teardown operations, so that the
+# operations acquiring resources can never block operations releasing resources.
+# NOTE: 5 is arbitrary, we would like more than 1 so that teardowns are not serialised.
+_teardown_limiter = CapacityLimiter(5)
 
 
 @asynccontextmanager
 async def contextmanager_in_threadpool(
     cm: AbstractContextManager[_T],
 ) -> AsyncGenerator[_T, None]:
-    # blocking __exit__ from running waiting on a free thread
-    # can create race conditions/deadlocks if the context manager itself
-    # has its own internal pool (e.g. a database connection pool)
-    # to avoid this we let __exit__ run without a capacity limit
-    # since we're creating a new limiter for each call, any non-zero limit
-    # works (1 is arbitrary)
-    exit_limiter = CapacityLimiter(1)
     try:
         yield await run_in_threadpool(cm.__enter__)
     except Exception as e:
         ok = bool(
-            await anyio.to_thread.run_sync(
-                cm.__exit__, type(e), e, e.__traceback__, limiter=exit_limiter
-            )
+            await run_in_teardown_threadpool(cm.__exit__, type(e), e, e.__traceback__)
         )
         if not ok:
             raise e
     else:
-        await anyio.to_thread.run_sync(
-            cm.__exit__, None, None, None, limiter=exit_limiter
-        )
+        await run_in_teardown_threadpool(cm.__exit__, None, None, None)
+
+
+async def run_in_teardown_threadpool(
+    func: Callable[_P, _T], *args: _P.args, **kwargs: _P.kwargs
+) -> _T:
+    """Run a function in the separate teardown threadpool.
+
+    This will run the function in the teardown threadpool in order to avoid it
+    being blocked by other operations waiting to acquire resources.
+
+    Unless you know what you are doing, you probably don't want this function,
+    use run_in_threadpool instead.
+    """
+    func = functools.partial(func, *args, **kwargs)
+    return await anyio.to_thread.run_sync(func, limiter=_teardown_limiter)
