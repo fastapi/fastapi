59d4a80fcf8a7b3a46b2560f75dc0d171e7ea8ff
Yurii Motov <109919500+YuriiMotov@users.noreply.github.com>
2026-05-27 12:47:28 +0200

    ✅ Add `httpx2` test dependency to avoid deprecation warning (#15603)

--- tests/test_tutorial/test_header_param_models/test_tutorial001.py
+++ tests/test_tutorial/test_header_param_models/test_tutorial001.py
@@ -1,6 +1,7 @@
 import importlib
 
 import pytest
+from dirty_equals import IsOneOf
 from fastapi.testclient import TestClient
 from inline_snapshot import snapshot
 
@@ -68,7 +69,9 @@ def test_header_param_model_invalid(client: TestClient):
                         "x_tag": [],
                         "host": "testserver",
                         "accept": "*/*",
-                        "accept-encoding": "gzip, deflate",
+                        "accept-encoding": IsOneOf(
+                            "gzip, deflate", "gzip, deflate, zstd"
+                        ),
                         "connection": "keep-alive",
                         "user-agent": "testclient",
                     },
