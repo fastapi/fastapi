21d1d8ec6a7ae7c97888e94dd926cdb7ee40a5d8
SUPREME <omsrisriradhakrsna108@gmail.com>
2026-05-31 12:54:18 +0530

    docs: add module-level docstring to websockets module
    Documents the WebSocket re-exports from Starlette, clarifying that this
    module provides a consistent FastAPI import path for WebSocket classes.

--- fastapi/websockets.py
+++ fastapi/websockets.py
@@ -1,3 +1,11 @@
+"""
+WebSocket support for FastAPI.
+
+Re-exports Starlette's WebSocket classes for use in FastAPI applications,
+providing a consistent import path for WebSocket, WebSocketDisconnect, and
+WebSocketState.
+"""
+
 from starlette.websockets import WebSocket as WebSocket  # noqa
 from starlette.websockets import WebSocketDisconnect as WebSocketDisconnect  # noqa
 from starlette.websockets import WebSocketState as WebSocketState  # noqa
