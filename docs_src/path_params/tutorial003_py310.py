1f8589b4acf090c4d478fe1c7fc80a254b2f3054
kotoYoshi <rikotoyoshi@gmail.com>
2026-06-04 23:37:56 +0900

    📝 Add return type annotations to path params tutorial example

--- docs_src/path_params/tutorial003_py310.py
+++ docs_src/path_params/tutorial003_py310.py
@@ -4,10 +4,10 @@ app = FastAPI()
 
 
 @app.get("/users/me")
-async def read_user_me():
+async def read_user_me() -> dict:
     return {"user_id": "the current user"}
 
 
 @app.get("/users/{user_id}")
-async def read_user(user_id: str):
+async def read_user(user_id: str) -> dict:
     return {"user_id": user_id}
