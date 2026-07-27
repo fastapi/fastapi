7ac0f1b541b8df5921b45486a5c72a49965d00af
Sebastián Ramírez <tiangolo@gmail.com>
2026-07-27 17:45:50 +0200

    🔖 Release version 0.140.4 (#16070)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- fastapi/__init__.py
+++ fastapi/__init__.py
@@ -1,6 +1,6 @@
 """FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
 
-__version__ = "0.140.3"
+__version__ = "0.140.4"
 
 from starlette import status as status
 
