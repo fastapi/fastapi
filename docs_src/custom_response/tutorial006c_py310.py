c26b1cf5a7b8beda7f63bd1ba8a71ef707791d7b
Dmitriy <dmitriy.svitkov2@mail.ru>
2026-08-10 21:13:23 +0300

    Add return type annotation to redirect_pydantic

--- docs_src/custom_response/tutorial006c_py310.py
+++ docs_src/custom_response/tutorial006c_py310.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 
 @app.get("/pydantic", response_class=RedirectResponse, status_code=302)
-async def redirect_pydantic():
+async def redirect_pydantic() -> str:
     return "https://docs.pydantic.dev/"
