afa86cbb4c87ae2a1243a4424a6835fc383a4154
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:24:40 +0000

    fix: remediate 1 vulnerability(ies) in docs_src/async_tests/app_a_py310/test_main.py
    Fixed vulnerabilities:
    - Using clear-text protocols is security-sensitive
      Using http protocol is insecure. Use https instead

--- docs_src/async_tests/app_a_py310/test_main.py
+++ docs_src/async_tests/app_a_py310/test_main.py
@@ -7,7 +7,7 @@ from .main import app
 @pytest.mark.anyio
 async def test_root():
     async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
+        transport=ASGITransport(app=app), base_url="https://test"
     ) as ac:
         response = await ac.get("/")
     assert response.status_code == 200
