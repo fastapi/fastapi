045b084b5a02a677c890b0e6b6ed4514cf4aa1c6
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:32:45 +0000

    fix: remediate 1 vulnerability(ies) in docs_src/query_params_str_validations/tutorial015_an_py310.py
    Fixed vulnerabilities:
    - Using pseudorandom number generators (PRNGs) is security-sensitive
      Make sure that using this pseudorandom number generator is safe here.

--- docs_src/query_params_str_validations/tutorial015_an_py310.py
+++ docs_src/query_params_str_validations/tutorial015_an_py310.py
@@ -26,5 +26,5 @@ async def read_items(
     if id:
         item = data.get(id)
     else:
-        id, item = random.choice(list(data.items()))
+        id, item = random.SystemRandom().choice(list(data.items()))
     return {"id": id, "name": item}
