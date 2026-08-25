235ed9d08ae483008d87c7a8b9d306c51e5d2bef
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:23:17 +0000

    fix: remediate 2 vulnerability(ies) in scripts/add_latest_release_date.py
    Fixed vulnerabilities:
    - Using slow regular expressions is security-sensitive
      Make sure the regex used here, which is vulnerable to polynomial runtime due to backtracking, cannot lead to denial of service.
    
    - Make sure that using a regular expression is safe here.
      Make sure that using a regular expression is safe here.

--- scripts/add_latest_release_date.py
+++ scripts/add_latest_release_date.py
@@ -5,7 +5,7 @@ import sys
 from datetime import date
 
 RELEASE_NOTES_FILE = "docs/en/docs/release-notes.md"
-RELEASE_HEADER_PATTERN = re.compile(r"^##\s+(\d+\.\d+\.\d+)(?:\s+\(([^()\n]{0,1000})\))?\s*$")
+RELEASE_HEADER_PATTERN = re.compile(r"^##\s+(\d+\.\d+\.\d+)(?:\s+\(([^
 
 
 def main() -> None:
