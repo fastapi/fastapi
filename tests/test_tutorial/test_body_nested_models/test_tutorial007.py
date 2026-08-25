19701c02b41190c6abcce595a7b4b21c629762a6
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:32:37 +0000

    fix: remediate 3 vulnerability(ies) in tests/test_tutorial/test_body_nested_models/test_tutorial007.py
    Fixed vulnerabilities:
    - Using clear-text protocols is security-sensitive
      Using http protocol is insecure. Use https instead

--- tests/test_tutorial/test_body_nested_models/test_tutorial007.py
+++ tests/test_tutorial/test_body_nested_models/test_tutorial007.py
@@ -34,7 +34,7 @@ def test_post_all(client: TestClient):
                 "tags": ["foo"],
                 "images": [
                     {
-                        "url": "https://example.com/image.png",
+                        "url": "https://example.com/secure-image.png",
                         "name": "example image",
                     }
                 ],
@@ -62,7 +62,7 @@ def test_put_only_required(client: TestClient):
                     "price": 35.4,
                     "images": [
                         {
-                            "url": "https://example.com/image.png",
+                            "url": "https://example.com/secure-image.png",
                             "name": "example image",
                         }
                     ],
@@ -84,7 +84,7 @@ def test_put_only_required(client: TestClient):
                 "tags": [],
                 "images": [
                     {
-                        "url": "https://example.com/image.png",
+                        "url": "https://example.com/secure-image.png",
                         "name": "example image",
                     }
                 ],
