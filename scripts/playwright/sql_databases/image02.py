4be0bb843544bbca80e84886f8ab3cc616aaf81c
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:05:07 +0000

    fix: remediate 1 vulnerability(ies) in scripts/playwright/sql_databases/image02.py
    Fixed vulnerabilities:
    - Make sure that executing this OS command is safe here.
      Make sure that executing this OS command is safe here.

--- scripts/playwright/sql_databases/image02.py
+++ scripts/playwright/sql_databases/image02.py
@@ -21,9 +21,11 @@ def run(playwright: Playwright) -> None:
     browser.close()
 
 
-process = subprocess.Popen(
-    ["fastapi", "run", "docs_src/sql_databases/tutorial002.py"],
-)
+process = subprocess.Popen([
+    "fastapi",
+    "run",
+    "docs_src/sql_databases/tutorial002.py",
+], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False)
 try:
     for _ in range(3):
         try:
