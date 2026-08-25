ac2cd18057443b16166728516a32595c8fc0f942
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:33:13 +0000

    fix: remediate 3 vulnerability(ies) in tests/test_tutorial/test_advanced_middleware/test_tutorial002.py
    Fixed vulnerabilities:
    - Using clear-text protocols is security-sensitive
      Using http protocol is insecure. Use https instead

--- tests/test_tutorial/test_advanced_middleware/test_tutorial002.py
+++ tests/test_tutorial/test_advanced_middleware/test_tutorial002.py
@@ -4,12 +4,12 @@ from docs_src.advanced_middleware.tutorial002_py310 import app
 
 
 def test_middleware():
-    client = TestClient(app, base_url="https://example.com")
+    client = TestClient(app, base_url="https://example.com:443")
     response = client.get("/")
     assert response.status_code == 200, response.text
-    client = TestClient(app, base_url="https://subdomain.example.com")
+    client = TestClient(app, base_url="https://subdomain.example.com:443")
     response = client.get("/")
     assert response.status_code == 200, response.text
-    client = TestClient(app, base_url="https://invalidhost")
+    client = TestClient(app, base_url="https://invalidhost:443")
     response = client.get("/")
     assert response.status_code == 400, response.text
