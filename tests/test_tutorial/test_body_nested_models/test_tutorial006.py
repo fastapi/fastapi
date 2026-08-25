2d53bb363837f8a8078a49de2fa94891188bee09
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:27:59 +0000

    fix: remediate 4 vulnerability(ies) in tests/test_tutorial/test_body_nested_models/test_tutorial006.py
    Fixed vulnerabilities:
    - Using clear-text protocols is security-sensitive
      Using http protocol is insecure. Use https instead

--- tests/test_tutorial/test_body_nested_models/test_tutorial006.py
+++ tests/test_tutorial/test_body_nested_models/test_tutorial006.py
@@ -31,7 +31,7 @@ def test_put_all(client: TestClient):
             "tax": 3.2,
             "tags": ["foo", "bar", "foo"],
             "images": [
-                {"url": "http://example.com/image.png", "name": "example image"}
+                {"url": "https://example.com/image.png", "name": "example image"}
             ],
         },
     )
@@ -45,7 +45,7 @@ def test_put_all(client: TestClient):
             "tax": 3.2,
             "tags": IsList("foo", "bar", check_order=False),
             "images": [
-                {"url": "http://example.com/image.png", "name": "example image"}
+                {"url": "https://example.com/image.png", "name": "example image"}
             ],
         },
     }
@@ -100,7 +100,7 @@ def test_put_images_not_list(client: TestClient):
         json={
             "name": "Foo",
             "price": 35.4,
-            "images": {"url": "http://example.com/image.png", "name": "example image"},
+            "images": {"url": "https://example.com/image.png", "name": "example image"},
         },
     )
     assert response.status_code == 422, response.text
@@ -109,7 +109,7 @@ def test_put_images_not_list(client: TestClient):
             {
                 "loc": ["body", "images"],
                 "input": {
-                    "url": "http://example.com/image.png",
+                    "url": "https://example.com/image.png",
                     "name": "example image",
                 },
                 "msg": "Input should be a valid list",
