63bcde374c70b2ae30aa63090d4a16e6e00f585f
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:29:32 +0000

    fix: remediate 1 vulnerability(ies) in docs_src/cors/tutorial001_py310.py
    Fixed vulnerabilities:
    - Using clear-text protocols is security-sensitive
      Using http protocol is insecure. Use https instead

--- docs_src/cors/tutorial001_py310.py
+++ docs_src/cors/tutorial001_py310.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "https://localhost.tiangolo.com",
+    "https://localhost",
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
