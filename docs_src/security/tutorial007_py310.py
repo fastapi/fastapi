f3b6889b31c96041d6d62dfa354d034f02c9cb46
Ubuntu <ubuntu@ip-172-31-17-45.ap-south-1.compute.internal>
2026-08-25 06:09:24 +0000

    fix: remediate 1 vulnerability(ies) in docs_src/security/tutorial007_py310.py
    Fixed vulnerabilities:
    - Hard-coded passwords are security-sensitive
      "password" detected here, review this potentially hard-coded credential.

--- docs_src/security/tutorial007_py310.py
+++ docs_src/security/tutorial007_py310.py
@@ -15,7 +15,7 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
         current_username_bytes, correct_username_bytes
     )
     current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = b"swordfish"
+    correct_password_bytes = __import__("os").environ.get("SWORD_FISH_PASSWORD", "").encode("utf8")
     is_correct_password = secrets.compare_digest(
         current_password_bytes, correct_password_bytes
     )
