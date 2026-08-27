4deb03cb761f141a888c6b1cedee19b044312601
Dream <2468001320@qq.com>
2026-08-28 00:55:18 +0800

    fix: add format binary to bytes schema for OAS 3.0 compatibility
    Fixes #15069

--- fastapi/_compat/v2.py
+++ fastapi/_compat/v2.py
@@ -58,7 +58,7 @@ class GenerateJsonSchema(_GenerateJsonSchema):
     # TODO: remove when this is merged (or equivalent): https://github.com/pydantic/pydantic/pull/12841
     # and dropping support for any version of Pydantic before that one (so, in a very long time)
     def bytes_schema(self, schema: CoreSchema) -> JsonSchemaValue:
-        json_schema = {"type": "string", "contentMediaType": "application/octet-stream"}
+        json_schema = {"type": "string", "format": "binary", "contentMediaType": "application/octet-stream"}
         bytes_mode = (
             self._config.ser_json_bytes
             if self.mode == "serialization"
