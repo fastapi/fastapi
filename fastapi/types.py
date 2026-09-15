7e4294104dd822bd5d2bbe82510105be135725ac
tamy-le <tamy.le.cs@gmail.com>
2026-09-16 01:05:12 +0900

    ✨ Add OpenAPI tag metadata types

--- fastapi/types.py
+++ fastapi/types.py
@@ -1,7 +1,7 @@
 import types
 from collections.abc import Callable
 from enum import Enum
-from typing import Any, TypeVar, Union
+from typing import Any, TypedDict, TypeVar, Union
 
 from pydantic import BaseModel
 from pydantic.main import IncEx as IncEx
@@ -10,3 +10,20 @@ DecoratedCallable = TypeVar("DecoratedCallable", bound=Callable[..., Any])
 UnionType = getattr(types, "UnionType", Union)
 ModelNameMap = dict[type[BaseModel] | type[Enum], str]
 DependencyCacheKey = tuple[Callable[..., Any] | None, tuple[str, ...], str]
+
+
+class _OpenAPIExternalDocsOptional(TypedDict, total=False):
+    description: str
+
+
+class OpenAPIExternalDocs(_OpenAPIExternalDocsOptional):
+    url: str
+
+
+class _OpenAPITagOptional(TypedDict, total=False):
+    description: str
+    externalDocs: OpenAPIExternalDocs
+
+
+class OpenAPITag(_OpenAPITagOptional):
+    name: str
